d75f3ddc7af4a982d573a5da18e5cae971682e26
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-02-11 11:31:08 +0200

    fix: rename page

--- web/src/navigation/index.tsx
+++ web/src/navigation/index.tsx
@@ -1,13 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from '../pages/Home';
-import TraceDiagram from '../pages/Trace';
+import Trace from '../pages/Trace';
 
 const Router = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/trace" element={<TraceDiagram />} />
+        <Route path="/trace" element={<Trace />} />
       </Routes>
     </BrowserRouter>
   );
