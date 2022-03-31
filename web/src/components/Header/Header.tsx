070f7ff9603501c164c3c8a917ba1467dfc18fe1
Oscar Reyes <oscar-rreyes1@hotmail.com>
2022-03-31 10:41:11 -0600

    🔗  137 Adding links to the navigation header (#138)
    * 137 adding links to the header nav
    
    * 137 moving urls to constants

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,13 +1,27 @@
 import {FC} from 'react';
+import {Link, useLocation} from 'react-router-dom';
+import {DOCUMENTATION_URL, GITHUB_URL} from '../../lib/Constants';
 import * as S from './Header.styled';
 
 const Header: FC = () => {
+  const {pathname} = useLocation();
+
   return (
     <S.Header>
-      <S.TitleText>Tracetest</S.TitleText>
-      <S.NavMenu>
-        <S.NavMenuItem>GitHub</S.NavMenuItem>
-        <S.NavMenuItem>Documentation</S.NavMenuItem>
+      <Link to="/">
+        <S.TitleText>Tracetest</S.TitleText>
+      </Link>
+      <S.NavMenu selectedKeys={[pathname]}>
+        <S.NavMenuItem>
+          <a href={GITHUB_URL} target="_blank">
+            GitHub
+          </a>
+        </S.NavMenuItem>
+        <S.NavMenuItem>
+          <a href={DOCUMENTATION_URL} target="_blank">
+            Documentation
+          </a>
+        </S.NavMenuItem>
       </S.NavMenu>
     </S.Header>
   );
--- web/src/lib/Constants.ts
+++ web/src/lib/Constants.ts
@@ -1,1 +1,4 @@
 export const BASE_URL = 'http://localhost:8080';
+
+export const DOCUMENTATION_URL = 'https://kubeshop.github.io/tracetest/';
+export const GITHUB_URL = 'https://github.com/kubeshop/tracetest';
