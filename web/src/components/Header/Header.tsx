665d48cbf08ec53e1955f7aacc42da7f9c1d6bb9
xoscar <oscar-rreyes1@hotmail.com>
2022-03-31 10:40:35 -0600

    137 moving urls to constants

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import {Link, useLocation} from 'react-router-dom';
+import {DOCUMENTATION_URL, GITHUB_URL} from '../../lib/Constants';
 import * as S from './Header.styled';
 
 const Header: FC = () => {
@@ -12,12 +13,12 @@ const Header: FC = () => {
       </Link>
       <S.NavMenu selectedKeys={[pathname]}>
         <S.NavMenuItem>
-          <a href="https://github.com/kubeshop/tracetest" target="_blank">
+          <a href={GITHUB_URL} target="_blank">
             GitHub
           </a>
         </S.NavMenuItem>
         <S.NavMenuItem>
-          <a href="https://kubeshop.github.io/tracetest/" target="_blank">
+          <a href={DOCUMENTATION_URL} target="_blank">
             Documentation
           </a>
         </S.NavMenuItem>
