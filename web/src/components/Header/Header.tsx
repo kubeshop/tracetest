db60ac79d92bca19d452b56c3715f5018dc66ee7
xoscar <oscar-rreyes1@hotmail.com>
2022-03-31 10:31:34 -0600

    137 adding links to the header nav

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
 import {FC} from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import * as S from './Header.styled';
 
 const Header: FC = () => {
+  const {pathname} = useLocation();
+
   return (
     <S.Header>
-      <S.TitleText>Tracetest</S.TitleText>
-      <S.NavMenu>
-        <S.NavMenuItem>GitHub</S.NavMenuItem>
-        <S.NavMenuItem>Documentation</S.NavMenuItem>
+      <Link to="/">
+        <S.TitleText>Tracetest</S.TitleText>
+      </Link>
+      <S.NavMenu selectedKeys={[pathname]}>
+        <S.NavMenuItem>
+          <a href="https://github.com/kubeshop/tracetest" target="_blank">
+            GitHub
+          </a>
+        </S.NavMenuItem>
+        <S.NavMenuItem>
+          <a href="https://kubeshop.github.io/tracetest/" target="_blank">
+            Documentation
+          </a>
+        </S.NavMenuItem>
       </S.NavMenu>
     </S.Header>
   );
