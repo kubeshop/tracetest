e141bccb97e39ee44b70c57358a1d2b124c0b885
xoscar <oscar-rreyes1@hotmail.com>
2022-04-18 14:04:40 -0500

    218 removing the initial trace test node from the diagram

--- web/src/components/Trace/Trace.tsx
+++ web/src/components/Trace/Trace.tsx
@@ -16,6 +16,7 @@ import * as S from './Trace.styled';
 
 import SpanDetail from './SpanDetail';
 import TestResults from './TestResults';
+import {DEFAULT_SPAN_NAME} from '../../lib/Constants';
 
 const Grid = styled.div`
   display: grid;
@@ -39,16 +40,23 @@ const Trace = ({test, testResultId}: {test: Test; testResultId: string}) => {
     refetch: refetchTrace,
   } = useGetTestResultByIdQuery({testId: test.testId, resultId: testResultId});
 
-  const spanMap = useMemo<TSpanMap>(() => {
-    return testResultDetails?.trace?.resourceSpans
-      ?.map(i => i.instrumentationLibrarySpans.map((el: any) => el.spans))
-      ?.flat(2)
-      ?.reduce((acc, span) => {
-        acc[span.spanId] = acc[span.spanId] || {id: span.spanId, parentIds: [], data: span};
+  const spanMap = useMemo(() => {
+    const spanList: ISpan[] =
+      testResultDetails?.trace?.resourceSpans
+        ?.map(i => i.instrumentationLibrarySpans.map((el: any) => el.spans))
+        ?.flat(2) || [];
+
+    const defaultSpan = spanList.find(({name, parentSpanId}) => name === DEFAULT_SPAN_NAME && !parentSpanId);
+
+    return spanList.reduce<TSpanMap>((acc, span) => {
+      if (span.spanId === defaultSpan?.spanId) return acc;
+
+      acc[span.spanId] = acc[span.spanId] || {id: span.spanId, parentIds: [], data: span};
+      if (span.parentSpanId !== defaultSpan?.spanId && span.parentSpanId)
         acc[span.spanId].parentIds.push(span.parentSpanId);
 
-        return acc;
-      }, {});
+      return acc;
+    }, {});
   }, [testResultDetails]);
 
   const addSelected = useStoreActions(actions => actions.addSelectedElements);
--- web/src/lib/Constants.ts
+++ web/src/lib/Constants.ts
@@ -2,3 +2,5 @@ export const BASE_URL = 'http://localhost:8080';
 
 export const DOCUMENTATION_URL = 'https://kubeshop.github.io/tracetest/';
 export const GITHUB_URL = 'https://github.com/kubeshop/tracetest';
+
+export const DEFAULT_SPAN_NAME = 'tracetest';
