4ca7e268023f4f97f8838d8c9cf4ec44cc3606fb
Cesco Ferraro <francescoaferraro@gmail.com>
2022-05-10 20:37:08 -0300

    PR changes applied

--- web/src/components/SpanDetail/components/HTTPAttributesTabs.tsx
+++ web/src/components/SpanDetail/components/HTTPAttributesTabs.tsx
@@ -1,45 +1,26 @@
 import {Table, Tabs, Typography} from 'antd';
 import React from 'react';
-import styled from 'styled-components';
+import AttributesService from '../../../services/Atributes.service';
 import CustomTable from '../../CustomTable';
-
-const STabs = styled(Tabs)`
-  .ant-tabs-content-holder {
-    padding-bottom: 100px;
-  }
-`;
+import {HTTPAttributesTabsComponent} from './Component.styled';
 
 interface IProps {
   attributeList: any[];
 }
 
-export const HTTPAttributesTabs = ({attributeList}: IProps): JSX.Element => {
+export const HTTPAttributesTabs: React.FC<IProps> = ({attributeList}) => {
   const render = (value: any) => <span data-cy="assertion-check-property">{value}</span>;
   return (
     <>
       <Typography.Title level={5} style={{margin: 0}}>
         Attributes
       </Typography.Title>
-      <STabs id="the_tabs" style={{marginBottom: 150, overflow: 'hidden'}}>
+      <HTTPAttributesTabsComponent id="the_tabs" style={{marginBottom: 150, overflow: 'hidden'}}>
         <Tabs.TabPane style={{overflow: 'hidden'}} tab={<span>Request</span>} key="span-request-detail">
           <CustomTable
             size="small"
             pagination={{hideOnSinglePage: true}}
-            dataSource={attributeList?.filter(a => {
-              return (
-                [
-                  'http.method',
-                  'http.url',
-                  'http.target',
-                  'http.host',
-                  'http.scheme',
-                  'http.request_content_length',
-                  'http.request_content_length_uncompressed',
-                  'http.retry_count exists',
-                  'http.user_agent',
-                ].includes(a.key) || a.key.includes('http.request')
-              );
-            })}
+            dataSource={AttributesService.requestAttributesListFN(attributeList)}
           >
             <Table.Column
               title="Key"
@@ -56,9 +37,7 @@ export const HTTPAttributesTabs = ({attributeList}: IProps): JSX.Element => {
           <CustomTable
             size="small"
             pagination={{hideOnSinglePage: true}}
-            dataSource={attributeList?.filter(a => {
-              return a.key.includes('http.response');
-            })}
+            dataSource={AttributesService.responseAttributesFN(attributeList)}
           >
             <Table.Column title="Key" dataIndex="key" key="key" ellipsis width="50%" render={render} />
             <Table.Column title="Value" dataIndex="value" key="value" ellipsis width="50%" render={render} />
@@ -70,7 +49,7 @@ export const HTTPAttributesTabs = ({attributeList}: IProps): JSX.Element => {
             <Table.Column title="Value" dataIndex="value" key="value" ellipsis width="50%" render={render} />
           </CustomTable>
         </Tabs.TabPane>
-      </STabs>
+      </HTTPAttributesTabsComponent>
     </>
   );
 };
