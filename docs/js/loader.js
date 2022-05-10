4290f10103d8465b31a4d85bdf85ca887c200e68
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-05-10 20:02:25 -0300

    add loader

---
+++ docs/js/loader.js
@@ -0,0 +1,16 @@
+window.mermaidConfig = {
+    startOnLoad: false,
+    theme: "default",
+    flowchart: {
+        htmlLabels: false
+    },
+    er: {
+        useMaxWidth: false
+    },
+    sequence: {
+        useMaxWidth: false,
+        noteFontWeight: "14px",
+        actorFontSize: "14px",
+        messageFontSize: "16px"
+    }
+}
