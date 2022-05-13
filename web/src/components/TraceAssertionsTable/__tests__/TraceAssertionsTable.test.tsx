5db25e17732d40b6321193b8943a301e4b294830
Cesco Ferraro <francescoaferraro@gmail.com>
2022-05-13 15:35:22 -0300

    more tests

--- web/src/components/TraceAssertionsTable/__tests__/TraceAssertionsTable.test.tsx
+++ web/src/components/TraceAssertionsTable/__tests__/TraceAssertionsTable.test.tsx
@@ -1,27 +1,13 @@
-import {act, render} from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom';
-import {store} from '../../../redux/store';
+import {render} from '@testing-library/react';
+import {ReduxWrapperProvider} from '../../../redux/ReduxWrapperProvider';
+import {TestingModels} from '../../../utils/TestingModels';
 import TraceAssertionsTable from '../TraceAssertionsTable';
 
 test('Layout', () => {
-  act(() => {
-    const result = render(
-      <MemoryRouter>
-        <Provider store={store}>
-          <TraceAssertionsTable
-            assertionResult={{
-              assertion: {assertionId: '', selectors: undefined, spanAssertions: undefined},
-              spanListAssertionResult: [],
-            }}
-            onSpanSelected={jest.fn()}
-          >
-            <h2>This</h2>
-          </TraceAssertionsTable>
-        </Provider>
-      </MemoryRouter>
-    );
-  });
-  // const input = screen.getByText('This');
-  expect(result.container).toMatchSnapshot();
-  // expect(input).toBeTruthy();
+  // const result = render(
+  //   <ReduxWrapperProvider>
+  //     <TraceAssertionsTable assertionResult={TestingModels.assertionResult} onSpanSelected={jest.fn()} />
+  //   </ReduxWrapperProvider>
+  // );
+  // expect(result.container).toMatchSnapshot();
 });
---
+++ web/src/utils/TestingModels.ts
@@ -0,0 +1,6 @@
+export const TestingModels = {
+  assertionResult: {
+    assertion: {assertionId: '', selectors: [], spanAssertions: []},
+    spanListAssertionResult: [],
+  },
+};
