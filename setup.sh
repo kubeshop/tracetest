c95c37551e8ceeea79ed626a95d06a6ab1801a13
Sebastian Choren <schoren@users.noreply.github.com>
2022-05-16 16:44:42 -0300

    Add sample setup.sh file

---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+NAMESPACE="tractest"
+SKIP_PMA="no"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--namespace)
+      EXTENSION="$2"
+      shift
+      shift
+      ;;
+    --skip-pma)
+      SKIP_PMA="YES"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+echo "----------------------------"
+echo "Installing Tracetest"
+echo "----------------------------"
+echo 
+echo
+
+helm repo add kubeshop https://kubeshop.github.io/helm-charts
+helm repo update
+
+# helm install tracetest kubeshop/tracetest \
+#   --namespace $NAMESPACE \
+#   --set tracingBackend=jaeger \
+#   --set jaegerConnectionConfig.endpoint="jaeger-query:16685"
+
+
+if [ "$SKIP_PMA" = "no" ]; then
+    echo 
+    echo
+    echo "----------------------------"
+    echo "Installing PokeShop"
+    echo "----------------------------"
+    echo 
+    echo
+    tmpdir=`mktemp -d`
+    curl -L https://github.com/kubeshop/pokeshop/tarball/master | tar -xz --strip-components 1 -C  $tmpdir
+    cd $tmpdir/helm-chart
+    helm dependency update
+    helm install -n $NAMESPACE -f values.yaml .
+
+fi
