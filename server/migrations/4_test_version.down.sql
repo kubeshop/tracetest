b06dbbd732226aa90ec388eecdb1639ca8b3b2f1
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-05-24 13:56:27 -0300

    fix down migration

--- server/migrations/4_test_version.down.sql
+++ server/migrations/4_test_version.down.sql
@@ -1,11 +1,10 @@
-ALTER TABLE tests
-DROP COLUMN "version";
-
-DROP INDEX idx_unique_test_id_version;
-
+ALTER TABLE runs
+DROP CONSTRAINT test_id_version_fk;
 
 ALTER TABLE runs
 DROP COLUMN test_version;
 
-ALTER TABLE runs
-DROP CONSTRAINT test_id_version_fk;
+DROP INDEX idx_unique_test_id_version;
+
+ALTER TABLE tests
+DROP COLUMN "version";
