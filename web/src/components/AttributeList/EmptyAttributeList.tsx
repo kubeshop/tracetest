6981e64a5d61eee4365213b537e9732b3fb9b689
xoscar <oscar-rreyes1@hotmail.com>
2022-05-31 12:01:05 -0500

    570 adding fixes

--- web/src/components/AttributeList/EmptyAttributeList.tsx
+++ web/src/components/AttributeList/EmptyAttributeList.tsx
@@ -1,4 +1,4 @@
-import {RESOURCE_SEMANTIC_CONVENTIONS_URL, TRACE_SEMANTIC_CONVENTIONS_URL} from '../../constants/Common.constants';
+import {RESOURCE_SEMANTIC_CONVENTIONS_URL, TRACE_SEMANTIC_CONVENTIONS_URL} from 'constants/Common.constants';
 import * as S from './AttributeList.styled';
 
 const EmptyAttributeList: React.FC = () => {
@@ -8,8 +8,14 @@ const EmptyAttributeList: React.FC = () => {
       <S.EmptyTitle>Looking for attributes?</S.EmptyTitle>
       <S.EmptyText>
         Take a look a the open telemetry specification for{' '}
-        <a href={TRACE_SEMANTIC_CONVENTIONS_URL}>trace semantic conventions</a> and here for{' '}
-        <a href={RESOURCE_SEMANTIC_CONVENTIONS_URL}>resource semantic conventions</a>.
+        <a href={TRACE_SEMANTIC_CONVENTIONS_URL} target="_blank">
+          trace semantic conventions
+        </a>{' '}
+        and here for{' '}
+        <a href={RESOURCE_SEMANTIC_CONVENTIONS_URL} target="_blank">
+          resource semantic conventions
+        </a>
+        .
       </S.EmptyText>
     </S.EmptyAttributeList>
   );
