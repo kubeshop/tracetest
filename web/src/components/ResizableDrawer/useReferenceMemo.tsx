aac9a5d6a7eed098018743f03b026532f3ef95d2
cescoferraro <francescoaferraro@gmail.com>
2022-06-01 15:40:13 -0300

    new drawer max height

--- web/src/components/ResizableDrawer/useReferenceMemo.tsx
+++ web/src/components/ResizableDrawer/useReferenceMemo.tsx
@@ -4,9 +4,12 @@ export function useReferenceMemo(visiblePortion: number) {
   return useMemo(() => {
     return {
       CLOSE: visiblePortion,
+      // used when the page initially loads
       INITIAL: window.outerHeight * 0.2,
+      // used when user open the assertion block form
       FORM: window.outerHeight * 0.4,
-      OPEN: window.outerHeight * 0.5,
+      // used to limit drawer height
+      OPEN: window.outerHeight * 0.85,
     };
   }, [visiblePortion]);
 }
