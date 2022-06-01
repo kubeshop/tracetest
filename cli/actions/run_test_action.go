4ef128152891035dc11057189249d67ec83b6973
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-06-01 16:11:57 -0300

    print json containing test and run ids after running test

--- cli/actions/run_test_action.go
+++ cli/actions/run_test_action.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/kubeshop/tracetest/cli/config"
@@ -24,6 +25,11 @@ type runTestAction struct {
 
 var _ Action[RunTestConfig] = &runTestAction{}
 
+type runTestOutput struct {
+	TestID string `json:"testId"`
+	RunID  string `json:"testRunId"`
+}
+
 func NewRunTestAction(config config.Config, logger *zap.Logger, client *openapi.APIClient) runTestAction {
 	return runTestAction{config, logger, client}
 }
@@ -34,44 +40,57 @@ func (a runTestAction) Run(ctx context.Context, args RunTestConfig) error {
 	}
 
 	a.logger.Debug("Running test from definition", zap.String("definitionFile", args.DefinitionFile))
-	return a.runDefinition(ctx, args.DefinitionFile)
+	output, err := a.runDefinition(ctx, args.DefinitionFile)
+
+	if err != nil {
+		return fmt.Errorf("could not run definition: %w", err)
+	}
+
+	bytes, err := json.Marshal(output)
+	if err != nil {
+		return fmt.Errorf("could not marshal output json: %w", err)
+	}
+
+	fmt.Printf(string(bytes))
+	return nil
 }
 
-func (a runTestAction) runDefinition(ctx context.Context, definitionFile string) error {
+func (a runTestAction) runDefinition(ctx context.Context, definitionFile string) (runTestOutput, error) {
 	definition, err := file.LoadDefinition(definitionFile)
 	if err != nil {
-		return err
+		return runTestOutput{}, err
 	}
 
 	err = definition.Validate()
 	if err != nil {
-		return fmt.Errorf("invalid definition file: %w", err)
+		return runTestOutput{}, fmt.Errorf("invalid definition file: %w", err)
 	}
 
 	if definition.Id == "" {
 		a.logger.Debug("test doesn't exist. Creating it")
 		testID, err := a.createTestFromDefinition(ctx, definition)
 		if err != nil {
-			return fmt.Errorf("could not create test from definition: %w", err)
+			return runTestOutput{}, fmt.Errorf("could not create test from definition: %w", err)
 		}
 
 		definition.Id = testID
 		err = file.SaveDefinition(definitionFile, definition)
 		if err != nil {
-			return fmt.Errorf("could not save definition: %w", err)
+			return runTestOutput{}, fmt.Errorf("could not save definition: %w", err)
 		}
 	}
 
-	// TODO: update definition
+	// TODO: update definition if definition.Id is present
 
-	testRunId, err := a.runTest(ctx, definition.Id)
+	testRun, err := a.runTest(ctx, definition.Id)
 	if err != nil {
-		return fmt.Errorf("could not run test: %w", err)
+		return runTestOutput{}, fmt.Errorf("could not run test: %w", err)
 	}
 
-	fmt.Println("testRunId", testRunId)
-
-	return nil
+	return runTestOutput{
+		TestID: definition.Id,
+		RunID:  *testRun.Id,
+	}, nil
 }
 
 func (a runTestAction) createTestFromDefinition(ctx context.Context, definition definition.Test) (string, error) {
@@ -81,7 +100,7 @@ func (a runTestAction) createTestFromDefinition(ctx context.Context, definition 
 	}
 
 	req := a.client.ApiApi.CreateTest(ctx)
-	req.Test(openapiTest)
+	req = req.Test(openapiTest)
 
 	createdTest, _, err := a.client.ApiApi.CreateTestExecute(req)
 	if err != nil {
@@ -91,12 +110,12 @@ func (a runTestAction) createTestFromDefinition(ctx context.Context, definition 
 	return *createdTest.Id, nil
 }
 
-func (a runTestAction) runTest(ctx context.Context, testID string) (string, error) {
+func (a runTestAction) runTest(ctx context.Context, testID string) (openapi.TestRun, error) {
 	req := a.client.ApiApi.RunTest(ctx, testID)
 	run, _, err := a.client.ApiApi.RunTestExecute(req)
 	if err != nil {
-		return "", fmt.Errorf("could not execute request: %w", err)
+		return openapi.TestRun{}, fmt.Errorf("could not execute request: %w", err)
 	}
 
-	return *run.Id, nil
+	return *run, nil
 }
