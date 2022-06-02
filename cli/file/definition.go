21501055960b355ddca4cfbd1084b2b81591d23e
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-06-02 12:15:49 -0300

    improve error messages

--- cli/file/definition.go
+++ cli/file/definition.go
@@ -11,13 +11,13 @@ import (
 func LoadDefinition(file string) (definition.Test, error) {
 	fileBytes, err := os.ReadFile(file)
 	if err != nil {
-		return definition.Test{}, fmt.Errorf("could not read file %s: %w", file, err)
+		return definition.Test{}, fmt.Errorf("could not read test definition file %s: %w", file, err)
 	}
 
 	test := definition.Test{}
 	err = yaml.Unmarshal(fileBytes, &test)
 	if err != nil {
-		return definition.Test{}, fmt.Errorf("could not parse definition file: %w", err)
+		return definition.Test{}, fmt.Errorf("could not parse test definition file: %w", err)
 	}
 
 	return test, nil
