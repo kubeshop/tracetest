83119b32c0e78fe67b178fe773c5cb16318de1a5
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-06-02 19:35:19 -0300

    add export cmd (#650)

---
+++ cli/cmd/test_export_cmd_test.go
@@ -0,0 +1,31 @@
+package cmd_test
+
+import (
+	"testing"
+
+	"github.com/kubeshop/tracetest/cli/cmd/e2e"
+	"github.com/kubeshop/tracetest/cli/file"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTestExportCmd(t *testing.T) {
+	cli := e2e.NewCLI()
+
+	definitionFile := "definition_file.yml"
+	t.Cleanup(func() {
+		deleteFile(definitionFile)
+	})
+
+	output, err := cli.RunCommand("test", "export", "--config", "e2e/config.yml", "--id", "a6074714-e986-4747-a399-b87748143884", "--output", definitionFile)
+	assert.NoError(t, err)
+	assert.Empty(t, output)
+
+	definition, err := file.LoadDefinition(definitionFile)
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, definition.Id)
+	assert.NotEmpty(t, definition.Name)
+	assert.NotEmpty(t, definition.Trigger.Type)
+	assert.NotEmpty(t, definition.Trigger.HTTPRequest.URL)
+}
