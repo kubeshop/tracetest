5b5ebcd56ed13ad69d59fb1d636c494c0c3ff9bd
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-06-03 15:27:46 -0300

    load config from home path when current dir doesnt contain config file

--- cli/actions/configure_action.go
+++ cli/actions/configure_action.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/kubeshop/tracetest/cli/config"
@@ -34,8 +35,13 @@ func NewConfigureAction(config config.Config, logger *zap.Logger, client *openap
 }
 
 func (a configureAction) Run(ctx context.Context, args ConfigureConfig) error {
+	existingConfig := a.loadExistingConfig(args)
 	var serverURL string
-	fmt.Print("Enter your Tracetest server URL: ")
+	if existingConfig.Scheme != "" && existingConfig.Endpoint != "" {
+		fmt.Printf("Enter your Tracetest server URL (current value: %s://%s): ", existingConfig.Scheme, existingConfig.Endpoint)
+	} else {
+		fmt.Print("Enter your Tracetest server URL: ")
+	}
 	_, err := fmt.Fscanf(os.Stdin, "%s", &serverURL)
 	if err != nil {
 		return fmt.Errorf("could not read text from input: %w", err)
@@ -68,14 +74,24 @@ func (a configureAction) Run(ctx context.Context, args ConfigureConfig) error {
 	return nil
 }
 
+func (a configureAction) loadExistingConfig(args ConfigureConfig) config.Config {
+	configPath, err := a.getConfigurationPath(args)
+	if err != nil {
+		return config.Config{}
+	}
+
+	c, err := config.LoadConfig(configPath)
+	if err != nil {
+		return config.Config{}
+	}
+
+	return c
+}
+
 func (a configureAction) saveConfiguration(ctx context.Context, config config.Config, args ConfigureConfig) error {
-	configPath := "./config.yml"
-	if args.Global {
-		homePath, err := os.UserHomeDir()
-		if err != nil {
-			return fmt.Errorf("could not get user home dir: %w", err)
-		}
-		configPath = path.Join(homePath, ".tracetest/config.yml")
+	configPath, err := a.getConfigurationPath(args)
+	if err != nil {
+		return fmt.Errorf("could not get configuration path: %w", err)
 	}
 
 	configYml, err := yaml.Marshal(config)
@@ -83,10 +99,26 @@ func (a configureAction) saveConfiguration(ctx context.Context, config config.Co
 		return fmt.Errorf("could not marshal configuration into yml: %w", err)
 	}
 
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(configPath), 0700) // Ensure folder exists
+	}
 	err = os.WriteFile(configPath, configYml, 0755)
 	if err != nil {
 		return fmt.Errorf("could not write file: %w", err)
 	}
 
 	return nil
+}
+
+func (a configureAction) getConfigurationPath(args ConfigureConfig) (string, error) {
+	configPath := "./config.yml"
+	if args.Global {
+		homePath, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("could not get user home dir: %w", err)
+		}
+		configPath = path.Join(homePath, ".tracetest/config.yml")
+	}
+
+	return configPath, nil
 }
--- cli/config/config.go
+++ cli/config/config.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -13,16 +16,42 @@ type Config struct {
 	ServerPath *string `yaml:"serverPath,omitempty"`
 }
 
+func (c Config) IsEmpty() bool {
+	thisConfigJson, _ := json.Marshal(c)
+	emptyConfigJson, _ := json.Marshal(Config{})
+
+	return string(thisConfigJson) == string(emptyConfigJson)
+}
+
 func LoadConfig(configFile string) (Config, error) {
+	config, err := loadConfig(configFile)
+	if err != nil {
+		return Config{}, err
+	}
+
+	if !config.IsEmpty() {
+		return config, nil
+	}
+
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("could not get user home path")
+	}
+
+	globalConfigPath := filepath.Join(homePath, ".tracetest/config.yml")
+	return loadConfig(globalConfigPath)
+}
+
+func loadConfig(configFile string) (Config, error) {
 	viper.SetConfigFile(configFile)
 	viper.SetConfigType("yaml")
 	viper.SetEnvPrefix("tracetest")
-	viper.AddConfigPath("$HOME/.tracetest/")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		return Config{}, fmt.Errorf("could not load config file: %w", err)
+		// No configuration file found, return empty config
+		return Config{}, nil
 	}
 
 	config := Config{}
