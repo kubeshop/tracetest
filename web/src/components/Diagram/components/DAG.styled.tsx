e8d981b36788cf9623b59aee80e7cbf8a7651bd6
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2022-06-06 12:41:56 -0500

    Add zoom in and zoom out controls in test view page (#671)

--- web/src/components/Diagram/components/DAG.styled.tsx
+++ web/src/components/Diagram/components/DAG.styled.tsx
@@ -1,3 +1,4 @@
+import {Button} from 'antd';
 import styled, {css} from 'styled-components';
 
 export const Container = styled.div<{$showAffected: boolean}>`
@@ -11,4 +12,23 @@ export const Container = styled.div<{$showAffected: boolean}>`
         opacity: 0.5;
       }
     `}
+`;
+
+export const Controls = styled.div`
+  background-color: #fbfbff;
+  border-bottom-left-radius: 8px;
+  position: absolute;
+  right: 0;
+  top: -14px;
+  z-index: 9;
+`;
+
+export const ZoomButton = styled(Button)`
+  color: rgba(3, 24, 73, 0.3);
+  width: 24px;
+
+  &:hover,
+  &:focus {
+    background-color: unset;
+  }
 `;
