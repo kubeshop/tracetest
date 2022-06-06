aa43e3a8528c8675e22c4eb993443f344090537e
Sebastian Choren <sebastian.choren@gmail.com>
2022-06-06 15:41:02 -0300

    - Check crds before installing - Better ouptut - Better cmd order

--- setup.sh
+++ setup.sh
@@ -21,6 +21,54 @@ help_message() {
   echo
 }
 
+crd_exists() {
+  kubectl get crd $1 > /dev/null 2>&1
+  return
+}
+
+install_cert_manager(){
+  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+  echo
+  echo "--> waiting for cert-manager"
+  kubectl wait --for=condition=ready pod -l app=webhook --namespace cert-manager
+  echo "--> cert-manager ready. Create self signed ClusterIssuer"
+  cat <<EOF | kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: selfsigned
+spec:
+  selfSigned: {}
+EOF
+  echo "--> ClusterIssuer created"
+}
+
+install_jaeger() {
+  if crd_exists "jaegers.jaegertracing.io"; then
+    echo "--> CRD 'jaegers.jaegertracing.io' already exists. Assume jaeger-operator is installed"
+  else
+    set +e #ignore errors here
+    kubectl create namespace observability
+    set -e
+
+    kubectl create -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.32.0/jaeger-operator.yaml -n observability
+    echo
+    echo "--> waiting for jaeger-operator"
+    kubectl wait --for=condition=ready pod -l name=jaeger-operator --namespace observability --timeout 5m
+    sleep 5
+    echo "--> jaeger-operator ready"
+  fi
+
+  echo "--> creating jaeger instance"
+  cat <<EOF | kubectl apply --namespace $NAMESPACE -f -
+apiVersion: jaegertracing.io/v1
+kind: Jaeger
+metadata:
+  name: jaeger
+EOF
+   echo "--> jaeger instance created"
+}
+
 while [[ $# -gt 0 ]]; do
   case $1 in
     --namespace)
@@ -61,14 +109,6 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-echo "----------------------------"
-echo "Installing Tracetest"
-echo "----------------------------"
-echo
-
-helm repo add kubeshop https://kubeshop.github.io/helm-charts
-helm repo update
-
 
 echo "--> install tracetest to namespace $NAMESPACE"
 helm upgrade --install tracetest kubeshop/tracetest \
@@ -85,45 +125,16 @@ if [ "$SKIP_JAEGER" != "YES" ]; then
     echo
     echo
 
-    echo
     echo "--> install cert-manager"
-    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
-    echo
-    echo "--> waiting for cert-manager"
-    kubectl wait --for=condition=ready pod -l app=webhook --namespace cert-manager
-    echo "--> cert-manager ready"
-    echo
-
-    cat <<EOF | kubectl apply -f -
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: selfsigned
-spec:
-  selfSigned: {}
-EOF
+    if crd_exists "certificates.cert-manager.io"; then
+      echo "--> CRD 'certificates.cert-manager.io' already exists. Assume cert-manager is installed"
+    else
+      install_cert_manager
+    fi
 
     echo
     echo "--> install jaeger"
-
-    set +e #ignore errors here
-    kubectl create namespace observability
-    set -e
-
-    kubectl create -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.32.0/jaeger-operator.yaml -n observability
-    echo
-    echo "--> waiting for jaeger-operator"
-    kubectl wait --for=condition=ready pod -l name=jaeger-operator --namespace observability --timeout 5m
-    sleep 5
-    echo "--> jaeger-operator ready"
-    echo
-
-    cat <<EOF | kubectl apply --namespace $NAMESPACE -f -
-apiVersion: jaegertracing.io/v1
-kind: Jaeger
-metadata:
-  name: jaeger
-EOF
+    install_jaeger
 fi
 
 
@@ -135,19 +146,34 @@ if [ "$SKIP_PMA" != "YES" ]; then
     echo "----------------------------"
     echo
     echo
+
     tmpdir=`mktemp -d`
     curl -L https://github.com/kubeshop/pokeshop/tarball/master | tar -xz --strip-components 1 -C  $tmpdir
     cd $tmpdir/helm-chart
     helm dependency update
+
     helm upgrade --install demo . \
       --namespace demo --create-namespace \
       -f values.yaml \
-      --set 'env[0].value=postgresql://ashketchum:squirtle123@demo-postgresql:5432/pokeshop?schema=public' \
-      --set 'env[1].value=demo-redis-master' \
-      --set 'env[3].value=pokemon:$(RABBITMQ_PASSWORD)@demo-rabbitmq-headless' \
-      --set 'env[4].value=https://pokeapi.co/api/v2' \
+      --set postgres.auth.username=ashketchum,postgres.auth.password=squirtle123,postgres.auth.database=pokeshop \
+      --set rabbitmq.auth.username=guest,rabbitmq.auth.password=guest,rabbitmq.auth.erlangCookie=secretcookie \
       --set 'env[5].value=jaeger-agent.'$NAMESPACE'.svc.cluster.local'
+
+    echo "--> waiting for demo app to be ready"
+    kubectl wait --for=condition=ready pod --namespace demo demo-rabbitmq-0
+    kubectl wait --for=condition=ready pod --namespace demo -l app.kubernetes.io/name=pokemon-api
+    echo "--> demo app ready"
+    rm -rf $tmpdir
 fi
+
+
+echo "----------------------------"
+echo "Installing Tracetest"
+echo "----------------------------"
+echo
+
+helm repo add kubeshop https://kubeshop.github.io/helm-charts
+helm repo update
 
 echo
 echo
