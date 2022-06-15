70d7cb37c102c8dea656d69a0d56eae849b80ae2
Sebastian Choren <sebastian.choren@gmail.com>
2022-06-14 22:06:58 -0300

    add debug

--- tracetesting/list_tests.bash
+++ tracetesting/list_tests.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+NAME="list_tests"
+echo "-> $NAME"
 source ./funcs.bash
 
 EXIT_STATUS=0
 
 # setup
 export TEST_ID=$(tracetest_target test run --definition ./definitions/pokemon_list.yml | jq -rc '.test.id')
+echo $TEST_ID
 
 #run test
-run_test "list_tests" "./definitions/tracetest_tests_list.yml"  || EXIT_STATUS=$?
+run_test $NAME "./definitions/tracetest_tests_list.yml"  || EXIT_STATUS=$?
 
 # cleanup
 tracetest_target_curl "/api/tests/$TEST_ID" -X DELETE
