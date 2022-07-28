20db550d2c4df9cab3e7defed5ab6f1f31e2a8b0
Sebastian Choren <sebastian.choren@gmail.com>
2022-07-28 16:07:08 -0300

    update install script

--- install-cli.sh
+++ install-cli.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cmd_exists() {
+  command -v $1 &> /dev/null
+}
+
 ensure_dependency_exist() {
-    if ! command -v $1 &> /dev/null
-    then
+    if ! cmd_exists $1; then
         echo "missing dependency: $1 is required to run this script"
         exit 2
     fi
@@ -14,12 +17,11 @@ ensure_required_dependencies_are_present() {
 }
 
 get_latest_version() {
-    redirect_url=`curl -Ls -o /dev/null -w %{url_effective} https://github.com/kubeshop/tracetest/releases/latest`
-    url_suffix="https://github.com/kubeshop/tracetest/releases/tag/"
-    version=${redirect_url/#$url_suffix}
-
-    echo $version
+  curl --silent "https://api.github.com/repos/kubeshop/tracetest/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
 }
+
 
 get_os() {
     os_name=`uname | tr '[:upper:]' '[:lower:]'`
@@ -41,8 +43,10 @@ get_download_link() {
     os=$1
     arch=$2
     version=$3
+    pkg=$4
+    raw_version=`echo $version | sed 's/v//'`
 
-    echo "https://github.com/kubeshop/tracetest/releases/download/$version/tracetest-$version-$os-$arch.tar.gz"
+    echo "https://github.com/kubeshop/tracetest/releases/download/${version}/tracetest_${raw_version}_${os}_${arch}.${pkg}"
 }
 
 download_file() {
@@ -55,11 +59,29 @@ download_file() {
     echo "File downloaded and saved to $path"
 }
 
-install_cli() {
-    compressed_file_path=$1
+install_tar() {
+  download_link=$1
+  file_path="/tmp/cli.tar.gz"
+  download_file "$download_link" "$file_path"
 
-    tar -xvf $compressed_file_path -C /tmp
-    sudo mv /tmp/tracetest /usr/local/bin/tracetest
+  tar -xvf $compressed_file_path -C /tmp
+  sudo mv /tmp/tracetest /usr/local/bin/tracetest
+}
+
+install_dpkg() {
+  download_link=$1
+  file_path="/tmp/cli.deb"
+  download_file "$download_link" "$file_path"
+
+  sudo dpkg -i $file_path
+}
+
+install_rpm() {
+  download_link=$1
+  file_path="/tmp/cli.rpm"
+  download_file "$download_link" "$file_path"
+
+  sudo rpm -i $file_path
 }
 
 run() {
@@ -72,16 +94,23 @@ run() {
     fi
 
     latest_version=`get_latest_version`
-    os=`get_os`
     arch=`get_arch`
-    download_link=`get_download_link $os $arch $latest_version`
-    file_path="/tmp/cli.tar.gz"
 
-    echo "Downloading Tracetest CLI $latest_version for $os $arch"
-    echo "URL link: $download_link"
-
-    download_file "$download_link" "$file_path"
-    install_cli "$file_path"
+    if cmd_exists dpkg; then
+      download_link=`get_download_link $os $arch $latest_version deb`
+      echo
+      echo
+      echo $download_link
+      echo
+      echo
+      install_dpkg $download_link
+    elif cmd_exists rpm; then
+      download_link=`get_download_link $os $arch $latest_version rpm`
+      install_rpm $download_link
+    else
+      download_link=`get_download_link $os $arch $latest_version tar.gz`
+      install_tar $download_link
+    fi
 }
 
 run
