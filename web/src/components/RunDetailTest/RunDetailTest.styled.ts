152facf08e57a81c15f7011b025f7cd8a3ecd3f4
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2022-09-22 13:53:20 +0200

    Add timeline view in test mode

--- web/src/components/RunDetailTest/RunDetailTest.styled.ts
+++ web/src/components/RunDetailTest/RunDetailTest.styled.ts
@@ -1,43 +1,4 @@
-import styled, {css} from 'styled-components';
-
-export const Aside = styled.div<{$isOpen: boolean}>`
-  background-color: ${({theme}) => theme.color.white};
-  box-shadow: 0 20px 24px rgba(153, 155, 168, 0.18);
-  height: 100%;
-  min-width: 30px;
-  overflow: visible;
-  position: relative;
-  transition: width ease 0.2s, min-width ease 0.2s;
-  width: 30px;
-  z-index: 2;
-
-  > div:first-child {
-    opacity: 0;
-  }
-
-  ${({$isOpen}) =>
-    $isOpen &&
-    css`
-      min-width: 270px;
-      transition: width ease 0.2s 0.05s, min-width ease 0.2s 0.05s;
-      width: 270px;
-
-      > div:first-child {
-        opacity: 1;
-      }
-    `}
-`;
-
-export const AsideButtonContainer = styled.div`
-  position: absolute;
-  right: -12px;
-  top: 48px;
-`;
-
-export const AsideContent = styled.div`
-  height: 100%;
-  overflow: hidden;
-`;
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -57,6 +18,13 @@ export const SectionLeft = styled(Section)`
 export const SectionRight = styled(Section)<{$shouldScroll: boolean}>`
   background-color: ${({theme}) => theme.color.white};
   box-shadow: 0 20px 24px rgba(153, 155, 168, 0.18);
-  overflow-y: ${({$shouldScroll}) => $shouldScroll ? 'scroll' : 'hidden'};
+  overflow-y: ${({$shouldScroll}) => ($shouldScroll ? 'scroll' : 'hidden')};
   z-index: 2;
+`;
+
+export const SwitchContainer = styled.div`
+  bottom: 163px;
+  left: 16px;
+  position: absolute;
+  z-index: 9;
 `;
