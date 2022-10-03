ca3805368c1fa9d219a87ac81d0e2f7297f83ded
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-10-03 16:45:41 -0300

    fix configure cmd test

--- cli/cmd/configure_cmd_test.go
+++ cli/cmd/configure_cmd_test.go
@@ -9,24 +9,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestConfigureCmd(t *testing.T) {
+func TestConfigureWithAnalytics(t *testing.T) {
 	cli := e2e.NewCLI()
 
-	configureCommand := cli.NewCommand("configure")
+	configureCommand := cli.NewCommand("configure", "--endpoint", "https://localhost:1234", "--analytics")
 
 	t.Cleanup(func() {
 		deleteFile("./config.yml")
 	})
 
-	output, err := configureCommand.Run("https://localhost:1234")
+	_, err := configureCommand.Run()
 	assert.NoError(t, err)
-	assert.NotEmpty(t, output)
 
 	tracetestConfig, err := config.LoadConfig("./config.yml")
 	require.NoError(t, err)
 
 	assert.Equal(t, "https", tracetestConfig.Scheme)
 	assert.Equal(t, "localhost:1234", tracetestConfig.Endpoint)
+	assert.True(t, tracetestConfig.AnalyticsEnabled)
 	assert.Nil(t, tracetestConfig.ServerPath)
+}
 
+func TestConfigureWithoutAnalytics(t *testing.T) {
+	cli := e2e.NewCLI()
+
+	configureCommand := cli.NewCommand("configure", "--endpoint", "https://localhost:1234", "--analytics=false")
+
+	t.Cleanup(func() {
+		deleteFile("./config.yml")
+	})
+
+	_, err := configureCommand.Run()
+	assert.NoError(t, err)
+
+	tracetestConfig, err := config.LoadConfig("./config.yml")
+	require.NoError(t, err)
+
+	assert.Equal(t, "https", tracetestConfig.Scheme)
+	assert.Equal(t, "localhost:1234", tracetestConfig.Endpoint)
+	assert.False(t, tracetestConfig.AnalyticsEnabled)
+	assert.Nil(t, tracetestConfig.ServerPath)
 }
