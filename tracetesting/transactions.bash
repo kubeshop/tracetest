bf50cbfcd8c7a50332dd0469fed7839e28d096bd
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-11-02 14:05:21 -0300

    add new tests

--- tracetesting/transactions.bash
+++ tracetesting/transactions.bash
@@ -13,10 +13,10 @@ echo "TEST_ID = $TEST_ID"
 
 test "transaction_create" ./definitions/transaction_create.yml || EXIT_STATUS=$?
 
-# export TRANSACTION_ID=$(tracetest_target test list -o json | jq -rc '.[0].id')
-# require_not_empty $TRANSACTION_ID "requires TRANSACTION_ID, got $TRANSACTION_ID " || exit $?
+export TRANSACTION_ID=$(tracetest_target_curl "/api/transactions" -X GET | jq -rc '.[0].id')
+require_not_empty $TRANSACTION_ID "requires TRANSACTION_ID, got $TRANSACTION_ID " || exit $?
 
-# test "transaction_list" ./definitions/transaction_list.yml || EXIT_STATUS=$?
-# test "transaction_delete" ./definitions/environment_delete.yml || EXIT_STATUS=$?
+test "transaction_list" ./definitions/transaction_list.yml || EXIT_STATUS=$?
+test "transaction_delete" ./definitions/transaction_delete.yml || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
