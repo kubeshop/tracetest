123143b30645a0111354931196ace78a98691cb6
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-11-16 17:29:47 -0300

    fix step duplication in transactions

--- server/testdb/transactions.go
+++ server/testdb/transactions.go
@@ -322,7 +322,7 @@ func (td *postgresDB) readTransactionRow(ctx context.Context, row scanner) (mode
 }
 
 func (td *postgresDB) getTransactionSteps(ctx context.Context, transaction model.Transaction) ([]model.Test, error) {
-	stmt, err := td.db.Prepare(getTestSQL + `INNER JOIN transaction_steps ts ON t.id = ts.test_id
+	stmt, err := td.db.Prepare(getTestSQL + testMaxVersionQuery + ` INNER JOIN transaction_steps ts ON t.id = ts.test_id
 	 WHERE ts.transaction_id = $1 AND ts.transaction_version = $2 ORDER BY ts.step_number ASC`)
 	if err != nil {
 		return []model.Test{}, fmt.Errorf("prepare: %w", err)
--- server/testdb/transactions_test.go
+++ server/testdb/transactions_test.go
@@ -239,3 +239,43 @@ func TestGetTransactionsWithMultipleVersions(t *testing.T) {
 		assert.Equal(t, 2, test.Version)
 	}
 }
+
+func TestGetTransactionsStepsWithMultipleVersions(t *testing.T) {
+	db, clean := getDB()
+	defer clean()
+
+	ctx := context.TODO()
+
+	transaction := createTransaction(t, db)
+
+	test1 := createTestWithName(t, db, "first step")
+	test2 := createTestWithName(t, db, "second step")
+
+	transaction.Steps = []model.Test{
+		test1,
+		test2,
+	}
+
+	_, err := db.UpdateTransaction(context.TODO(), transaction)
+	require.NoError(t, err)
+
+	updatedTransaction, err := db.GetLatestTransactionVersion(ctx, transaction.ID)
+	require.NoError(t, err)
+
+	assert.Len(t, updatedTransaction.Steps, 2)
+
+	test1.Name = "new test name"
+	updatedTest, err := db.UpdateTest(ctx, test1)
+	require.NoError(t, err)
+
+	updatedTransaction, err = db.GetLatestTransactionVersion(ctx, transaction.ID)
+	require.NoError(t, err)
+
+	assert.Len(t, updatedTransaction.Steps, 2)
+
+	assert.Equal(t, updatedTransaction.Steps[0].ID, updatedTest.ID)
+	assert.Equal(t, updatedTransaction.Steps[0].Version, updatedTest.Version)
+
+	assert.Equal(t, updatedTransaction.Steps[1].ID, test2.ID)
+	assert.Equal(t, updatedTransaction.Steps[1].Version, test2.Version)
+}
