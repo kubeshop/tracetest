ca25d379bdbdb02eb33cb97fdef4d94c87f2798c
Adnan Rahic <ado.raha198@gmail.com>
2022-12-02 15:25:16 +0100

    docs(search): fix algolia config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Tracetest Docs',
   tagline: 'Trace-based testing',
-  url: 'https://tracetest.io',
+  url: 'https://docs.tracetest.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
