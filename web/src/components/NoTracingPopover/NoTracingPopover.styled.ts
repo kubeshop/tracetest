54eff9b8d3ed0836dc29a59e8f9036e1bf472be2
Oscar Reyes <oscar-rreyes1@hotmail.com>
2022-12-13 10:15:37 -0600

    feature(frontend): adding no tracing mode warning (#1677)
    * feature(frontend): adding no tracing mode warning
    
    * feature(frontend): adding no tracing mode warning
    
    * feature(frontend): adding no tracing mode warning

---
+++ web/src/components/NoTracingPopover/NoTracingPopover.styled.ts
@@ -0,0 +1,66 @@
+import {SettingOutlined} from '@ant-design/icons';
+import {Button, Typography} from 'antd';
+import styled from 'styled-components';
+
+export const Icon = styled(SettingOutlined)`
+  && {
+    cursor: pointer;
+    font-size: ${({theme}) => theme.size.lg};
+`;
+
+export const WarningButton = styled(Button)`
+  background: ${({theme}) => theme.color.warningYellow};
+  width: 57px;
+  height: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 !important;
+  border: none;
+  font-weight: 600;
+
+  &:hover,
+  &:focus {
+    background: ${({theme}) => theme.color.warningYellow};
+  }
+`;
+
+export const Title = styled(Typography.Title)`
+  && {
+    font-size: ${({theme}) => theme.size.lg};
+    margin: 0;
+  }
+`;
+
+export const Text = styled(Typography.Text)`
+  && {
+    font-size: ${({theme}) => theme.size.md};
+    margin: 0;
+  }
+`;
+
+export const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  max-width: 340px;
+`;
+
+export const ButtonContainer = styled.div`
+  justify-content: flex-end;
+  display: flex;
+  margin-top: 6px;
+`;
+
+export const Trigger = styled.div`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  cursor: pointer;
+  margin-right: 24px;
+  border-radius: 12px;
+  padding: 4px 7px;
+  background: ${({theme}) => theme.color.backgroundDark};
+  height: 24px;
+`;
---
+++ web/src/components/NoTracingPopover/NoTracingPopover.tsx
@@ -0,0 +1,36 @@
+import {Button, Popover} from 'antd';
+import {useMemo} from 'react';
+import {Link} from 'react-router-dom';
+import * as S from './NoTracingPopover.styled';
+
+const NoTracingPopover = () => {
+  const content = useMemo(
+    () => (
+      <S.MessageContainer>
+        <div>
+          <S.Title>Tracing is not configured.</S.Title>
+          <S.Text>
+            Tracetest needs configuration information about your backend trace data store so it can collect the trace
+            after a test run. Please configure your data store now.
+          </S.Text>
+        </div>
+        <S.ButtonContainer>
+          <Link to="/settings">
+            <Button type="primary">Configure</Button>
+          </Link>
+        </S.ButtonContainer>
+      </S.MessageContainer>
+    ),
+    []
+  );
+
+  return (
+    <Popover content={content} overlayClassName="no-tracing-popover" placement="bottom">
+      <S.Trigger>
+        <S.Icon /> No-Tracing Mode
+      </S.Trigger>
+    </Popover>
+  );
+};
+
+export default NoTracingPopover;
