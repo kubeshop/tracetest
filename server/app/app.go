34f94389f427030c485b342242a77412749ce2f0
Sebastian Choren <schoren@users.noreply.github.com>
2022-12-16 12:48:49 -0300

    chrore(server): refactor app for readability (#1698)

--- server/app/app.go
+++ server/app/app.go
@@ -3,14 +3,9 @@ package app
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
-	"regexp"
-	"strings"
-	"text/template"
 
 	"github.com/gorilla/handlers"
 	"github.com/kubeshop/tracetest/server/analytics"
@@ -21,6 +16,7 @@ import (
 	httpServer "github.com/kubeshop/tracetest/server/http"
 	"github.com/kubeshop/tracetest/server/http/mappings"
 	"github.com/kubeshop/tracetest/server/http/websocket"
+	"github.com/kubeshop/tracetest/server/model"
 	"github.com/kubeshop/tracetest/server/openapi"
 	"github.com/kubeshop/tracetest/server/otlp"
 	"github.com/kubeshop/tracetest/server/subscription"
@@ -28,6 +24,7 @@ import (
 	"github.com/kubeshop/tracetest/server/tracedb"
 	"github.com/kubeshop/tracetest/server/traces"
 	"github.com/kubeshop/tracetest/server/tracing"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
@@ -56,44 +53,6 @@ func New(config Config, db *sql.DB) (*App, error) {
 	}
 
 	return app, nil
-}
-
-func jsonEscape(text any) string {
-	initial, err := json.Marshal(text)
-	if err != nil {
-		panic(err)
-	}
-
-	encoded, err := json.Marshal(string(initial))
-	if err != nil {
-		panic(err)
-	}
-
-	formatted := string(encoded)
-	return strings.Trim(formatted, `"`)
-}
-
-func spaHandler(prefix, staticPath, indexPath string, tplVars map[string]string) http.HandlerFunc {
-	var fileMatcher = regexp.MustCompile(`\.[a-zA-Z]*$`)
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		if !fileMatcher.MatchString(r.URL.Path) {
-			tpl, err := template.ParseFiles(filepath.Join(staticPath, indexPath))
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-
-			if err = tpl.Execute(w, tplVars); err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-
-		} else {
-			http.FileServer(http.Dir(staticPath)).ServeHTTP(w, r)
-		}
-	}
-
-	return http.StripPrefix(prefix, http.HandlerFunc(handler)).ServeHTTP
 }
 
 func (a *App) Stop() {
@@ -143,7 +102,6 @@ func (a *App) Start() error {
 	})
 
 	serverID, isNewInstall, err := testDB.ServerID()
-
 	if err != nil {
 		return err
 	}
@@ -165,90 +123,40 @@ func (a *App) Start() error {
 		return fmt.Errorf("could not create trigger span tracer: %w", err)
 	}
 
-	triggerReg := trigger.NewRegsitry(tracer, applicationTracer)
-	triggerReg.Add(trigger.HTTP())
-	triggerReg.Add(trigger.GRPC())
-
 	subscriptionManager := subscription.NewManager()
 
-	execTestUpdater := (executor.CompositeUpdater{}).
-		Add(executor.NewDBUpdater(testDB)).
-		Add(executor.NewSubscriptionUpdater(subscriptionManager))
-
-	assertionExecutor := executor.NewAssertionExecutor(tracer)
-	outputProcesser := executor.InstrumentedOutputProcessor(tracer)
-	assertionRunner := executor.NewAssertionRunner(execTestUpdater, assertionExecutor, outputProcesser, subscriptionManager)
-	assertionRunner.Start(5)
-	a.registerStopFn(func() {
-		fmt.Println("stopping assertionRunner")
-		assertionRunner.Stop()
-	})
-
-	traceConversionConfig := traces.NewConversionConfig()
-	// hardcoded for now. In the future we will get those values from the database
-	traceConversionConfig.AddTimeFields(
-		"tracetest.span.duration",
+	rf := newRunnerFacades(
+		a.config.Config,
+		testDB,
+		traceDB,
+		applicationTracer,
+		tracer,
+		subscriptionManager,
 	)
 
-	pollerExecutor := executor.NewPollerExecutor(a.config.Config, tracer, execTestUpdater, traceDB)
+	// worker count. should be configurable
+	rf.tracePoller.Start(5)
+	rf.runner.Start(5)
+	rf.runner.Start(5)
+	rf.transactionRunner.Start(5)
+	rf.assertionRunner.Start(5)
 
-	tracePoller := executor.NewTracePoller(pollerExecutor, execTestUpdater, assertionRunner, a.config.PoolingRetryDelay(), a.config.MaxWaitTimeForTraceDuration(), subscriptionManager)
-	tracePoller.Start(5) // worker count. should be configurable
 	a.registerStopFn(func() {
 		fmt.Println("stopping tracePoller")
-		tracePoller.Stop()
+		rf.tracePoller.Stop()
 	})
-
-	runner := executor.NewPersistentRunner(triggerReg, testDB, execTestUpdater, tracePoller, tracer, subscriptionManager)
-	runner.Start(5) // worker count. should be configurable
 	a.registerStopFn(func() {
 		fmt.Println("stopping runner")
-		runner.Stop()
+		rf.runner.Stop()
 	})
-
-	transactionRunner := executor.NewTransactionRunner(runner, testDB, subscriptionManager)
-	transactionRunner.Start(5)
 	a.registerStopFn(func() {
 		fmt.Println("stopping transactionRunner")
-		transactionRunner.Stop()
+		rf.transactionRunner.Stop()
 	})
-
-	mappers := mappings.New(traceConversionConfig, comparator.DefaultRegistry(), testDB)
-
-	controller := httpServer.NewController(testDB, runner, transactionRunner, assertionRunner, mappers)
-	apiApiController := openapi.NewApiApiController(controller)
-	customController := httpServer.NewCustomController(controller, apiApiController, openapi.DefaultErrorHandler, tracer)
-	httpRouter := customController
-	if a.config.Server.PathPrefix != "" {
-		httpRouter = httpServer.NewPrefixedRouter(httpRouter, a.config.Server.PathPrefix)
-	}
-
-	router := openapi.NewRouter(httpRouter)
-
-	wsRouter := websocket.NewRouter()
-	wsRouter.Add("subscribe", websocket.NewSubscribeCommandExecutor(subscriptionManager, mappers))
-	wsRouter.Add("unsubscribe", websocket.NewUnsubscribeCommandExecutor(subscriptionManager))
-
-	router.Handle("/ws", wsRouter.Handler())
-
-	router.PathPrefix(a.config.Server.PathPrefix).Handler(
-		spaHandler(
-			a.config.Server.PathPrefix,
-			"./html",
-			"index.html",
-			map[string]string{
-				"AnalyticsKey":         analytics.FrontendKey,
-				"AnalyticsEnabled":     fmt.Sprintf("%t", a.config.GA.Enabled),
-				"ServerPathPrefix":     fmt.Sprintf("%s/", a.config.Server.PathPrefix),
-				"ServerID":             serverID,
-				"AppVersion":           Version,
-				"Env":                  Env,
-				"DemoEnabled":          jsonEscape(a.config.Demo.Enabled),
-				"DemoEndpoints":        jsonEscape(a.config.Demo.Endpoints),
-				"ExperimentalFeatures": jsonEscape(a.config.ExperimentalFeatures),
-			},
-		),
-	)
+	a.registerStopFn(func() {
+		fmt.Println("stopping assertionRunner")
+		rf.assertionRunner.Stop()
+	})
 
 	err = analytics.SendEvent("Server Started", "beacon", "")
 	if err != nil {
@@ -262,22 +170,150 @@ func (a *App) Start() error {
 		otlpServer.Stop()
 	})
 
+	httpServer := newHttpServer(
+		serverID,
+		a.config.Config,
+		testDB,
+		tracer,
+		subscriptionManager,
+		rf,
+	)
+	a.registerStopFn(func() {
+		fmt.Println("stopping http server")
+		httpServer.Shutdown(ctx)
+	})
+
+	go httpServer.ListenAndServe()
+	log.Printf("HTTP Server started")
+
+	return nil
+}
+
+func newRunnerFacades(
+	conf config.Config,
+	testDB model.Repository,
+	traceDB tracedb.TraceDB,
+	appTracer trace.Tracer,
+	tracer trace.Tracer,
+	subscriptionManager *subscription.Manager,
+) *runnerFacade {
+
+	execTestUpdater := (executor.CompositeUpdater{}).
+		Add(executor.NewDBUpdater(testDB)).
+		Add(executor.NewSubscriptionUpdater(subscriptionManager))
+
+	assertionRunner := executor.NewAssertionRunner(
+		execTestUpdater,
+		executor.NewAssertionExecutor(tracer),
+		executor.InstrumentedOutputProcessor(tracer),
+		subscriptionManager,
+	)
+
+	pollerExecutor := executor.NewPollerExecutor(conf, tracer, execTestUpdater, traceDB)
+
+	tracePoller := executor.NewTracePoller(
+		pollerExecutor,
+		execTestUpdater,
+		assertionRunner,
+		conf.PoolingRetryDelay(),
+		conf.MaxWaitTimeForTraceDuration(),
+		subscriptionManager,
+	)
+
+	runner := executor.NewPersistentRunner(
+		triggerRegistry(tracer, appTracer),
+		testDB,
+		execTestUpdater,
+		tracePoller,
+		tracer,
+		subscriptionManager,
+	)
+
+	transactionRunner := executor.NewTransactionRunner(runner, testDB, subscriptionManager)
+
+	return &runnerFacade{
+		runner:            runner,
+		transactionRunner: transactionRunner,
+		assertionRunner:   assertionRunner,
+		tracePoller:       tracePoller,
+	}
+}
+
+func triggerRegistry(tracer, appTracer trace.Tracer) *trigger.Registry {
+	triggerReg := trigger.NewRegsitry(tracer, appTracer)
+	triggerReg.Add(trigger.HTTP())
+	triggerReg.Add(trigger.GRPC())
+
+	return triggerReg
+}
+
+func newHttpServer(
+	serverID string,
+	conf config.Config,
+	testDB model.Repository,
+	tracer trace.Tracer,
+	subscriptionManager *subscription.Manager,
+	rf *runnerFacade,
+) *http.Server {
+	mappers := mappings.New(tracesConversionConfig(), comparator.DefaultRegistry(), testDB)
+
+	router := openapi.NewRouter(httpRouter(conf, testDB, tracer, rf, mappers))
+
+	wsRouter := websocket.NewRouter()
+	wsRouter.Add("subscribe", websocket.NewSubscribeCommandExecutor(subscriptionManager, mappers))
+	wsRouter.Add("unsubscribe", websocket.NewUnsubscribeCommandExecutor(subscriptionManager))
+
+	router.Handle("/ws", wsRouter.Handler())
+
+	router.
+		PathPrefix(conf.Server.PathPrefix).
+		Handler(
+			httpServer.SPAHandler(
+				conf,
+				serverID,
+				Version,
+				Env,
+			),
+		)
+
 	port := 11633
-	if a.config.Server.HttpPort != 0 {
-		port = a.config.Server.HttpPort
+	if conf.Server.HttpPort != 0 {
+		port = conf.Server.HttpPort
 	}
 
 	httpServer := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
 		Handler: handlers.CompressHandler(router),
 	}
-	a.registerStopFn(func() {
-		fmt.Println("stopping http server")
-		httpServer.Shutdown(ctx)
-	})
 
-	log.Printf("HTTP Server started")
-	go httpServer.ListenAndServe()
+	return httpServer
+}
 
-	return nil
+func httpRouter(
+	conf config.Config,
+	testDB model.Repository,
+	tracer trace.Tracer,
+	rf *runnerFacade,
+	mappers mappings.Mappings,
+) openapi.Router {
+	controller := httpServer.NewController(testDB, rf, mappers)
+	apiApiController := openapi.NewApiApiController(controller)
+	customController := httpServer.NewCustomController(controller, apiApiController, openapi.DefaultErrorHandler, tracer)
+	httpRouter := customController
+
+	if conf.Server.PathPrefix != "" {
+		httpRouter = httpServer.NewPrefixedRouter(httpRouter, conf.Server.PathPrefix)
+	}
+
+	return httpRouter
+}
+
+func tracesConversionConfig() traces.ConversionConfig {
+	tcc := traces.NewConversionConfig()
+	// hardcoded for now. In the future we will get those values from the database
+	tcc.AddTimeFields(
+		"tracetest.span.duration",
+	)
+
+	return tcc
 }
