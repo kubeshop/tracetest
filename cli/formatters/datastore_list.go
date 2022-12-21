40e21d2a936befaa0c67ff279bf5b34440d78519
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-12-21 17:44:42 -0300

    feat: data store list

---
+++ cli/formatters/datastore_list.go
@@ -0,0 +1,76 @@
+package formatters
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/alexeyco/simpletable"
+	"github.com/kubeshop/tracetest/cli/config"
+	"github.com/kubeshop/tracetest/cli/openapi"
+)
+
+type dataStoreList struct {
+	config config.Config
+}
+
+func DataStoreList(config config.Config) dataStoreList {
+	return dataStoreList{
+		config: config,
+	}
+}
+
+func (f dataStoreList) Format(dataStores []openapi.DataStore) string {
+	switch CurrentOutput {
+	case Pretty:
+		return f.pretty(dataStores)
+	case JSON:
+		return f.json(dataStores)
+	}
+
+	return ""
+}
+
+func (f dataStoreList) json(dataStores []openapi.DataStore) string {
+	bytes, err := json.Marshal(dataStores)
+	if err != nil {
+		panic(fmt.Errorf("could not marshal output json: %w", err))
+	}
+
+	return string(bytes)
+}
+
+func (f dataStoreList) pretty(dataStores []openapi.DataStore) string {
+	if len(dataStores) == 0 {
+		return "No tests"
+	}
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Text: "ID"},
+			{Text: "NAME"},
+			{Text: "DEFAULT"},
+		},
+	}
+
+	for _, t := range dataStores {
+		table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+			{Text: *t.Id},
+			{Text: *t.Name},
+			{Text: f.getDefaultMark(t)},
+		})
+	}
+
+	table.SetStyle(simpletable.StyleCompactLite)
+
+	return table.String()
+}
+
+func (f dataStoreList) getDefaultMark(dataStore openapi.DataStore) string {
+	if dataStore.IsDefault != nil && *dataStore.IsDefault {
+		return "*"
+	}
+
+	return ""
+}
