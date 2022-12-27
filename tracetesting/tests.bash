7da032d669d653c006968d0850bea9bfba74eddc
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2022-12-27 14:02:57 -0500

    revert test

--- tracetesting/tests.bash
+++ tracetesting/tests.bash
@@ -26,8 +26,8 @@ test "test_run" ./definitions/test_run.yml || EXIT_STATUS=$?
 export RUN_ID=$(tracetest_target_curl "/api/tests/$TEST_ID/run?take=1&skip=0" | jq -rc '.[0].id')
 require_not_empty $RUN_ID "requires RUN_ID, got $RUN_ID " || exit $?
 
-# test "test_rerun" ./definitions/test_rerun.yml || EXIT_STATUS=$?
-# test "test_run_delete" ./definitions/test_run_delete.yml || EXIT_STATUS=$?
-# test "test_delete" ./definitions/test_delete.yml || EXIT_STATUS=$?
+test "test_rerun" ./definitions/test_rerun.yml || EXIT_STATUS=$?
+test "test_run_delete" ./definitions/test_run_delete.yml || EXIT_STATUS=$?
+test "test_delete" ./definitions/test_delete.yml || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
