3e071a9dbb3593fa910ffdcccae52e68c1b076cb
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2022-12-28 10:44:29 -0500

    fix(frontend): improve data store connection message (#1747)

--- web/src/components/TestConnectionNotification/TestConnectionNotification.styled.ts
+++ web/src/components/TestConnectionNotification/TestConnectionNotification.styled.ts
@@ -1,17 +1,28 @@
 import {CheckCircleFilled, MinusCircleFilled} from '@ant-design/icons';
 import styled from 'styled-components';
+import {Typography} from 'antd';
 
 export const SuccessCheckIcon = styled(CheckCircleFilled)`
   color: ${({theme}) => theme.color.success};
-  margin-top: 5px;
+  margin-top: 3px;
 `;
+
 export const FailedCheckIcon = styled(MinusCircleFilled)`
   color: ${({theme}) => theme.color.error};
-  margin-top: 5px;
+  margin-top: 3px;
 `;
+
 export const Container = styled.div``;
+
 export const StepContainer = styled.div`
+  align-items: start;
   display: flex;
   gap: 6px;
-  align-items: start;
+  margin-bottom: 8px;
+`;
+
+export const Title = styled(Typography.Title)`
+  && {
+    margin: 0;
+  }
 `;
