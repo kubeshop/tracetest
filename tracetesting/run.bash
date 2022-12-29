aee2a7b5fb1890faa13017f725444a862ea2b1b9
Daniel Dias <danielbpdias@gmail.com>
2022-12-29 14:10:11 -0300

    Testing script change against Github Actions

--- tracetesting/run.bash
+++ tracetesting/run.bash
@@ -2,12 +2,6 @@
 
 set -e
 
-export TRACETEST_CLI_TARGET=${TRACETEST_CLI_TARGET:-"tracetest"}
-if ! command -v "$TRACETEST_CLI_TARGET" &> /dev/null; then
-  echo "\$TRACETEST_CLI_TARGET not set to executable. set to $TRACETEST_CLI_TARGET";
-  exit 2
-fi
-
 export TRACETEST_CLI_MAIN=${TRACETEST_CLI_MAIN:-"${TRACETEST_CLI_TARGET}"}
 if ! command -v "$TRACETEST_CLI_MAIN" &> /dev/null; then
   echo "\$TRACETEST_CLI_MAIN not set to executable. set to $TRACETEST_CLI_MAIN";
@@ -20,18 +14,13 @@ if [  "$TARGET_URL" = "" ]; then
   exit 2
 fi
 
-
 export TRACETEST_MAIN_ENDPOINT=${TRACETEST_MAIN_ENDPOINT:-"localhost:11633"}
-export TRACETEST_TARGET_ENDPOINT=${TRACETEST_TARGET_ENDPOINT:-"localhost:11634"}
 export DEMO_APP_URL=${DEMO_APP_URL-"http://demo-pokemon-api.demo"}
 export DEMO_APP_GRPC_URL=${DEMO_APP_GRPC_URL-"demo-pokemon-api.demo:8082"}
 
-
 echo "TRACETEST_CLI_MAIN: $TRACETEST_CLI_MAIN"
-echo "TRACETEST_CLI_TARGET: $TRACETEST_CLI_TARGET"
 echo "TARGET_URL: $TARGET_URL"
 echo "TRACETEST_MAIN_ENDPOINT: $TRACETEST_MAIN_ENDPOINT"
-echo "TRACETEST_TARGET_ENDPOINT: $TRACETEST_TARGET_ENDPOINT"
 echo "DEMO_APP_URL: $DEMO_APP_URL"
 echo "DEMO_APP_GRPC_URL: $DEMO_APP_GRPC_URL"
 
@@ -41,22 +30,22 @@ endpoint: $TRACETEST_MAIN_ENDPOINT
 analyticsEnabled: false
 EOF
 
-cat << EOF > config.target.yml
-scheme: http
-endpoint: $TRACETEST_TARGET_ENDPOINT
-analyticsEnabled: false
-EOF
-
 mkdir -p results/responses
 
 EXIT_STATUS=0
+
+test() {
+  name=$1
+  definition=$2
+  $TRACETEST_CLI_MAIN --config ./config.main.yml test run --definition $definition --wait-for-result --junit results/$name.xml
+  return $?
+}
+
+test transaction_test_suite ./features/transaction/_test_suite.yml
 
 # bash ./tests.bash || EXIT_STATUS=$?
 # bash ./grpc.bash || EXIT_STATUS=$?
 # bash ./environments.bash || EXIT_STATUS=$?
 # bash ./transactions.bash || EXIT_STATUS=$?
-
-source ./funcs.bash
-test "transaction_test_suite" ./features/transaction/_test_suite.yml || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
