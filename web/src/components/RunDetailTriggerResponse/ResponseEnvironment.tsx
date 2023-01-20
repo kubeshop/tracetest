cfd0048bec548b8dce25958c563be9bb6b69bfee
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-01-20 12:32:47 -0500

    feat(frontend): add empty state in environment tab (#1861)

--- web/src/components/RunDetailTriggerResponse/ResponseEnvironment.tsx
+++ web/src/components/RunDetailTriggerResponse/ResponseEnvironment.tsx
@@ -1,4 +1,5 @@
 import KeyValueRow from 'components/KeyValueRow';
+import {ENVIRONMENTS_DOCUMENTATION_URL} from 'constants/Common.constants';
 import {useTestRun} from 'providers/TestRun/TestRun.provider';
 import * as S from './RunDetailTriggerResponse.styled';
 
@@ -6,6 +7,21 @@ const ResponseEnvironment = () => {
   const {
     run: {environment},
   } = useTestRun();
+
+  if (!environment?.values?.length) {
+    return (
+      <S.EmptyContainer>
+        <S.EmptyIcon />
+        <S.EmptyTitle>There are no environment variables used in this test</S.EmptyTitle>
+        <S.EmptyText>
+          Learn more about environments{' '}
+          <a href={ENVIRONMENTS_DOCUMENTATION_URL} target="_blank">
+            here
+          </a>
+        </S.EmptyText>
+      </S.EmptyContainer>
+    );
+  }
 
   return (
     <S.ResponseEnvironmentContainer>
