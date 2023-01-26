3318a4a9c0663ba2373b2d04ebb4dd2aedb2d735
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-01-26 16:12:12 -0500

    fix(frontend): fix custom syntax linter in json editor

--- web/src/components/CreateTestPlugins/Rest/steps/RequestDetails/BodyField/useLanguageExtensionsMemo.tsx
+++ web/src/components/CreateTestPlugins/Rest/steps/RequestDetails/BodyField/useLanguageExtensionsMemo.tsx
@@ -1,4 +1,4 @@
-import {json, jsonLanguage, jsonParseLinter} from '@codemirror/lang-json';
+import {json, jsonLanguage} from '@codemirror/lang-json';
 import {Extension} from '@codemirror/state';
 import {xml, xmlLanguage} from '@codemirror/lang-xml';
 import {LanguageSupport} from '@codemirror/language';
@@ -6,6 +6,7 @@ import {Diagnostic, linter, lintGutter} from '@codemirror/lint';
 import {EditorView} from '@codemirror/view';
 import {XMLValidator} from 'fast-xml-parser';
 import {useMemo} from 'react';
+import {jsonParseLinter} from './jsonLint';
 import {BodyMode} from './useBodyMode';
 
 export function useLanguageExtensionsMemo(bodyMode: BodyMode): Extension[] {
