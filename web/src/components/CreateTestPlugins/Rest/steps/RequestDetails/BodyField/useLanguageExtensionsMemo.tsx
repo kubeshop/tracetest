3169bfdc80b8cef166378ee2f43285b2deb36c14
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-01-27 12:41:03 -0500

    fix(frontend): fix custom syntax linter in json editor (#1900)
    * fix(frontend): fix custom syntax linter in json editor
    
    * move json linter to Editor.service

--- web/src/components/CreateTestPlugins/Rest/steps/RequestDetails/BodyField/useLanguageExtensionsMemo.tsx
+++ web/src/components/CreateTestPlugins/Rest/steps/RequestDetails/BodyField/useLanguageExtensionsMemo.tsx
@@ -1,4 +1,4 @@
-import {json, jsonLanguage, jsonParseLinter} from '@codemirror/lang-json';
+import {json, jsonLanguage} from '@codemirror/lang-json';
 import {Extension} from '@codemirror/state';
 import {xml, xmlLanguage} from '@codemirror/lang-xml';
 import {LanguageSupport} from '@codemirror/language';
@@ -6,6 +6,7 @@ import {Diagnostic, linter, lintGutter} from '@codemirror/lint';
 import {EditorView} from '@codemirror/view';
 import {XMLValidator} from 'fast-xml-parser';
 import {useMemo} from 'react';
+import EditorService from 'services/Editor.service';
 import {BodyMode} from './useBodyMode';
 
 export function useLanguageExtensionsMemo(bodyMode: BodyMode): Extension[] {
@@ -34,7 +35,7 @@ export function useLanguageExtensionsMemo(bodyMode: BodyMode): Extension[] {
       case 'json':
         return [
           json(),
-          linter((view: EditorView): Diagnostic[] => jsonParseLinter()(view)),
+          linter((view: EditorView): Diagnostic[] => EditorService.jsonParseLinter()(view)),
           new LanguageSupport(jsonLanguage),
           lintGutter({}),
         ];
