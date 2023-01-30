d19e0838d075fe1eb3ad4bfd03404cc4276e2ee6
Daniel Dias <danielbpdias@gmail.com>
2023-01-30 19:24:16 -0300

    Removing unused redirect that was breaking docusaurus build

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -136,10 +136,6 @@ const config = {
             to: '/ci-cd-automation/github-actions-pipeline',
             from: ['/ci-cd-automation/ci-cd-best-practices' /*, '/docs/legacyDocFrom2016'*/],
           },
-          {
-            to: '/live-examples/pokeshop',
-            from: ['/pokeshop', '/pokeshop/' /*, '/docs/legacyDocFrom2016'*/],
-          },
         ],
         // createRedirects(existingPath) {
         //   if (existingPath.includes('/community')) {
