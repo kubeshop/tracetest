1f07b2d981fae270292949e2957b759fdad998aa
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-02-03 16:46:01 -0300

    add CLI reference to docs

---
+++ cli/cmd/docgen_cmd.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+var docsOutputDir string
+
+var docGenCmd = &cobra.Command{
+	Use:    "docgen",
+	Short:  "Generate the CLI documentation",
+	Long:   "Generate the CLI documentation",
+	PreRun: setupCommand(),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := os.MkdirAll(docsOutputDir, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		err = doc.GenMarkdownTreeCustom(rootCmd, docsOutputDir, func(s string) string {
+			return "# CLI Referece\n"
+		}, func(s string) string { return s })
+		if err != nil {
+			panic(err)
+		}
+	},
+	PostRun: teardownCommand,
+}
+
+func init() {
+	docGenCmd.PersistentFlags().StringVarP(&docsOutputDir, "output", "o", "", "tracetest docgen -o my/docs/dir")
+	rootCmd.AddCommand(docGenCmd)
+}
--- cli/cmd/test_export_cmd.go
+++ cli/cmd/test_export_cmd.go
@@ -17,8 +17,8 @@ var (
 
 var testExportCmd = &cobra.Command{
 	Use:    "export",
-	Short:  "export a test",
-	Long:   "export a test",
+	Short:  "exports a test into a file",
+	Long:   "exports a test into a file",
 	PreRun: setupCommand(),
 	Run: func(cmd *cobra.Command, args []string) {
 		analytics.Track("Test Export", "cmd", map[string]string{})
