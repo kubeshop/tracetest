9a11a7e1ad27aaf9138b18e69a93986a912c12e4
Sebastian Choren <sebastian.choren@gmail.com>
2023-02-09 13:07:31 -0300

    fixed demo endpoint casing

--- server/config/demo.go
+++ server/config/demo.go
@@ -1,5 +1,7 @@
 package config
 
+import "strings"
+
 func (c *Config) DemoEnabled() []string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -11,5 +13,11 @@ func (c *Config) DemoEndpoints() map[string]string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	return c.config.Demo.Endpoints
+	fixed := map[string]string{}
+	for k, v := range c.config.Demo.Endpoints {
+		// uppercase first letter
+		fixed[strings.ToUpper(k[:1])+k[1:]] = v
+	}
+
+	return fixed
 }
