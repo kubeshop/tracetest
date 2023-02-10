e630f75f97ddae1b32145548815a2affd56ddebb
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-02-10 16:22:59 -0300

    move reference to reference folder and set it as last item in list

--- docs/docs/cli/reference/cli-sidebar.js
+++ docs/docs/cli/reference/cli-sidebar.js
@@ -5,107 +5,107 @@ const pages = [
     {
         type: "doc",
 		label: "tracetest",
-		id: "cli/generated/tracetest"
+		id: "cli/reference/tracetest"
 	},
     {
         type: "doc",
 		label: "tracetest completion",
-		id: "cli/generated/tracetest_completion"
+		id: "cli/reference/tracetest_completion"
 	},
     {
         type: "doc",
 		label: "tracetest completion bash",
-		id: "cli/generated/tracetest_completion_bash"
+		id: "cli/reference/tracetest_completion_bash"
 	},
     {
         type: "doc",
 		label: "tracetest completion fish",
-		id: "cli/generated/tracetest_completion_fish"
+		id: "cli/reference/tracetest_completion_fish"
 	},
     {
         type: "doc",
 		label: "tracetest completion powershell",
-		id: "cli/generated/tracetest_completion_powershell"
+		id: "cli/reference/tracetest_completion_powershell"
 	},
     {
         type: "doc",
 		label: "tracetest completion zsh",
-		id: "cli/generated/tracetest_completion_zsh"
+		id: "cli/reference/tracetest_completion_zsh"
 	},
     {
         type: "doc",
 		label: "tracetest configure",
-		id: "cli/generated/tracetest_configure"
+		id: "cli/reference/tracetest_configure"
 	},
     {
         type: "doc",
 		label: "tracetest datastore",
-		id: "cli/generated/tracetest_datastore"
+		id: "cli/reference/tracetest_datastore"
 	},
     {
         type: "doc",
 		label: "tracetest datastore apply",
-		id: "cli/generated/tracetest_datastore_apply"
+		id: "cli/reference/tracetest_datastore_apply"
 	},
     {
         type: "doc",
 		label: "tracetest datastore export",
-		id: "cli/generated/tracetest_datastore_export"
+		id: "cli/reference/tracetest_datastore_export"
 	},
     {
         type: "doc",
 		label: "tracetest datastore list",
-		id: "cli/generated/tracetest_datastore_list"
+		id: "cli/reference/tracetest_datastore_list"
 	},
     {
         type: "doc",
 		label: "tracetest docgen",
-		id: "cli/generated/tracetest_docgen"
+		id: "cli/reference/tracetest_docgen"
 	},
     {
         type: "doc",
 		label: "tracetest environment",
-		id: "cli/generated/tracetest_environment"
+		id: "cli/reference/tracetest_environment"
 	},
     {
         type: "doc",
 		label: "tracetest environment apply",
-		id: "cli/generated/tracetest_environment_apply"
+		id: "cli/reference/tracetest_environment_apply"
 	},
     {
         type: "doc",
 		label: "tracetest server",
-		id: "cli/generated/tracetest_server"
+		id: "cli/reference/tracetest_server"
 	},
     {
         type: "doc",
 		label: "tracetest server install",
-		id: "cli/generated/tracetest_server_install"
+		id: "cli/reference/tracetest_server_install"
 	},
     {
         type: "doc",
 		label: "tracetest test",
-		id: "cli/generated/tracetest_test"
+		id: "cli/reference/tracetest_test"
 	},
     {
         type: "doc",
 		label: "tracetest test export",
-		id: "cli/generated/tracetest_test_export"
+		id: "cli/reference/tracetest_test_export"
 	},
     {
         type: "doc",
 		label: "tracetest test list",
-		id: "cli/generated/tracetest_test_list"
+		id: "cli/reference/tracetest_test_list"
 	},
     {
         type: "doc",
 		label: "tracetest test run",
-		id: "cli/generated/tracetest_test_run"
+		id: "cli/reference/tracetest_test_run"
 	},
     {
         type: "doc",
 		label: "tracetest version",
-		id: "cli/generated/tracetest_version"
+		id: "cli/reference/tracetest_version"
 	},
 ]
 
