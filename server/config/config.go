dc185310f25a2184bcab30d1621aabc4b5408e8d
Sebastian Choren <sebastian.choren@gmail.com>
2023-02-10 11:30:34 -0300

    move server configs to viper

--- server/config/config.go
+++ server/config/config.go
@@ -3,16 +3,26 @@ package config
 import (
 	"os"
 	"sync"
+
+	"github.com/spf13/viper"
 )
 
 type Config struct {
 	config *config
+	vp     *viper.Viper
 	mu     sync.Mutex
 }
 
+var defaultSetters []func(*viper.Viper)
+
 func New() *Config {
+	vp := viper.New()
+	for _, ds := range defaultSetters {
+		ds(vp)
+	}
 	return &Config{
 		config: &config{},
+		vp:     vp,
 	}
 }
 
--- server/config/server.go
+++ server/config/server.go
@@ -1,47 +1,97 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	defaultSetters = append(defaultSetters, serverDefaultSetter)
+}
+
+func serverDefaultSetter(vp *viper.Viper) {
+	vp.SetDefault("postgres.host", "postgres")
+	vp.SetDefault("postgres.username", "postgres")
+	vp.SetDefault("postgres.password", "postgres")
+	vp.SetDefault("postgres.dbname", "tracetest")
+	vp.SetDefault("postgres.port", 5432)
+	vp.SetDefault("postgres.params", "")
+
+	vp.SetDefault("server.port", 11633)
+	vp.SetDefault("server.pathPrefix", "/")
+
+	vp.SetDefault("experimentalFeatures", []string{})
+
+	vp.SetDefault("internalTelemetry.enabled", false)
+	vp.SetDefault("internalTelemetry.otelCollectorAddress", "")
+}
+
 func (c *Config) PostgresConnString() string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	return c.config.PostgresConnString
+	str := fmt.Sprintf(
+		"host=%s user=%s password=%s port=%d dbname=%s",
+		c.vp.GetString("postgres.host"),
+		c.vp.GetString("postgres.username"),
+		c.vp.GetString("postgres.password"),
+		c.vp.GetInt("postgres.port"),
+		c.vp.GetString("postgres.dbname"),
+	)
+
+	if params := c.vp.GetString("postgres.params"); params != "" {
+		str += " " + params
+	}
+
+	return str
 }
 
 func (c *Config) ServerPathPrefix() string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	return c.config.Server.PathPrefix
+	return c.vp.GetString("server.pathPrefix")
 }
 
 func (c *Config) SetServerPathPrefix(prefix string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.config.Server.PathPrefix = prefix
+	c.vp.Set("server.pathPrefix", prefix)
 }
 
 func (c *Config) ServerPort() int {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	if c.config.Server.HttpPort != 0 {
-		return c.config.Server.HttpPort
-	}
-
-	return 11633
+	return c.vp.GetInt("server.port")
 }
 
 func (c *Config) SetServerPort(port int) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.config.Server.HttpPort = port
+	c.vp.Set("server.port", port)
 }
 
 func (c *Config) ExperimentalFeatures() []string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	return c.config.ExperimentalFeatures
+	return c.vp.GetStringSlice("experimentalFeatures")
+}
+
+func (c *Config) InternalTelemetryEnabled() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.vp.GetBool("internalTelemetry.enabled")
+}
+
+func (c *Config) InternalTelemetryOtelCollectorAddress() string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.vp.GetString("internalTelemetry.otelCollectorAddress")
 }
