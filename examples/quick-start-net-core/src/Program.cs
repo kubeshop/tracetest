2f84ab9bac2cb97356f215389e92ba115d6e1ff5
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-02-14 11:07:10 -0600

    adding more information

--- examples/quick-start-net-core/src/Program.cs
+++ examples/quick-start-net-core/src/Program.cs
@@ -7,6 +7,8 @@ var serviceVersion = "1.0.0";
 
 var builder = WebApplication.CreateBuilder(args);
 
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -20,8 +22,15 @@ builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
       .AddConsoleExporter()
       .AddOtlpExporter(opt =>
         {
+          opt.BatchExportProcessorOptions = new OpenTelemetry.BatchExportProcessorOptions<System.Diagnostics.Activity> { MaxQueueSize = 1000, ExporterTimeoutMilliseconds = 100, MaxExportBatchSize = 1 };
+
+          // Using Grpc Protocol
           opt.Protocol = OtlpExportProtocol.Grpc;
           opt.Endpoint = new Uri("http://otel-collector:4317");
+
+          // Using Http Protocol
+          // opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+          // opt.Endpoint = new Uri("http://otel-collector:4318/v1/traces");
         })
       .AddSource(serviceName)
       .SetResourceBuilder(appResourceBuilder)
