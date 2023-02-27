f5255f63307c3a72e07e5e42ac40a63d973a0162
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-02-27 13:34:52 -0500

    fix(frontend): improve data store endpoint placeholder

--- web/src/components/Settings/DataStorePlugin/forms/ElasticSearch/AddressesList.tsx
+++ web/src/components/Settings/DataStorePlugin/forms/ElasticSearch/AddressesList.tsx
@@ -4,37 +4,36 @@ import {FormListFieldData} from 'antd/lib/form/FormList';
 import * as S from '../../DataStorePluginForm.styled';
 
 interface IProps {
-  fields: FormListFieldData[];
   add(): void;
+  fields: FormListFieldData[];
   remove(name: number): void;
+  placeholder?: string;
 }
 
-const AddressesList = ({fields, add, remove}: IProps) => {
-  return (
-    <>
-      <S.ItemListContainer>
-        {fields.map(({key, name, ...field}, index) => (
-          <S.ListItem key={key}>
-            <Form.Item key={key} name={[name]} {...{field}} style={{width: '100%'}}>
-              <Input placeholder={`Address #${index + 1}`} />
-            </Form.Item>
-            <S.ItemActionContainer>
-              {index !== 0 && (
-                <S.DeleteCheckIcon
-                  onClick={() => {
-                    remove(name);
-                  }}
-                />
-              )}
-            </S.ItemActionContainer>
-          </S.ListItem>
-        ))}
-      </S.ItemListContainer>
-      <S.AddButton icon={<PlusOutlined />} onClick={() => add()} data-cy="add-assertion-form-add-check">
-        Add new
-      </S.AddButton>
-    </>
-  );
-};
+const AddressesList = ({add, fields, remove, placeholder = ''}: IProps) => (
+  <>
+    <S.ItemListContainer>
+      {fields.map(({key, name, ...field}, index) => (
+        <S.ListItem key={key}>
+          <Form.Item key={key} name={[name]} {...{field}} style={{width: '100%'}}>
+            <Input placeholder={placeholder} />
+          </Form.Item>
+          <S.ItemActionContainer>
+            {index !== 0 && (
+              <S.DeleteCheckIcon
+                onClick={() => {
+                  remove(name);
+                }}
+              />
+            )}
+          </S.ItemActionContainer>
+        </S.ListItem>
+      ))}
+    </S.ItemListContainer>
+    <S.AddButton icon={<PlusOutlined />} onClick={() => add()} data-cy="add-assertion-form-add-check">
+      Add new
+    </S.AddButton>
+  </>
+);
 
 export default AddressesList;
--- web/src/components/Settings/DataStorePlugin/forms/ElasticSearch/ElasticSearch.tsx
+++ web/src/components/Settings/DataStorePlugin/forms/ElasticSearch/ElasticSearch.tsx
@@ -1,6 +1,7 @@
 import {Checkbox, Col, Form, Input, Row} from 'antd';
+import RequestDetailsFileInput from 'components/CreateTestPlugins/Grpc/steps/RequestDetails/RequestDetailsFileInput';
+import {SupportedDataStoresToDefaultEndpoint} from 'constants/DataStore.constants';
 import {SupportedDataStores, TDraftDataStore} from 'types/Config.types';
-import RequestDetailsFileInput from '../../../../CreateTestPlugins/Grpc/steps/RequestDetails/RequestDetailsFileInput';
 import * as S from '../../DataStorePluginForm.styled';
 import AddressesList from './AddressesList';
 
@@ -8,6 +9,7 @@ const OpenSearch = () => {
   const form = Form.useFormInstance<TDraftDataStore>();
   const dataStoreType = Form.useWatch('dataStoreType', form) || SupportedDataStores.OpenSearch;
   const baseName = ['dataStore', dataStoreType];
+  const endpointPlaceholder = SupportedDataStoresToDefaultEndpoint[dataStoreType];
 
   return (
     <>
@@ -24,7 +26,9 @@ const OpenSearch = () => {
         <Col span={12}>
           <S.ItemListLabel>Addresses</S.ItemListLabel>
           <Form.List name={[...baseName, 'addresses']}>
-            {(fields, {add, remove}) => <AddressesList fields={fields} add={add} remove={remove} />}
+            {(fields, {add, remove}) => (
+              <AddressesList fields={fields} add={add} remove={remove} placeholder={endpointPlaceholder} />
+            )}
           </Form.List>
         </Col>
       </Row>
