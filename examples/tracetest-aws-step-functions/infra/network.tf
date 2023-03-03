176b8009dd39cd1aa276e9573f6f18b57c4e934d
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-03-03 08:55:21 -0600

    chore(docs): adding aws step functions recipe documentation

--- examples/tracetest-aws-step-functions/infra/network.tf
+++ examples/tracetest-aws-step-functions/infra/network.tf
@@ -6,3 +6,40 @@ module "network" {
   public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
   private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
 }
+
+module "tracetest_alb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Load balancer security group"
+  vpc_id      = module.network.vpc_id
+
+  ingress_with_cidr_blocks = [{
+    from_port   = 11633
+    to_port     = 11633
+    protocol    = "tcp"
+    description = "HTTP access for Tracetest"
+    cidr_blocks = "0.0.0.0/0"
+  }]
+
+  egress_with_cidr_blocks = [{
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    description = "HTTP access to anywhere"
+    cidr_blocks = "0.0.0.0/0"
+  }]
+}
+
+resource "aws_lb" "tracetest_alb" {
+  name               = "${local.name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.tracetest_alb_security_group.security_group_id]
+  subnets            = module.network.public_subnets_ids
+
+  enable_deletion_protection = false
+  tags                       = local.tags
+}
+
