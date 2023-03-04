bbc6c600d7c51ebdbc55f45ec19c66186b8a9af3
Daniel Dias <danielbpdias@gmail.com>
2023-03-03 22:59:30 -0300

    added pagination code

--- server/config/configresource/config_resource.go
+++ server/config/configresource/config_resource.go
@@ -165,7 +165,17 @@ const (
 )
 
 func (r *repository) List(ctx context.Context, take, skip int, query, sortBy, sortDirection string) ([]Config, error) {
-	rows, err := r.db.QueryContext(ctx, baseSelect)
+	listQuery := baseSelect
+
+	if take > 0 {
+		listQuery = fmt.Sprintf("%s LIMIT %d", listQuery, take)
+	}
+
+	if skip > 0 {
+		listQuery = fmt.Sprintf("%s OFFSET %d", listQuery, skip)
+	}
+
+	rows, err := r.db.QueryContext(ctx, listQuery)
 	if err != nil {
 		return nil, fmt.Errorf("sql query: %w", err)
 	}
