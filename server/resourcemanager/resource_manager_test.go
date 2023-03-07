c7d42a82eb2f709bb0c1262bd064530bae3b2d7b
Sebastian Choren <sebastian.choren@gmail.com>
2023-03-07 17:52:39 -0300

    add tests for restricted resource

--- server/resourcemanager/resource_manager_test.go
+++ server/resourcemanager/resource_manager_test.go
@@ -165,6 +165,84 @@ func TestSampleResource(t *testing.T) {
 	})
 }
 
+func TestRestrictedResource(t *testing.T) {
+	sample := sampleResource{
+		ID:        "1",
+		Name:      "the name",
+		SomeValue: "the value",
+	}
+
+	sampleUpdated := sampleResource{
+		ID:        "1",
+		Name:      "the name updated",
+		SomeValue: "the value updated",
+	}
+
+	rmtests.TestResourceTypeWithErrorOperations(t, rmtests.ResourceTypeTest{
+		ResourceType: "RestrictedResource",
+		RegisterManagerFn: func(router *mux.Router) rm.Manager {
+			mockManager := new(restrictedResourceManager)
+			manager := rm.New[sampleResource]("RestrictedResource", mockManager, func() id.ID {
+				return id.ID("3")
+			})
+			manager.RegisterRoutes(router)
+
+			return manager
+		},
+		Prepare: func(t *testing.T, op rmtests.Operation, manager rm.Manager) {
+			mockManager := manager.Handler().(*restrictedResourceManager)
+			mockManager.Test(t)
+
+			switch op {
+
+			// Update
+			case rmtests.OperationUpdateNotFound:
+				mockManager.
+					On("Update", sampleUpdated).
+					Return(sampleResource{}, sql.ErrNoRows)
+			case rmtests.OperationUpdateSuccess:
+				mockManager.
+					On("Update", sampleUpdated).
+					Return(sampleUpdated, nil)
+			case rmtests.OperationUpdateInternalError:
+				mockManager.
+					On("Update", sampleUpdated).
+					Return(sampleResource{}, fmt.Errorf("some error"))
+
+				// Get
+			case rmtests.OperationGetNotFound:
+				mockManager.
+					On("Get", sample.ID).
+					Return(sampleResource{}, sql.ErrNoRows)
+			case rmtests.OperationGetSuccess:
+				mockManager.
+					On("Get", sample.ID).
+					Return(sample, nil)
+			case rmtests.OperationGetInternalError:
+				mockManager.
+					On("Get", sample.ID).
+					Return(sampleResource{}, fmt.Errorf("some error"))
+			}
+		},
+		SampleJSON: `{
+			"type": "RestrictedResource",
+			"spec": {
+				"id": "1",
+				"name": "the name",
+				"some_value": "the value"
+			}
+		}`,
+		SampleJSONUpdated: `{
+			"type": "RestrictedResource",
+			"spec": {
+				"id": "1",
+				"name": "the name updated",
+				"some_value": "the value updated"
+			}
+		}`,
+	})
+}
+
 // test structures and mocks
 
 type sampleResource struct {
@@ -182,22 +260,33 @@ func (sr sampleResource) Validate() error {
 	return nil
 }
 
-type basicResourceManager struct {
+type baseResourceManager struct {
 	mock.Mock
 }
 
-func (m *basicResourceManager) Get(_ context.Context, id id.ID) (sampleResource, error) {
+func (m *baseResourceManager) Get(_ context.Context, id id.ID) (sampleResource, error) {
 	args := m.Called(id)
 	return args.Get(0).(sampleResource), args.Error(1)
 }
 
-func (m *basicResourceManager) Update(_ context.Context, s sampleResource) (sampleResource, error) {
+func (m *baseResourceManager) Update(_ context.Context, s sampleResource) (sampleResource, error) {
 	args := m.Called(s)
 	return args.Get(0).(sampleResource), args.Error(1)
 }
 
+type restrictedResourceManager struct {
+	baseResourceManager
+}
+
+func (m *restrictedResourceManager) Operations() []rm.Operation {
+	return []rm.Operation{
+		rm.OperationGet,
+		rm.OperationUpdate,
+	}
+}
+
 type sampleResourceManager struct {
-	basicResourceManager
+	baseResourceManager
 }
 
 func (m *sampleResourceManager) SetID(sr sampleResource, id id.ID) sampleResource {
