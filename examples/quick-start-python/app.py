3ee2c12f5c0326689a62c0304101fba754696c9e
Adnan Rahic <ado.raha198@gmail.com>
2023-03-07 19:01:50 +0100

    examples(python): add otel instrumentation

--- examples/quick-start-python/app.py
+++ examples/quick-start-python/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "App Works!!!"
+
+@app.route("/server_request")
+def server_request():
+    print(request.args.get("param"))
+    return "served"
