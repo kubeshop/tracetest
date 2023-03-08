6876240dd2f47e12f48111cd0aea2921b23f3dfd
Adnan Rahic <ado.raha198@gmail.com>
2023-03-08 15:07:47 +0100

    examples(python): add manual instrumentation

--- examples/quick-start-python/app.py
+++ examples/quick-start-python/app.py
@@ -1,13 +1,32 @@
 from flask import Flask, request
 import json
 
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "App Works!!!"
+@app.route("/manual")
+def manual():
+    with tracer.start_as_current_span(
+        "manual", 
+        attributes={ "endpoint": "/manual", "foo": "bar" } 
+    ):
+        return "App works with a manual instrumentation."
 
-@app.route("/server_request")
-def server_request():
-    print(request.args.get("param"))
-    return "served"
+@app.route('/automatic')
+def automatic():
+    return "App works with automatic instrumentation."
+
+@app.route("/")
+def home():
+    return "App works."
