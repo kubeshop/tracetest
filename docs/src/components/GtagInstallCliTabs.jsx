05c35f20a304cb87f393469d167c8b1905a81c2d
Adnan Rahic <ado.raha198@gmail.com>
2023-03-13 12:20:09 +0100

    docs(ga4): add install event

---
+++ docs/src/components/GtagInstallCliTabs.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import CodeBlock from '@theme/CodeBlock';
+
+export default function GtagInstallCliTabs() {
+  function trigger({ event, installationPlatform }) {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+    'installationPlatform': installationPlatform,
+    'event': 'installationPlatform',
+    });
+
+    console.log(installationPlatform);
+  }
+  return (
+    <Tabs groupId="operating-systems">
+      <TabItem value="mac" label="MAC" default>
+        <div onClick={() => trigger({ installationPlatform: 'MacOS' })}>
+          <CodeBlock
+              language="bash"
+              title="Terminal"
+              >
+            {`brew install kubeshop/tracetest/tracetest`}
+          </CodeBlock>
+        </div>
+      </TabItem>
+      <TabItem value="linux" label="LINUX">
+        <div onClick={() => trigger({ installationPlatform: 'Linux' })}>
+          <CodeBlock
+              language="bash"
+              title="Terminal"
+          >
+          {`curl -L https://raw.githubusercontent.com/kubeshop/tracetest/main/install-cli.sh | bash`}
+          </CodeBlock>
+        </div>
+      </TabItem>
+      <TabItem value="win" label="WINDOWS">
+        <div onClick={() => trigger({ installationPlatform: 'Windows' })}>
+          <CodeBlock
+              language="bash"
+              title="Terminal"
+          >
+          {`choco source add --name=kubeshop_repo --source=https://chocolatey.kubeshop.io/chocolatey & choco install tracetest`}
+          </CodeBlock>
+        </div>
+      </TabItem>
+    </Tabs>
+  );
+};
