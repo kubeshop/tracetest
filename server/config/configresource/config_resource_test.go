ff52018ac44a399aee53e146cb6fddb5d1a2709c
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-03-14 17:45:26 -0300

    add todo comment

--- server/config/configresource/config_resource_test.go
+++ server/config/configresource/config_resource_test.go
@@ -134,6 +134,7 @@ func TestConfigResource(t *testing.T) {
 			}
 		}`,
 	},
+		// TODO: remove this when we support multiple profiles
 		rmtests.ExcludeOperations(
 			rmtests.OperationGetNotFound,
 			rmtests.OperationUpdateNotFound,
