b4262705e9e3df4a652b84a82b6b89571805ee3e
Sebastian Choren <sebastian.choren@gmail.com>
2023-04-05 18:09:36 -0300

    add nil pointer checks

--- server/model/yaml/yamlconvert/datastore.go
+++ server/model/yaml/yamlconvert/datastore.go
@@ -9,14 +9,25 @@ import (
 func DataStore(in model.DataStore) yaml.File {
 	out := yaml.DataStore{}
 	deepcopy.DeepCopy(in, &out)
-	deepcopy.DeepCopy(in.Values.Jaeger, &out.Jaeger)
-	deepcopy.DeepCopy(in.Values.Jaeger.TLSSetting, &out.Jaeger.Tls)
-	deepcopy.DeepCopy(in.Values.Tempo, &out.Tempo)
-	deepcopy.DeepCopy(in.Values.Tempo.Grpc.TLSSetting, &out.Tempo.Grpc.Tls)
-	deepcopy.DeepCopy(in.Values.Tempo.Http.TLSSetting, &out.Tempo.Http.Tls)
-	deepcopy.DeepCopy(in.Values.OpenSearch, &out.OpenSearch)
-	deepcopy.DeepCopy(in.Values.SignalFx, &out.SignalFx)
-	deepcopy.DeepCopy(in.Values.AwsXRay, &out.AwsXRay)
+	if in.Values.Jaeger != nil {
+		deepcopy.DeepCopy(in.Values.Jaeger, &out.Jaeger)
+		deepcopy.DeepCopy(in.Values.Jaeger.TLSSetting, &out.Jaeger.Tls)
+	}
+	if in.Values.Tempo != nil {
+		deepcopy.DeepCopy(in.Values.Tempo, &out.Tempo)
+		deepcopy.DeepCopy(in.Values.Tempo.Grpc.TLSSetting, &out.Tempo.Grpc.Tls)
+		deepcopy.DeepCopy(in.Values.Tempo.Http.TLSSetting, &out.Tempo.Http.Tls)
+	}
+
+	if in.Values.OpenSearch != nil {
+		deepcopy.DeepCopy(in.Values.OpenSearch, &out.OpenSearch)
+	}
+	if in.Values.SignalFx != nil {
+		deepcopy.DeepCopy(in.Values.SignalFx, &out.SignalFx)
+	}
+	if in.Values.AwsXRay != nil {
+		deepcopy.DeepCopy(in.Values.AwsXRay, &out.AwsXRay)
+	}
 
 	return yaml.File{
 		Type: yaml.FileTypeDataStore,
