60574add837117d460d92a756e6d44cf2ea0770b
Daniel Dias <danielbpdias@gmail.com>
2023-04-19 18:19:59 -0300

    Fixing rebase problems

--- server/tracedb/datastoreresource/repository.go
+++ server/tracedb/datastoreresource/repository.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/kubeshop/tracetest/server/id"
+	"github.com/kubeshop/tracetest/server/pkg/id"
 	"github.com/kubeshop/tracetest/server/resourcemanager"
 )
 
--- server/tracedb/datastoreresource/repository_test.go
+++ server/tracedb/datastoreresource/repository_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
-	"github.com/kubeshop/tracetest/server/id"
+	"github.com/kubeshop/tracetest/server/pkg/id"
 	"github.com/kubeshop/tracetest/server/resourcemanager"
 	rmtests "github.com/kubeshop/tracetest/server/resourcemanager/testutil"
 	"github.com/kubeshop/tracetest/server/testmock"
