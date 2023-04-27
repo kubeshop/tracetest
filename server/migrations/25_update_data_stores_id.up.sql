d35d5de10e37e1b8ddff48d4b3e4803d0e32c84f
Daniel Dias <danielbpdias@gmail.com>
2023-04-27 12:43:20 -0300

    Adding PR suggestion

--- server/migrations/25_update_data_stores_id.up.sql
+++ server/migrations/25_update_data_stores_id.up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 UPDATE data_stores
 SET id = 'current'
-WHERE id = (select id from data_stores order by created_at limit 1);
+WHERE id = (select id from data_stores where is_default='true' order by created_at limit 1);
 
 DELETE FROM data_stores WHERE id != 'current';
 
