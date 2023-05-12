bf6cf539523301a670410d2ba21f1173c29263b9
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-05-12 22:18:01 +0900

    fix(frontend): fix otlp test connection message (#2515)

--- web/src/providers/DataStore/hooks/useDataStoreNotification.tsx
+++ web/src/providers/DataStore/hooks/useDataStoreNotification.tsx
@@ -15,7 +15,7 @@ const useDataStoreNotification = () => {
           type: 'info',
           title: <Typography.Title level={2}>No Automated Test</Typography.Title>,
           description:
-            'Since the OpenTelemetry Collector sends traces to Tracetest, there is no automated test. Once you have configured your OpenTelemetry Collector to send Tracetest spans to Tracetest, try running a Tracetest test against your application under test.',
+            'Please note that configuring your OpenTelemetry Collector to send spans to Tracetest is just the first step. To enable successful testing, save this data store, and then try running a Tracetest test against your application under test. Note that there is no automated test since the OpenTelemetry Collector sends traces to Tracetest.',
         });
       }
 
