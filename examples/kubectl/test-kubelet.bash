6cca0c28fc2e0fb8d08fb657f9606ba10d8f46e2
Sebastian Choren <sebastian.choren@gmail.com>
2023-05-17 18:18:20 -0300

    add kubelet script

--- examples/kubectl/test-kubelet.bash
+++ examples/kubectl/test-kubelet.bash
@@ -4,17 +4,19 @@ JAEGER_UI_URL=${JAEGER_UI_URL:-"http://127.0.0.1:16686"}
 DEBUG=${DEBUG}
 TEST_FILE=$1
 KUBECTL_CMD=$2
+POD_PREFIX=$3
 
 echo "-> Parameters:"
 echo "      JAEGER_UI_URL: ${JAEGER_UI_URL}"
 echo "      TEST_FILE:     ${TEST_FILE}"
 echo "      KUBECTL_CMD:   ${KUBECTL_CMD}"
-echo
+echo "      POD_PREFIX:    ${POD_PREFIX}"
 
+echo
 
 outputFile=$(mktemp)
 debugInfo=$(mktemp)
-$KUBECTL_CMD -v 9 > $outputFile 2>  $debugInfo
+$KUBECTL_CMD > $outputFile 2>  $debugInfo
 exitCode=$?
 
 if [ $exitCode -ne 0 ]; then
@@ -42,10 +44,13 @@ fi
 echo "-> wait a moment so things propagate correctly"
 sleep 5
 
-AUDIT_ID=$(cat $debugInfo | grep "Audit-Id:" | tr -s ' ' | cut -d' ' -f6)
-echo "-> Audit-Id": $AUDIT_ID
+pods=$(kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.namespace}/{.metadata.name}{"\n"}{end}' | grep $POD_PREFIX | head -n1)
 
-START=$(date -v-1H +%s)
+if [ "$DEBUG" == "yes" ]; then
+  echo "**** Pods: $pods"
+fi
+
+START=$(date -v-5M +%s)
 END=$(date +%s)
 
 curlDebug=$(mktemp)
@@ -56,9 +61,9 @@ curl -v --get "${JAEGER_UI_URL}/api/traces" \
   --data-urlencode "minDuration" \
   --data-urlencode "maxDuration" \
   --data-urlencode "limit=20" \
-  --data-urlencode "lookback=1h" \
-  --data-urlencode "service=apiserver" \
-  --data-urlencode 'tags={"audit-id":"'$AUDIT_ID'"}' > $traces 2> $curlDebug
+  --data-urlencode "lookback=5m" \
+  --data-urlencode "service=kubelet" \
+  --data-urlencode 'tags={"k8s.pod":"'$pods'"}' > $traces 2> $curlDebug
 
 if [ "$DEBUG" == "yes" ]; then
   echo "**** curl debug output file: $curlDebug"
@@ -77,6 +82,7 @@ testFile=$(mktemp)
 tracetestCommand="tracetest test run --definition $testFile --wait-for-result"
 cat $TEST_FILE | sed "8s/.*/      id: $TRACE_ID/" > $testFile
 
+DEBUG=yes
 if [ "$DEBUG" == "yes" ]; then
   echo "**** replaced test file: $testFile"
   cat $testFile
