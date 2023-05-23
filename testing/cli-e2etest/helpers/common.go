a124daf51db13b49b383802a613fa0a3a04d2681
Daniel Dias <danielbpdias@gmail.com>
2023-05-23 16:51:03 -0300

    Improve test output and remove parallelism

--- testing/cli-e2etest/helpers/common.go
+++ testing/cli-e2etest/helpers/common.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/goccy/go-yaml"
+	"github.com/kubeshop/tracetest/cli-e2etest/command"
 	"github.com/stretchr/testify/require"
 )
 
@@ -48,4 +49,12 @@ func UnmarshalYAMLSequence[T any](t *testing.T, data string) []T {
 	}
 
 	return result
+}
+
+func RequireExitCodeEqual(t *testing.T, result *command.ExecResult, expectedExitCode int) {
+	require.Equal(
+		t, expectedExitCode, result.ExitCode,
+		"command %s finished with wrong exit code. Expected code: %d Obtained code: %d \nStdOut: %s \nStdErr: %s",
+		result.CommandExecuted, expectedExitCode, result.ExitCode, result.StdOut, result.StdErr,
+	)
 }
