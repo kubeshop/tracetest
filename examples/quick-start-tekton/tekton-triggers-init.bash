d9a2e3cbc7f16627352bdc50a620d03e7f66f032
Adnan Rahic <ado.raha198@gmail.com>
2023-05-23 11:43:54 +0200

    docs+ex(tekton): initial samples

---
+++ examples/quick-start-tekton/tekton-triggers-init.bash
@@ -0,0 +1,4 @@
+kubectl apply --filename \
+https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename \
+https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
