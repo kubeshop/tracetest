3ac68ca4f5e071fa0b323c773d7711265e9f5bd2
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-05-26 12:37:04 -0600

    feature(cli): adding server env for version test

--- testing/cli-e2etest/testscenarios/version_test.go
+++ testing/cli-e2etest/testscenarios/version_test.go
@@ -1,9 +1,9 @@
 package testscenarios
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/kubeshop/tracetest/cli-e2etest/environment"
 	"github.com/kubeshop/tracetest/cli-e2etest/tracetestcli"
 	"github.com/stretchr/testify/require"
 )
@@ -12,14 +12,14 @@ func TestVersionCommand(t *testing.T) {
 	// instantiate require with testing helper
 	require := require.New(t)
 
+	// setup test server environment
+	env := environment.CreateAndStart(t)
+	defer env.Close(t)
+
 	// Given I am a Tracetest CLI user
 	// When I try to check the tracetest version
 	// Then I should receive a version string with success
-
 	result := tracetestcli.Exec(t, "version")
-	fmt.Println("STD OUT: ", result.StdOut)
-	fmt.Println("COMMAND: ", result.CommandExecuted)
-	fmt.Println("STD ERR: ", result.StdErr)
 
 	require.Equal(0, result.ExitCode)
 	require.Greater(len(result.StdOut), 0)
