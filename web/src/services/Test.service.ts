7d1b98573b4a70710fdda24d9a3da15f6de308a4
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-05-29 16:58:28 -0500

    fix(frontend): fix types for test specs (#2606)

--- web/src/services/Test.service.ts
+++ web/src/services/Test.service.ts
@@ -6,7 +6,6 @@ import {IPlugin} from 'types/Plugins.types';
 import {TDraftTest} from 'types/Test.types';
 import Validator from 'utils/Validator';
 import Test, {TRawTest} from 'models/Test.model';
-import { rawTestSpecToNewFormat } from 'models/TestSpecs.model';
 import TestDefinitionService from './TestDefinition.service';
 import GrpcService from './Triggers/Grpc.service';
 import HttpService from './Triggers/Http.service';
@@ -63,7 +62,7 @@ const TestService = () => ({
       ...(original
         ? {
             outputs: toRawTestOutputs(original.outputs ?? []),
-            specs: original.definition.specs.map(def => rawTestSpecToNewFormat(TestDefinitionService.toRaw(def))),
+            specs: original.definition.specs.map(def => TestDefinitionService.toRaw(def)),
           }
         : {}),
     };
