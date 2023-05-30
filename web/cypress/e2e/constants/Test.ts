4cacd452bcf1c5b0bb7e373abc5d258d4309878d
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-05-30 15:34:34 -0600

    fixing pokeshop url variable

--- web/cypress/e2e/constants/Test.ts
+++ web/cypress/e2e/constants/Test.ts
@@ -1,4 +1,4 @@
-export const POKEMON_HTTP_ENDPOINT = process.env.POKEMON_HTTP_ENDPOINT || 'http://demo-pokemon-api.demo.svc.cluster.local';
+export const POKEMON_HTTP_ENDPOINT = Cypress.env('pokemonApiUrl');
 export const PokeshopDemo = [
   {
     name: 'Pokeshop - List',
