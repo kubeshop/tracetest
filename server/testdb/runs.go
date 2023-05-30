e02077ad2cf8786147eccd7077d7e7568d900868
Daniel Baptista Dias <danielbdias@users.noreply.github.com>
2023-05-30 10:05:32 -0300

    fix(server): fix transaction run reporting order (#2617)

--- server/testdb/runs.go
+++ server/testdb/runs.go
@@ -334,12 +334,12 @@ SELECT
 	"environment",
 
 	-- transaction run
-	transaction_run.transaction_run_id,
-	transaction_run.transaction_run_transaction_id
-FROM test_runs
-LEFT OUTER JOIN
-	transaction_run_steps transaction_run
-ON transaction_run.test_run_id = id AND transaction_run.test_run_test_id = test_id
+	transaction_run_steps.transaction_run_id,
+	transaction_run_steps.transaction_run_transaction_id
+FROM
+	test_runs
+		LEFT OUTER JOIN transaction_run_steps
+		ON transaction_run_steps.test_run_id = test_runs.id AND transaction_run_steps.test_run_test_id = test_runs.test_id
 `
 
 func (td *postgresDB) GetRun(ctx context.Context, testID id.ID, runID int) (model.Run, error) {
@@ -554,11 +554,12 @@ func readRunRow(row scanner) (model.Run, error) {
 }
 
 func (td *postgresDB) GetTransactionRunSteps(ctx context.Context, tr tests.TransactionRun) ([]model.Run, error) {
-	// stmt, err := td.db.Prepare(selectRunQuery + " WHERE id = $1 AND test_id = $2")
-	stmt, err := td.db.Prepare(selectRunQuery + ` INNER JOIN
-		transaction_run_steps trs ON
-			test_runs.id = trs.test_run_id AND test_runs.test_id = trs.test_run_test_id
-		WHERE trs.transaction_run_id = $1 AND trs.transaction_run_transaction_id = $2`)
+	query := selectRunQuery + `
+WHERE transaction_run_steps.transaction_run_id = $1 AND transaction_run_steps.transaction_run_transaction_id = $2
+ORDER BY test_runs.completed_at ASC
+`
+
+	stmt, err := td.db.Prepare(query)
 	if err != nil {
 		return []model.Run{}, fmt.Errorf("prepare: %w", err)
 	}
