a28630aea627106b49be2eace834689c6f1a6a8b
Daniel Dias <danielbpdias@gmail.com>
2023-06-07 16:45:16 -0300

    Updating list tests

--- testing/cli-e2etest/testscenarios/environment/list_environments_test.go
+++ testing/cli-e2etest/testscenarios/environment/list_environments_test.go
@@ -11,13 +11,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestListEnvironments(t *testing.T) {
+func executeListEnvironmentsPreReqs(t *testing.T, env environment.Manager) {
 	// instantiate require with testing helper
 	require := require.New(t)
-
-	// setup isolated e2e environment
-	env := environment.CreateAndStart(t)
-	defer env.Close(t)
 
 	cliConfig := env.GetCLIConfigPath(t)
 
@@ -59,52 +55,124 @@ func TestListEnvironments(t *testing.T) {
 	require.Equal("Environment", environmentVars.Type)
 	require.Equal("one-more-env", environmentVars.Spec.ID)
 	require.Equal("one-more-env", environmentVars.Spec.Name)
+}
 
-	// When I try to list these environments by an invalid field
-	// Then I should receive an error
+func TestListEnvironments(t *testing.T) {
 
-	result = tracetestcli.Exec(t, "list environment --sortBy id --output yaml", tracetestcli.WithCLIConfig(cliConfig))
-	helpers.RequireExitCodeEqual(t, result, 1)
-	require.Contains(result.StdErr, "invalid sort field: id") // TODO: think on how to improve this error handling
+	// setup isolated e2e environment
+	env := environment.CreateAndStart(t)
+	defer env.Close(t)
 
-	// When I try to list these environments by a valid field and in YAML format
-	// Then I should receive three environments
+	executeListEnvironmentsPreReqs(t, env)
 
-	result = tracetestcli.Exec(t, "list environment --sortBy name --sortDirection asc --output yaml", tracetestcli.WithCLIConfig(cliConfig))
-	helpers.RequireExitCodeEqual(t, result, 0)
+	cliConfig := env.GetCLIConfigPath(t)
 
-	environmentVarsList := helpers.UnmarshalYAMLSequence[types.EnvironmentResource](t, result.StdOut)
-	require.Len(environmentVarsList, 3)
+	t.Run("list with invalid sortBy field", func(t *testing.T) {
+		// instantiate require with testing helper
+		require := require.New(t)
 
-	// due the our database sorting algorithm, "another-env" comes in the front of ".env"
-	// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
-	anotherEnvironmentVars := environmentVarsList[0]
-	require.Equal("Environment", anotherEnvironmentVars.Type)
-	require.Equal("another-env", anotherEnvironmentVars.Spec.ID)
-	require.Equal("another-env", anotherEnvironmentVars.Spec.Name)
-	require.Len(anotherEnvironmentVars.Spec.Values, 2)
-	require.Equal("Here", anotherEnvironmentVars.Spec.Values[0].Key)
-	require.Equal("We", anotherEnvironmentVars.Spec.Values[0].Value)
-	require.Equal("Come", anotherEnvironmentVars.Spec.Values[1].Key)
-	require.Equal("Again", anotherEnvironmentVars.Spec.Values[1].Value)
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
+		// When I try to list these environments by an invalid field
+		// Then I should receive an error
+		result := tracetestcli.Exec(t, "list environment --sortBy id --output yaml", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 1)
+		require.Contains(result.StdErr, "invalid sort field: id") // TODO: think on how to improve this error handling
+	})
 
-	environmentVars = environmentVarsList[1]
-	require.Equal("Environment", environmentVars.Type)
-	require.Equal(".env", environmentVars.Spec.ID)
-	require.Equal(".env", environmentVars.Spec.Name)
-	require.Len(environmentVars.Spec.Values, 2)
-	require.Equal("FIRST_VAR", environmentVars.Spec.Values[0].Key)
-	require.Equal("some-value", environmentVars.Spec.Values[0].Value)
-	require.Equal("SECOND_VAR", environmentVars.Spec.Values[1].Key)
-	require.Equal("another_value", environmentVars.Spec.Values[1].Value)
+	t.Run("list with YAML format", func(t *testing.T) {
+		// instantiate require with testing helper
+		require := require.New(t)
 
-	oneMoreEnvironmentVars := environmentVarsList[2]
-	require.Equal("Environment", oneMoreEnvironmentVars.Type)
-	require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.ID)
-	require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.Name)
-	require.Len(oneMoreEnvironmentVars.Spec.Values, 2)
-	require.Equal("This", oneMoreEnvironmentVars.Spec.Values[0].Key)
-	require.Equal("Is", oneMoreEnvironmentVars.Spec.Values[0].Value)
-	require.Equal("The", oneMoreEnvironmentVars.Spec.Values[1].Key)
-	require.Equal("Third", oneMoreEnvironmentVars.Spec.Values[1].Value)
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
+
+		// When I try to list these environments by a valid field and in YAML format
+		// Then I should receive three environments
+		result := tracetestcli.Exec(t, "list environment --sortBy name --sortDirection asc --output yaml", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
+
+		environmentVarsList := helpers.UnmarshalYAMLSequence[types.EnvironmentResource](t, result.StdOut)
+		require.Len(environmentVarsList, 3)
+
+		// due the our database sorting algorithm, "another-env" comes in the front of ".env"
+		// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
+		anotherEnvironmentVars := environmentVarsList[0]
+		require.Equal("Environment", anotherEnvironmentVars.Type)
+		require.Equal("another-env", anotherEnvironmentVars.Spec.ID)
+		require.Equal("another-env", anotherEnvironmentVars.Spec.Name)
+		require.Len(anotherEnvironmentVars.Spec.Values, 2)
+		require.Equal("Here", anotherEnvironmentVars.Spec.Values[0].Key)
+		require.Equal("We", anotherEnvironmentVars.Spec.Values[0].Value)
+		require.Equal("Come", anotherEnvironmentVars.Spec.Values[1].Key)
+		require.Equal("Again", anotherEnvironmentVars.Spec.Values[1].Value)
+
+		environmentVars := environmentVarsList[1]
+		require.Equal("Environment", environmentVars.Type)
+		require.Equal(".env", environmentVars.Spec.ID)
+		require.Equal(".env", environmentVars.Spec.Name)
+		require.Len(environmentVars.Spec.Values, 2)
+		require.Equal("FIRST_VAR", environmentVars.Spec.Values[0].Key)
+		require.Equal("some-value", environmentVars.Spec.Values[0].Value)
+		require.Equal("SECOND_VAR", environmentVars.Spec.Values[1].Key)
+		require.Equal("another_value", environmentVars.Spec.Values[1].Value)
+
+		oneMoreEnvironmentVars := environmentVarsList[2]
+		require.Equal("Environment", oneMoreEnvironmentVars.Type)
+		require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.ID)
+		require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.Name)
+		require.Len(oneMoreEnvironmentVars.Spec.Values, 2)
+		require.Equal("This", oneMoreEnvironmentVars.Spec.Values[0].Key)
+		require.Equal("Is", oneMoreEnvironmentVars.Spec.Values[0].Value)
+		require.Equal("The", oneMoreEnvironmentVars.Spec.Values[1].Key)
+		require.Equal("Third", oneMoreEnvironmentVars.Spec.Values[1].Value)
+	})
+
+	t.Run("list with JSON format", func(t *testing.T) {
+		// instantiate require with testing helper
+		require := require.New(t)
+
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
+
+		// When I try to list these environments by a valid field and in JSON format
+		// Then I should receive three environments
+		result := tracetestcli.Exec(t, "list environment --sortBy name --sortDirection asc --output json", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
+
+		environmentVarsList := helpers.UnmarshalJSON[[]types.EnvironmentResource](t, result.StdOut)
+		require.Len(environmentVarsList, 3)
+
+		// due the our database sorting algorithm, "another-env" comes in the front of ".env"
+		// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
+		anotherEnvironmentVars := environmentVarsList[0]
+		require.Equal("Environment", anotherEnvironmentVars.Type)
+		require.Equal("another-env", anotherEnvironmentVars.Spec.ID)
+		require.Equal("another-env", anotherEnvironmentVars.Spec.Name)
+		require.Len(anotherEnvironmentVars.Spec.Values, 2)
+		require.Equal("Here", anotherEnvironmentVars.Spec.Values[0].Key)
+		require.Equal("We", anotherEnvironmentVars.Spec.Values[0].Value)
+		require.Equal("Come", anotherEnvironmentVars.Spec.Values[1].Key)
+		require.Equal("Again", anotherEnvironmentVars.Spec.Values[1].Value)
+
+		environmentVars := environmentVarsList[1]
+		require.Equal("Environment", environmentVars.Type)
+		require.Equal(".env", environmentVars.Spec.ID)
+		require.Equal(".env", environmentVars.Spec.Name)
+		require.Len(environmentVars.Spec.Values, 2)
+		require.Equal("FIRST_VAR", environmentVars.Spec.Values[0].Key)
+		require.Equal("some-value", environmentVars.Spec.Values[0].Value)
+		require.Equal("SECOND_VAR", environmentVars.Spec.Values[1].Key)
+		require.Equal("another_value", environmentVars.Spec.Values[1].Value)
+
+		oneMoreEnvironmentVars := environmentVarsList[2]
+		require.Equal("Environment", oneMoreEnvironmentVars.Type)
+		require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.ID)
+		require.Equal("one-more-env", oneMoreEnvironmentVars.Spec.Name)
+		require.Len(oneMoreEnvironmentVars.Spec.Values, 2)
+		require.Equal("This", oneMoreEnvironmentVars.Spec.Values[0].Key)
+		require.Equal("Is", oneMoreEnvironmentVars.Spec.Values[0].Value)
+		require.Equal("The", oneMoreEnvironmentVars.Spec.Values[1].Key)
+		require.Equal("Third", oneMoreEnvironmentVars.Spec.Values[1].Value)
+	})
 }
