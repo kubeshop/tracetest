2201d1afb4c97f8f9145499ada43dd62d492e00d
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-06-14 14:04:22 -0300

    add augmented list

--- server/app/app.go
+++ server/app/app.go
@@ -441,6 +441,9 @@ func registerDataStoreResource(repository *datastoreresource.Repository, router 
 func registerTestResource(repository test.Repository, router *mux.Router, provisioner *provisioning.Provisioner, tracer trace.Tracer) {
 	operations := []resourcemanager.Operation{
 		resourcemanager.OperationList,
+		// TODO: replace it with the option `resourcemanager.CanBeAugmented()`
+		// once we have the `resourcemanager.OperationGet` operation.
+		resourcemanager.OperationListAugmented,
 	}
 
 	manager := resourcemanager.New[test.Test](
