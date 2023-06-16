05f28fbf4eb948c7662c46559f3e2630b50db4b8
Daniel Dias <danielbpdias@gmail.com>
2023-06-16 14:14:41 -0300

    Adding run test for transactions

---
+++ testing/cli-e2etest/testscenarios/transaction/run_transaction_test.go
@@ -0,0 +1,43 @@
+package transaction
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kubeshop/tracetest/cli-e2etest/environment"
+	"github.com/kubeshop/tracetest/cli-e2etest/helpers"
+	"github.com/kubeshop/tracetest/cli-e2etest/tracetestcli"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRunTransaction(t *testing.T) {
+	// setup isolated e2e environment
+	env := environment.CreateAndStart(t, environment.WithDataStoreEnabled(), environment.WithPokeshop())
+	defer env.Close(t)
+
+	cliConfig := env.GetCLIConfigPath(t)
+
+	t.Run("should pass", func(t *testing.T) {
+		// instantiate require with testing helper
+		require := require.New(t)
+
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
+		// And the datasource is already set
+
+		// When I try to run a transaction
+		// Then it should pass
+		transactionFile := env.GetTestResourcePath(t, "new-transaction")
+
+		command := fmt.Sprintf("test run -w -d %s", transactionFile)
+		result := tracetestcli.Exec(t, command, tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
+		require.Contains(result.StdOut, "New Transaction") // transaction name
+		require.Contains(result.StdOut, "Pokeshop - Add")  // first test
+		require.Contains(result.StdOut, "✔ It should add a Pokemon correctly")
+		require.Contains(result.StdOut, "✔ It should save the correct data")
+
+		require.Contains(result.StdOut, "Pokeshop - Get") // second test
+		require.Contains(result.StdOut, "✔ It should Get Pokemons correctly")
+	})
+}
