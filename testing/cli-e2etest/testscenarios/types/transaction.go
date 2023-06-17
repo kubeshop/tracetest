7ed574bd7a2d255dd2f3cf9d2c3ed8df5117d1ed
Daniel Dias <danielbpdias@gmail.com>
2023-06-16 21:38:16 -0300

    Adding list test

--- testing/cli-e2etest/testscenarios/types/transaction.go
+++ testing/cli-e2etest/testscenarios/types/transaction.go
@@ -14,3 +14,26 @@ type TransactionResource struct {
 	Type string      `json:"type"`
 	Spec Transaction `json:"spec"`
 }
+
+type AugmentedTransactionLastRun struct {
+	Passes int `json:"passes"`
+	Fails  int `json:"fails"`
+}
+
+type AugmentedTransactionSummary struct {
+	Runs    int                         `json:"runs"`
+	LastRun AugmentedTransactionLastRun `json:"lastRun"`
+}
+
+type AugmentedTransaction struct {
+	ID          string                      `json:"id"`
+	Name        string                      `json:"name"`
+	Description string                      `json:"description"`
+	Steps       []string                    `json:"steps"`
+	Summary     AugmentedTransactionSummary `json:"summary"`
+}
+
+type AugmentedTransactionResource struct {
+	Type string               `json:"type"`
+	Spec AugmentedTransaction `json:"spec"`
+}
