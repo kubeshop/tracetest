8b807f6704f040c6319bfb99b13a55f8d9eaedbb
Sebastian Choren <sebastian.choren@gmail.com>
2023-06-22 10:46:53 -0300

    fix(server): fix trigger json encoding versioning and test

--- server/test/trigger/trigger_json_test.go
+++ server/test/trigger/trigger_json_test.go
@@ -4,29 +4,29 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/kubeshop/tracetest/server/model"
+	"github.com/kubeshop/tracetest/server/test/trigger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTriggerFormatV1(t *testing.T) {
 	v1 := struct {
-		Type    model.TriggerType
-		HTTP    *model.HTTPRequest
-		GRPC    *model.GRPCRequest
-		TraceID *model.TRACEIDRequest
+		Type    trigger.TriggerType
+		HTTP    *trigger.HTTPRequest
+		GRPC    *trigger.GRPCRequest
+		TraceID *trigger.TraceIDRequest
 	}{
-		Type: model.TriggerTypeHTTP,
-		HTTP: &model.HTTPRequest{
-			Method: model.HTTPMethodGET,
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
 			URL:    "http://example.com/list",
 		},
 	}
 
-	expected := model.Trigger{
-		Type: model.TriggerTypeHTTP,
-		HTTP: &model.HTTPRequest{
-			Method: model.HTTPMethodGET,
+	expected := trigger.Trigger{
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
 			URL:    "http://example.com/list",
 		},
 	}
@@ -34,7 +34,7 @@ func TestTriggerFormatV1(t *testing.T) {
 	v1Json, err := json.Marshal(v1)
 	require.NoError(t, err)
 
-	current := model.Trigger{}
+	current := trigger.Trigger{}
 	err = json.Unmarshal(v1Json, &current)
 	require.NoError(t, err)
 
@@ -43,22 +43,22 @@ func TestTriggerFormatV1(t *testing.T) {
 
 func TestTriggerFormatV2(t *testing.T) {
 	v2 := struct {
-		Type    model.TriggerType     `json:"triggerType"`
-		HTTP    *model.HTTPRequest    `json:"http,omitempty"`
-		GRPC    *model.GRPCRequest    `json:"grpc,omitempty"`
-		TraceID *model.TRACEIDRequest `json:"traceid,omitempty"`
+		Type    trigger.TriggerType     `json:"triggerType"`
+		HTTP    *trigger.HTTPRequest    `json:"http,omitempty"`
+		GRPC    *trigger.GRPCRequest    `json:"grpc,omitempty"`
+		TraceID *trigger.TraceIDRequest `json:"traceid,omitempty"`
 	}{
-		Type: model.TriggerTypeHTTP,
-		HTTP: &model.HTTPRequest{
-			Method: model.HTTPMethodGET,
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
 			URL:    "http://example.com/list",
 		},
 	}
 
-	expected := model.Trigger{
-		Type: model.TriggerTypeHTTP,
-		HTTP: &model.HTTPRequest{
-			Method: model.HTTPMethodGET,
+	expected := trigger.Trigger{
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
 			URL:    "http://example.com/list",
 		},
 	}
@@ -66,8 +66,40 @@ func TestTriggerFormatV2(t *testing.T) {
 	v2Json, err := json.Marshal(v2)
 	require.NoError(t, err)
 
-	current := model.Trigger{}
+	current := trigger.Trigger{}
 	err = json.Unmarshal(v2Json, &current)
+	require.NoError(t, err)
+
+	assert.Equal(t, expected, current)
+}
+
+func TestTriggerFormatV3(t *testing.T) {
+	v3 := struct {
+		Type    trigger.TriggerType     `json:"type"`
+		HTTP    *trigger.HTTPRequest    `json:"http,omitempty"`
+		GRPC    *trigger.GRPCRequest    `json:"grpc,omitempty"`
+		TraceID *trigger.TraceIDRequest `json:"traceid,omitempty"`
+	}{
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
+			URL:    "http://example.com/list",
+		},
+	}
+
+	expected := trigger.Trigger{
+		Type: trigger.TriggerTypeHTTP,
+		HTTP: &trigger.HTTPRequest{
+			Method: trigger.HTTPMethodGET,
+			URL:    "http://example.com/list",
+		},
+	}
+
+	v3Json, err := json.Marshal(v3)
+	require.NoError(t, err)
+
+	current := trigger.Trigger{}
+	err = json.Unmarshal(v3Json, &current)
 	require.NoError(t, err)
 
 	assert.Equal(t, expected, current)
