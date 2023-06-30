2e655edef5de083b99e74c6976390bb4baee02a6
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-06-30 08:38:18 -0600

    feature(frontend): Contact Us Modal (#2835)
    * feature(frontend): Contact Us Modal
    
    * feature(frontend): Contact Us Modal

---
+++ web/src/components/ContactUs/ContactUsModalFooter.tsx
@@ -0,0 +1,19 @@
+import {DISCORD_URL, GITHUB_ISSUES_URL} from 'constants/Common.constants';
+import * as S from './ContactUs.styled';
+
+const ContactUsModalFooter = () => {
+  return (
+    <S.ModalFooter>
+      <a href={GITHUB_ISSUES_URL} target="_blank">
+        <S.FullWidthButton type="primary">Create an Issue</S.FullWidthButton>
+      </a>
+      <a href={DISCORD_URL} target="_blank">
+        <S.FullWidthButton ghost type="primary">
+          Contact Team on Discord
+        </S.FullWidthButton>
+      </a>
+    </S.ModalFooter>
+  );
+};
+
+export default ContactUsModalFooter;
