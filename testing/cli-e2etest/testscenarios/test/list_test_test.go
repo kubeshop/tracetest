31ce75c41f2bc6bfc292b8351e74c397b1e3b9a7
Sebastian Choren <sebastian.choren@gmail.com>
2023-07-05 12:36:49 -0300

    add list tests

--- testing/cli-e2etest/testscenarios/test/list_test_test.go
+++ testing/cli-e2etest/testscenarios/test/list_test_test.go
@@ -2,9 +2,9 @@ package test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/kubeshop/tracetest/cli-e2etest/environment"
 	"github.com/kubeshop/tracetest/cli-e2etest/helpers"
 	"github.com/kubeshop/tracetest/cli-e2etest/testscenarios/types"
@@ -81,8 +81,6 @@ func TestListTests(t *testing.T) {
 		testVarsList := helpers.UnmarshalYAMLSequence[types.TestResource](t, result.StdOut)
 		require.Len(testVarsList, 2)
 
-		spew.Dump(testVarsList)
-
 		listTest := testVarsList[0]
 		assert.Equal("Test", listTest.Type)
 		assert.Equal("fH_8AulVR", listTest.Spec.ID)
@@ -110,102 +108,86 @@ func TestListTests(t *testing.T) {
 		assert.Equal("application/json", importTest.Spec.Trigger.HTTPRequest.Headers[0].Value)
 	})
 
-	// 	t.Run("list with JSON format", func(t *testing.T) {
-	// 		// Given I am a Tracetest CLI user
-	// 		// And I have my server recently created
+	t.Run("list with JSON format", func(t *testing.T) {
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
 
-	// 		// When I try to list these tests by a valid field and in JSON format
-	// 		// Then I should receive three tests
-	// 		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection asc --output json", tracetestcli.WithCLIConfig(cliConfig))
-	// 		helpers.RequireExitCodeEqual(t, result, 0)
+		// When I try to list these tests by a valid field and in JSON format
+		// Then I should receive three tests
+		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection asc --output json", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
 
-	// 		testVarsList := helpers.UnmarshalJSON[[]types.TestResource](t, result.StdOut)
-	// 		require.Len(testVarsList, 3)
+		testVarsList := helpers.UnmarshalJSON[types.ResourceList[types.TestResource]](t, result.StdOut)
+		require.Len(testVarsList.Items, 2)
+		require.Equal(len(testVarsList.Items), testVarsList.Count)
 
-	// 		// due our database sorting algorithm, "another-env" comes in the front of ".env"
-	// 		// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
-	// 		anotherTestVars := testVarsList[0]
-	// 		require.Equal("Test", anotherTestVars.Type)
-	// 		require.Equal("another-env", anotherTestVars.Spec.ID)
-	// 		require.Equal("another-env", anotherTestVars.Spec.Name)
-	// 		require.Len(anotherTestVars.Spec.Values, 2)
-	// 		require.Equal("Here", anotherTestVars.Spec.Values[0].Key)
-	// 		require.Equal("We", anotherTestVars.Spec.Values[0].Value)
-	// 		require.Equal("Come", anotherTestVars.Spec.Values[1].Key)
-	// 		require.Equal("Again", anotherTestVars.Spec.Values[1].Value)
+		importTest := testVarsList.Items[0]
+		assert.Equal("Test", importTest.Type)
+		assert.Equal("RXrbV__4g", importTest.Spec.ID)
+		assert.Equal("Pokeshop - Import", importTest.Spec.Name)
+		assert.Equal("Import a Pokemon", importTest.Spec.Description)
+		assert.Equal("http", importTest.Spec.Trigger.Type)
+		assert.Equal("http://demo-api:8081/pokemon/import", importTest.Spec.Trigger.HTTPRequest.URL)
+		assert.Equal("POST", importTest.Spec.Trigger.HTTPRequest.Method)
+		assert.Equal(`{"id":52}`, importTest.Spec.Trigger.HTTPRequest.Body)
+		require.Len(importTest.Spec.Trigger.HTTPRequest.Headers, 1)
+		assert.Equal("Content-Type", importTest.Spec.Trigger.HTTPRequest.Headers[0].Key)
+		assert.Equal("application/json", importTest.Spec.Trigger.HTTPRequest.Headers[0].Value)
 
-	// 		testVars := testVarsList[1]
-	// 		require.Equal("Test", testVars.Type)
-	// 		require.Equal(".env", testVars.Spec.ID)
-	// 		require.Equal(".env", testVars.Spec.Name)
-	// 		require.Len(testVars.Spec.Values, 2)
-	// 		require.Equal("FIRST_VAR", testVars.Spec.Values[0].Key)
-	// 		require.Equal("some-value", testVars.Spec.Values[0].Value)
-	// 		require.Equal("SECOND_VAR", testVars.Spec.Values[1].Key)
-	// 		require.Equal("another_value", testVars.Spec.Values[1].Value)
+		listTest := testVarsList.Items[1]
+		assert.Equal("Test", listTest.Type)
+		assert.Equal("fH_8AulVR", listTest.Spec.ID)
+		assert.Equal("Pokeshop - List", listTest.Spec.Name)
+		assert.Equal("List Pokemon", listTest.Spec.Description)
+		assert.Equal("http", listTest.Spec.Trigger.Type)
+		assert.Equal("http://demo-api:8081/pokemon?take=20&skip=0", listTest.Spec.Trigger.HTTPRequest.URL)
+		assert.Equal("GET", listTest.Spec.Trigger.HTTPRequest.Method)
+		assert.Equal("", listTest.Spec.Trigger.HTTPRequest.Body)
+		require.Len(listTest.Spec.Trigger.HTTPRequest.Headers, 1)
+		assert.Equal("Content-Type", listTest.Spec.Trigger.HTTPRequest.Headers[0].Key)
+		assert.Equal("application/json", listTest.Spec.Trigger.HTTPRequest.Headers[0].Value)
+	})
 
-	// 		oneMoreTestVars := testVarsList[2]
-	// 		require.Equal("Test", oneMoreTestVars.Type)
-	// 		require.Equal("one-more-env", oneMoreTestVars.Spec.ID)
-	// 		require.Equal("one-more-env", oneMoreTestVars.Spec.Name)
-	// 		require.Len(oneMoreTestVars.Spec.Values, 2)
-	// 		require.Equal("This", oneMoreTestVars.Spec.Values[0].Key)
-	// 		require.Equal("Is", oneMoreTestVars.Spec.Values[0].Value)
-	// 		require.Equal("The", oneMoreTestVars.Spec.Values[1].Key)
-	// 		require.Equal("Third", oneMoreTestVars.Spec.Values[1].Value)
-	// 	})
+	t.Run("list with pretty format", func(t *testing.T) {
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
 
-	// 	t.Run("list with pretty format", func(t *testing.T) {
-	// 		// Given I am a Tracetest CLI user
-	// 		// And I have my server recently created
+		// When I try to list these tests by a valid field and in pretty format
+		// Then it should print a table with 6 lines printed: header, separator, three envs and empty line
+		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection asc --output pretty", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
 
-	// 		// When I try to list these tests by a valid field and in pretty format
-	// 		// Then it should print a table with 6 lines printed: header, separator, three envs and empty line
-	// 		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection asc --output pretty", tracetestcli.WithCLIConfig(cliConfig))
-	// 		helpers.RequireExitCodeEqual(t, result, 0)
+		lines := strings.Split(result.StdOut, "\n")
+		require.Len(lines, 5)
 
-	// 		lines := strings.Split(result.StdOut, "\n")
-	// 		require.Len(lines, 6)
+		require.Contains(lines[2], "RXrbV__4g")
+		require.Contains(lines[3], "fH_8AulVR")
+	})
 
-	// 		// due our database sorting algorithm, "another-env" comes in the front of ".env"
-	// 		// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
-	// 		require.Contains(lines[2], "another-env")
-	// 		require.Contains(lines[3], ".env")
-	// 		require.Contains(lines[4], "one-more-env")
-	// 	})
+	t.Run("list with YAML format skipping the first and taking two items", func(t *testing.T) {
+		// Given I am a Tracetest CLI user
+		// And I have my server recently created
 
-	// 	t.Run("list with YAML format skipping the first and taking two items", func(t *testing.T) {
-	// 		// Given I am a Tracetest CLI user
-	// 		// And I have my server recently created
+		// When I try to list these tests by a valid field, paging options and in YAML format
+		// Then I should receive two tests
+		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection desc --skip 1 --take 2 --output json", tracetestcli.WithCLIConfig(cliConfig))
+		helpers.RequireExitCodeEqual(t, result, 0)
 
-	// 		// When I try to list these tests by a valid field, paging options and in YAML format
-	// 		// Then I should receive two tests
-	// 		result := tracetestcli.Exec(t, "list test --sortBy name --sortDirection asc --skip 1 --take 2 --output yaml", tracetestcli.WithCLIConfig(cliConfig))
-	// 		helpers.RequireExitCodeEqual(t, result, 0)
+		testVarsList := helpers.UnmarshalJSON[types.ResourceList[types.TestResource]](t, result.StdOut)
+		require.Len(testVarsList.Items, 1)
+		require.Equal(2, testVarsList.Count) // items is the total number of items in the server
 
-	// 		testVarsList := helpers.UnmarshalYAMLSequence[types.TestResource](t, result.StdOut)
-	// 		require.Len(testVarsList, 2)
-
-	// 		// due our database sorting algorithm, "another-env" comes in the front of ".env"
-	// 		// ref https://wiki.postgresql.org/wiki/FAQ#Why_do_my_strings_sort_incorrectly.3F
-	// 		testVars := testVarsList[0]
-	// 		require.Equal("Test", testVars.Type)
-	// 		require.Equal(".env", testVars.Spec.ID)
-	// 		require.Equal(".env", testVars.Spec.Name)
-	// 		require.Len(testVars.Spec.Values, 2)
-	// 		require.Equal("FIRST_VAR", testVars.Spec.Values[0].Key)
-	// 		require.Equal("some-value", testVars.Spec.Values[0].Value)
-	// 		require.Equal("SECOND_VAR", testVars.Spec.Values[1].Key)
-	// 		require.Equal("another_value", testVars.Spec.Values[1].Value)
-
-	//		oneMoreTestVars := testVarsList[1]
-	//		require.Equal("Test", oneMoreTestVars.Type)
-	//		require.Equal("one-more-env", oneMoreTestVars.Spec.ID)
-	//		require.Equal("one-more-env", oneMoreTestVars.Spec.Name)
-	//		require.Len(oneMoreTestVars.Spec.Values, 2)
-	//		require.Equal("This", oneMoreTestVars.Spec.Values[0].Key)
-	//		require.Equal("Is", oneMoreTestVars.Spec.Values[0].Value)
-	//		require.Equal("The", oneMoreTestVars.Spec.Values[1].Key)
-	//		require.Equal("Third", oneMoreTestVars.Spec.Values[1].Value)
-	//	})
+		importTest := testVarsList.Items[0]
+		assert.Equal("Test", importTest.Type)
+		assert.Equal("RXrbV__4g", importTest.Spec.ID)
+		assert.Equal("Pokeshop - Import", importTest.Spec.Name)
+		assert.Equal("Import a Pokemon", importTest.Spec.Description)
+		assert.Equal("http", importTest.Spec.Trigger.Type)
+		assert.Equal("http://demo-api:8081/pokemon/import", importTest.Spec.Trigger.HTTPRequest.URL)
+		assert.Equal("POST", importTest.Spec.Trigger.HTTPRequest.Method)
+		assert.Equal(`{"id":52}`, importTest.Spec.Trigger.HTTPRequest.Body)
+		require.Len(importTest.Spec.Trigger.HTTPRequest.Headers, 1)
+		assert.Equal("Content-Type", importTest.Spec.Trigger.HTTPRequest.Headers[0].Key)
+		assert.Equal("application/json", importTest.Spec.Trigger.HTTPRequest.Headers[0].Value)
+	})
 }
