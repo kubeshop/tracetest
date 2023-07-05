d70dcb1d1c05de92291684c827e7cd1102c7765a
Daniel Dias <danielbpdias@gmail.com>
2023-07-05 19:15:22 -0300

    Move test deprecated notice to subcommands

--- cli/cmd/legacy_test_cmd.go
+++ cli/cmd/legacy_test_cmd.go
@@ -5,12 +5,11 @@ import (
 )
 
 var testCmd = &cobra.Command{
-	GroupID:    cmdGroupTests.ID,
-	Use:        "test",
-	Short:      "Manage your tracetest tests",
-	Long:       "Manage your tracetest tests",
-	Deprecated: "Please use `tracetest (apply|delete|export|get|list) test` commands instead.",
-	PreRun:     setupCommand(),
+	GroupID: cmdGroupTests.ID,
+	Use:     "test",
+	Short:   "Manage your tracetest tests",
+	Long:    "Manage your tracetest tests",
+	PreRun:  setupCommand(),
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
