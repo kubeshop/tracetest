db61f9b3d3aab380cfb495dcb9a9bbcd0957470a
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-19 12:36:37 -0300

    feat: add structure for agent (#2964)
    * feat: add structure for agent
    
    * add short and long description for agent command
    
    * fix build
    
    * add go.work

---
+++ agent/initialization/start.go
@@ -0,0 +1,12 @@
+package initialization
+
+import (
+	"fmt"
+
+	"github.com/kubeshop/tracetest/agent/config"
+)
+
+// Start the agent with given configuration
+func Start(config config.Config) {
+	fmt.Println("Starting agent")
+}
