95f573c871ddc01a98bc282fc03985974e194cc9
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-20 22:30:59 -0300

    feat: create client for receiving trigger requests from the server (#2970)
    * wip: connect to grpc server
    
    * feat: add connection workflow
    
    * add protobuf for trigger request
    
    * add flow to receive trigger requests
    
    * add comments and rename files
    
    * cleanup makefile
    
    * configure CI to run agent tests
    
    * fix makefile
    
    * add flow for sending trigger result back to the server

---
+++ agent/client/workflow_startup_test.go
@@ -0,0 +1,25 @@
+package client_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/kubeshop/tracetest/agent/client"
+	"github.com/kubeshop/tracetest/agent/client/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStartupFlow(t *testing.T) {
+	server := mocks.NewGrpcServer()
+
+	client, err := client.Connect(context.Background(), server.Addr())
+	require.NoError(t, err)
+
+	err = client.Start(context.Background())
+	require.NoError(t, err)
+
+	assert.NotNil(t, client.SessionConfiguration())
+	assert.Equal(t, 1*time.Second, client.SessionConfiguration().BatchTimeout)
+}
