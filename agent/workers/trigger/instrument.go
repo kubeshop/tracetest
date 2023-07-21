ebce90a8cb976b59ae981313b3ac22ec4d12d1be
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-21 17:18:34 -0300

    move executor/trigger package to agent and adapt it

---
+++ agent/workers/trigger/instrument.go
@@ -0,0 +1,98 @@
+package trigger
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kubeshop/tracetest/server/model"
+	"github.com/kubeshop/tracetest/server/test/trigger"
+	"go.opentelemetry.io/contrib/propagators/aws/xray"
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/contrib/propagators/jaeger"
+	"go.opentelemetry.io/contrib/propagators/ot"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func Instrument(tracer, triggerSpanTracer trace.Tracer, wrapped Triggerer) Triggerer {
+	return &instrumentedTriggerer{
+		tracer:            tracer,
+		triggerSpanTracer: triggerSpanTracer,
+		triggerer:         wrapped,
+	}
+}
+
+type instrumentedTriggerer struct {
+	tracer            trace.Tracer
+	triggerSpanTracer trace.Tracer
+	triggerer         Triggerer
+}
+
+func (t *instrumentedTriggerer) Type() trigger.TriggerType {
+	return trigger.TriggerType("instrumented")
+}
+
+func (t *instrumentedTriggerer) Trigger(ctx context.Context, triggerConfig trigger.Trigger, opts Options) (Response, error) {
+	_, span := t.tracer.Start(ctx, "Trigger test")
+	defer span.End()
+
+	tracestate, err := trace.ParseTraceState("tracetest=true")
+	if err != nil {
+		return Response{}, fmt.Errorf("could not create tracestate: %w", err)
+	}
+
+	spanContextConfig := trace.SpanContextConfig{
+		TraceState: tracestate,
+	}
+
+	if len(opts.TraceID) > 0 {
+		spanContextConfig.TraceID = opts.TraceID
+	}
+
+	spanContext := trace.NewSpanContext(spanContextConfig)
+
+	triggerCtx := trace.ContextWithSpanContext(context.Background(), spanContext)
+
+	triggerSpanCtx, triggerSpan := t.triggerSpanTracer.Start(triggerCtx, model.TriggerSpanName)
+	defer triggerSpan.End()
+
+	triggerSpan.SpanContext().TraceState().Insert("tracetest", "true")
+
+	tid := triggerSpan.SpanContext().TraceID()
+	sid := triggerSpan.SpanContext().SpanID()
+
+	resp, err := t.triggerer.Trigger(triggerSpanCtx, triggerConfig, opts)
+
+	resp.TraceID = tid
+	resp.SpanID = sid
+
+	attrs := []attribute.KeyValue{
+		attribute.String("tracetest.run.trigger.trace_id", tid.String()),
+		attribute.String("tracetest.run.trigger.span_id", sid.String()),
+		attribute.String("tracetest.run.trigger.test_id", string(opts.TestID)),
+		attribute.String("tracetest.run.trigger.type", string(t.triggerer.Type())),
+	}
+
+	if err != nil {
+		span.RecordError(err)
+		attrs = append(attrs, attribute.String("tracetest.run.trigger.error", err.Error()))
+	}
+
+	for k, v := range resp.SpanAttributes {
+		attrs = append(attrs, attribute.String(k, v))
+	}
+
+	span.SetAttributes(attrs...)
+
+	return resp, err
+}
+
+func propagators() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(propagation.Baggage{},
+		b3.New(),
+		jaeger.Jaeger{},
+		ot.OT{},
+		xray.Propagator{},
+		propagation.TraceContext{})
+}
---
+++ agent/workers/trigger/triggerer.go
@@ -0,0 +1,69 @@
+package trigger
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/kubeshop/tracetest/server/pkg/id"
+	"github.com/kubeshop/tracetest/server/test/trigger"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type Options struct {
+	TraceID trace.TraceID
+	TestID  id.ID
+}
+
+type Triggerer interface {
+	Trigger(context.Context, trigger.Trigger, Options) (Response, error)
+	Type() trigger.TriggerType
+}
+
+type Response struct {
+	SpanAttributes map[string]string
+	Result         trigger.TriggerResult
+	TraceID        trace.TraceID
+	SpanID         trace.SpanID
+}
+
+func NewRegsitry(tracer, triggerSpanTracer trace.Tracer) *Registry {
+	return &Registry{
+		tracer:            tracer,
+		triggerSpanTracer: triggerSpanTracer,
+		reg:               map[trigger.TriggerType]Triggerer{},
+	}
+}
+
+type Registry struct {
+	sync.Mutex
+	tracer            trace.Tracer
+	triggerSpanTracer trace.Tracer
+	reg               map[trigger.TriggerType]Triggerer
+}
+
+func (r *Registry) Add(t Triggerer) {
+	r.Lock()
+	defer r.Unlock()
+
+	r.reg[t.Type()] = t
+}
+
+var ErrTriggererTypeNotRegistered = errors.New("triggerer type not found")
+
+func (r *Registry) Get(triggererType trigger.TriggerType) (Triggerer, error) {
+	r.Lock()
+	defer r.Unlock()
+
+	t, found := r.reg[triggererType]
+	if !found {
+		return nil, fmt.Errorf(`cannot get trigger type "%s": %w`, triggererType, ErrTriggererTypeNotRegistered)
+	}
+
+	return Instrument(r.tracer, r.triggerSpanTracer, t), nil
+}
+
+func WrapInQuotes(input string, quoteChar string) string {
+	return fmt.Sprintf("%s%s%s", quoteChar, input, quoteChar)
+}
