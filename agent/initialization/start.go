1ce6b855e4f2b5d41b1936b9a2c44ae71ed7ecdc
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-24 11:26:37 -0300

    feat: send trigger requests from agent (#2975)
    * feat: add empty trigger worker
    
    * move executor/trigger package to agent and adapt it
    
    * adapt test
    
    * implement trigger
    
    * test trigger flow
    
    * send trigger response to server
    
    * add TODO

--- agent/initialization/start.go
+++ agent/initialization/start.go
@@ -1,12 +1,37 @@
 package initialization
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	"github.com/kubeshop/tracetest/agent/client"
 	"github.com/kubeshop/tracetest/agent/config"
+	"github.com/kubeshop/tracetest/agent/proto"
+	"github.com/kubeshop/tracetest/agent/workers"
 )
 
 // Start the agent with given configuration
 func Start(config config.Config) {
 	fmt.Println("Starting agent")
+	ctx := context.Background()
+
+	client, err := client.Connect(ctx, config.ServerURL,
+		client.WithAPIKey(config.APIKey),
+		client.WithAgentName(config.AgentName),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	triggerWorker := workers.NewTriggerWorker(client)
+
+	client.OnTriggerRequest(func(ctx context.Context, tr *proto.TriggerRequest) error {
+		return triggerWorker.Trigger(ctx, tr)
+	})
+
+	err = client.Start(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ agent/workers/trigger/traceid.go
@@ -0,0 +1,28 @@
+package trigger
+
+import (
+	"context"
+
+	"github.com/kubeshop/tracetest/server/test/trigger"
+)
+
+func TRACEID() Triggerer {
+	return &traceidTriggerer{}
+}
+
+type traceidTriggerer struct{}
+
+func (t *traceidTriggerer) Trigger(ctx context.Context, triggerConfig trigger.Trigger, opts *Options) (Response, error) {
+	response := Response{
+		Result: trigger.TriggerResult{
+			Type:    t.Type(),
+			TraceID: &trigger.TraceIDResponse{ID: triggerConfig.TraceID.ID},
+		},
+	}
+
+	return response, nil
+}
+
+func (t *traceidTriggerer) Type() trigger.TriggerType {
+	return trigger.TriggerTypeTraceID
+}
