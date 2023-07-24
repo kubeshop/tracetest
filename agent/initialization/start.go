ee414347d3060d47849fd399fd860e08d4a484ce
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-24 17:28:30 -0300

    pass worker functions directly to client hooks

--- agent/initialization/start.go
+++ agent/initialization/start.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/kubeshop/tracetest/agent/client"
 	"github.com/kubeshop/tracetest/agent/config"
-	"github.com/kubeshop/tracetest/agent/proto"
 	"github.com/kubeshop/tracetest/agent/workers"
 )
 
@@ -27,13 +26,8 @@ func Start(config config.Config) {
 	triggerWorker := workers.NewTriggerWorker(client)
 	pollingWorker := workers.NewPollerWorker(client)
 
-	client.OnTriggerRequest(func(ctx context.Context, tr *proto.TriggerRequest) error {
-		return triggerWorker.Trigger(ctx, tr)
-	})
-
-	client.OnPollingRequest(func(ctx context.Context, pr *proto.PollingRequest) error {
-		return pollingWorker.Poll(ctx, pr)
-	})
+	client.OnTriggerRequest(triggerWorker.Trigger)
+	client.OnPollingRequest(pollingWorker.Poll)
 
 	err = client.Start(ctx)
 	if err != nil {
