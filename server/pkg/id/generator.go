945b5f9ce2df985b9f55a91f2d3e02a114611aa2
Sebastian Choren <sebastian.choren@gmail.com>
2023-07-26 12:38:59 -0300

    remove race condiion

--- server/pkg/id/generator.go
+++ server/pkg/id/generator.go
@@ -37,14 +37,10 @@ type Generator interface {
 }
 
 func NewRandGenerator() Generator {
-	return randGenerator{
-		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
-	}
+	return randGenerator{}
 }
 
-type randGenerator struct {
-	rand *rand.Rand
-}
+type randGenerator struct{}
 
 func (g randGenerator) UUID() uuid.UUID {
 	return uuid.New()
@@ -56,12 +52,12 @@ func (g randGenerator) ID() ID {
 
 func (g randGenerator) TraceID() trace.TraceID {
 	tid := trace.TraceID{}
-	g.rand.Read(tid[:])
+	rand.New(rand.NewSource(time.Now().UnixNano())).Read(tid[:])
 	return tid
 }
 
 func (g randGenerator) SpanID() trace.SpanID {
 	sid := trace.SpanID{}
-	g.rand.Read(sid[:])
+	rand.New(rand.NewSource(time.Now().UnixNano())).Read(sid[:])
 	return sid
 }
