a340b5981e194e95ae9ea545ab6aca87d8ba0a72
Adnan Rahic <ado.raha198@gmail.com>
2023-07-26 13:12:12 +0200

    docs(getting-started): improve getting started overview and add docker compose install

--- docs/src/components/GettingStartedGuide/index.tsx
+++ docs/src/components/GettingStartedGuide/index.tsx
@@ -3,23 +3,49 @@
 import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
+import Translate from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
 
 const GettingStartedGuides = [
   {
     name: '👇 Install Tracetest',
     url: './installation',
+    description: (
+      <Translate >
+        Set up Tracetest and start trace-based testing your distributed system.
+      </Translate>
+    ),
     button: 'Set up Tracetest',
   },
   {
     name: '🙌 Open Tracetest',
     url: './open',
+    description: (
+      <Translate>
+        After installing it, open Tracetest start to creating trace-based tests.
+      </Translate>
+    ),
     button: 'Create tests',
   },
   {
     name: '🤔 Don\'t have OpenTelemetry?',
     url: './no-otel',
-    button: 'Set up tracing',
+    description: (
+      <Translate >
+        Install OpenTelemetry in 5 minutes without any code changes!
+      </Translate>
+    ),
+    button: 'Set up OTel',
+  },
+  {
+    name: '🤩 Open Source',
+    url: 'https://github.com/kubeshop/tracetest',
+    description: (
+      <Translate>
+        Check out the Tracetest GitHub repo! Please consider giving us a star! ⭐️
+      </Translate>
+    ),
+    button: 'Go to GitHub',
   },
 ];
 
@@ -27,14 +53,16 @@ interface Props {
   name: string;
   url: string;
   button: string;
+  description: JSX.Element;
 }
 
-function GettingStartedGuideCard({name, url, button}: Props) {
+function GettingStartedGuideCard({name, url, description, button}: Props) {
   return (
-    <div className="col col--4 margin-bottom--lg">
+    <div className="col col--6 margin-bottom--lg">
       <div className={clsx('card')}>
         <div className="card__body">
-          <Heading as="h4" style={{ margin: 0 }}>{name}</Heading>
+          <Heading as="h3">{name}</Heading>
+          <p>{description}</p>
         </div>
         <div className="card__footer">
           <div className="button-group button-group--block">
