28c58a9b1df3463bac571fb696c9ebf33a9a2d96
Daniel Dias <danielbpdias@gmail.com>
2023-07-26 21:47:13 -0300

    Updated example with producer

--- examples/quick-start-go-and-kafka/producer-api/main.go
+++ examples/quick-start-go-and-kafka/producer-api/main.go
@@ -6,11 +6,11 @@ import (
 	"io"
 	"net/http"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 
 	"github.com/kubeshop/tracetest/examples/quick-start-go-and-kafka/producer-api/config"
+	"github.com/kubeshop/tracetest/examples/quick-start-go-and-kafka/producer-api/streaming"
 	"github.com/kubeshop/tracetest/examples/quick-start-go-and-kafka/producer-api/telemetry"
 )
 
@@ -24,28 +24,38 @@ func main() {
 		fmt.Printf("error creating zap logger, error: %v", err)
 		return
 	}
+	defer logger.Sync()
 
-	spanExporter, err := telemetry.NewSpanExporter(ctx, currentConfig.OtelExporterEndpoint)
+	logger.Info("Setting up server...")
+
+	logger.Info("Initializing OpenTelemetry...")
+	tracer, err := telemetry.Setup(ctx, currentConfig.OtelExporterEndpoint, currentConfig.OtelServiceName)
 	if err != nil {
-		logger.Fatal("failed to initialize exporter", zap.Error(err))
+		logger.Error("Unable to setup OpenTelemetry", zap.Error(err))
+		return
 	}
+	logger.Info("OpenTelemetry initialized.")
 
-	traceProvider := telemetry.NewTraceProvider(spanExporter, currentConfig.OtelServiceName, logger)
-	defer func() { _ = traceProvider.Shutdown(ctx) }()
-	otel.SetTracerProvider(traceProvider)
-
-	tracer := traceProvider.Tracer(currentConfig.OtelServiceName)
+	logger.Info("Initializing Kafka publisher...")
+	publisher, err := streaming.GetKafkaPublisher(currentConfig.KafkaBrokerUrl, currentConfig.KafkaTopic)
+	if err != nil {
+		logger.Error("Unable to setup Kafka publisher", zap.Error(err))
+		return
+	}
+	defer publisher.Close()
+	logger.Info("Kafka publisher initialized.")
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/publish", publishHandler(tracer))
+	mux.HandleFunc("/publish", publishHandler(tracer, publisher))
 
-	fmt.Println("Starting producer-api on port 8080...")
+	logger.Info("Starting producer-api on port 8080...")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
-		logger.Error("error running server", zap.Error(err))
+		logger.Error("unable to start server", zap.Error(err))
+		return
 	}
 }
 
-func publishHandler(tracer trace.Tracer) func(http.ResponseWriter, *http.Request) {
+func publishHandler(tracer trace.Tracer, publisher *streaming.Publisher) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		if req.Method != "POST" {
 			w.WriteHeader(http.StatusNotFound)
@@ -57,10 +67,16 @@ func publishHandler(tracer trace.Tracer) func(http.ResponseWriter, *http.Request
 
 		bodyContent, err := io.ReadAll(req.Body)
 		if err != nil {
+			fmt.Printf("error when reading body: %s", err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Println(string(bodyContent))
+		err = publisher.Publish(req.Context(), bodyContent)
+		if err != nil {
+			fmt.Printf("error when publishing message: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 }
