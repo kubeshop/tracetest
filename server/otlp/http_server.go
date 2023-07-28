293b06cb5a0a771ea6a7c6294fb904cd9ff742e7
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-28 12:39:51 -0300

    apply daniel's comments to the otlp server code

--- server/otlp/http_server.go
+++ server/otlp/http_server.go
@@ -107,10 +107,8 @@ func (s httpServer) parseJson(body []byte) (*pb.ExportTraceServiceRequest, error
 }
 
 func (s httpServer) parseBody(reqBody io.ReadCloser, contentType string) (*pb.ExportTraceServiceRequest, error) {
-	var body []byte
-	if b, err := io.ReadAll(reqBody); err == nil {
-		body = b
-	} else {
+	body, err := io.ReadAll(reqBody)
+	if err != nil {
 		return nil, err
 	}
 
@@ -140,7 +138,7 @@ func newHttpResponse(w http.ResponseWriter, contentType string) httpResponse {
 func (r httpResponse) send(statusCode int, message proto.Message) error {
 	body, err := r.paseResponseBody(message)
 	if err != nil {
-		fmt.Println("Could not attach body to response", err.Error())
+		fmt.Println("could not attach body to response: %w", err)
 		return err
 	}
 
