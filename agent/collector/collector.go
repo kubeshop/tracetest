f231ad4d883c16193974dde744b4b70096a7dfd9
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-07-28 13:22:11 -0300

    feat: agent internal collector (#2997)
    * feat: add otlp and http servers with no operation yet
    
    * feat: implement a very basic collector in the agent
    
    * remove tracer from otlp_server structure
    
    * Update agent/collector/http_server.go
    
    Co-authored-by: Daniel Baptista Dias <danielbdias@users.noreply.github.com>
    
    * Update agent/collector/http_server.go
    
    Co-authored-by: Daniel Baptista Dias <danielbdias@users.noreply.github.com>
    
    * reuse otlp servers
    
    * apply daniel's comments to the otlp server code
    
    * remove dependency on ioutil from http otlp server
    
    * delete http server from agent collector
    
    * make collector ingester use otlp ingester interface
    
    ---------
    
    Co-authored-by: Daniel Baptista Dias <danielbdias@users.noreply.github.com>

---
+++ agent/collector/collector.go
@@ -0,0 +1,68 @@
+package collector
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kubeshop/tracetest/server/otlp"
+)
+
+type Config struct {
+	HTTPPort          int
+	GRPCPort          int
+	BatchTimeout      time.Duration
+	RemoteServerURL   string
+	RemoteServerToken string
+}
+
+func Start(ctx context.Context, config Config) error {
+	ingester, err := newForwardIngester(ctx, config.BatchTimeout, remoteIngesterConfig{
+		URL:   config.RemoteServerURL,
+		Token: config.RemoteServerToken,
+	})
+	if err != nil {
+		return fmt.Errorf("could not start local collector: %w", err)
+	}
+
+	grpcServer := otlp.NewGrpcServer(fmt.Sprintf("0.0.0.0:%d", config.GRPCPort), ingester)
+	httpServer := otlp.NewHttpServer(fmt.Sprintf("0.0.0.0:%d", config.HTTPPort), ingester)
+
+	onProcessTermination(func() {
+		grpcServer.Stop()
+		httpServer.Stop()
+		if stoppableIngester, ok := ingester.(stoppable); ok {
+			stoppableIngester.Stop()
+		}
+	})
+
+	go func() {
+		err := grpcServer.Start()
+		if err != nil {
+			log.Println("ERROR: could not start gRPC OTLP listener: %w", err)
+		}
+	}()
+
+	go func() {
+		err := httpServer.Start()
+		if err != nil {
+			log.Println("ERROR: could not start HTTP OTLP listener: %w", err)
+		}
+	}()
+
+	return nil
+}
+
+func onProcessTermination(callback func()) {
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+		<-c
+		callback()
+	}()
+}
