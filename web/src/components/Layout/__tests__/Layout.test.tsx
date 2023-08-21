269b745d9090ad1844c9e990c8270df9ec5062f8
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-08-21 14:07:39 -0600

    fixing test

--- web/src/components/Layout/__tests__/Layout.test.tsx
+++ web/src/components/Layout/__tests__/Layout.test.tsx
@@ -2,8 +2,7 @@ import {render} from 'test-utils';
 import Layout from '../Layout';
 
 it('Layout', async () => {
-  const {getByText, getByTestId} = render(<Layout />);
+  const {getByTestId} = render(<Layout />);
 
   expect(getByTestId('menu-link')).toBeInTheDocument();
-  expect(getByText('This')).toBeTruthy();
 });
