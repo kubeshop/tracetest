2052fa0fa97317f80e6b3b928b8e8ae9a1828248
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-08-23 18:29:54 -0300

    add tests to cover all config values are configurable via env vars

--- agent/config/config.go
+++ agent/config/config.go
@@ -12,7 +12,7 @@ import (
 type Config struct {
 	DevMode   bool   `mapstructure:"dev_mode"`
 	APIKey    string `mapstructure:"api_key"`
-	Name      string `mapstructure:"name"`
+	Name      string `mapstructure:"agent_name"`
 	ServerURL string `mapstructure:"server_url"`
 }
 
@@ -21,11 +21,7 @@ func LoadConfig() (Config, error) {
 		viper.EnvKeyReplacer(strings.NewReplacer(".", "_")),
 	)
 
-	homeFolder, err := os.UserHomeDir()
-	if err != nil {
-		return Config{}, fmt.Errorf("could not get user home folder")
-	}
-	tracetestFolder := path.Join(homeFolder, ".tracetest")
+	tracetestFolder := getTracetestFolder()
 
 	vp.SetEnvPrefix("tracetest")
 	vp.AddConfigPath(tracetestFolder)
@@ -35,16 +31,28 @@ func LoadConfig() (Config, error) {
 	vp.AutomaticEnv()
 
 	vp.SetDefault("DEV_MODE", false)
+	vp.SetDefault("AGENT_NAME", "")
+	vp.SetDefault("API_KEY", "")
 	vp.SetDefault("SERVER_URL", "https://cloud.tracetest.io")
 
 	config := Config{}
 
 	vp.ReadInConfig()
 
-	err = vp.Unmarshal(&config)
+	err := vp.Unmarshal(&config)
 	if err != nil {
 		return Config{}, fmt.Errorf("could not load config: %w", err)
 	}
 
 	return config, nil
+}
+
+func getTracetestFolder() string {
+	homeFolder, err := os.UserHomeDir()
+	if err != nil {
+		// as a fallback, just return the current folder
+		return "."
+	}
+
+	return path.Join(homeFolder, ".tracetest")
 }
---
+++ agent/config/config_test.go
@@ -0,0 +1,44 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/kubeshop/tracetest/agent/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigDefaults(t *testing.T) {
+	cfg, err := config.LoadConfig()
+
+	require.NoError(t, err)
+
+	assert.Equal(t, "", cfg.APIKey)
+	assert.Equal(t, "", cfg.Name)
+	assert.Equal(t, false, cfg.DevMode)
+	assert.Equal(t, "https://cloud.tracetest.io", cfg.ServerURL)
+}
+
+func TestConfigWithEnvs(t *testing.T) {
+	t.Cleanup(func() {
+		os.Unsetenv("TRACETEST_AGENT_NAME")
+		os.Unsetenv("TRACETEST_API_KEY")
+		os.Unsetenv("TRACETEST_DEV_MODE")
+		os.Unsetenv("TRACETEST_SERVER_URL")
+	})
+
+	os.Setenv("TRACETEST_AGENT_NAME", "my-agent-name")
+	os.Setenv("TRACETEST_API_KEY", "my-agent-api-key")
+	os.Setenv("TRACETEST_DEV_MODE", "true")
+	os.Setenv("TRACETEST_SERVER_URL", "https://custom.server.com")
+
+	cfg, err := config.LoadConfig()
+
+	require.NoError(t, err)
+
+	assert.Equal(t, "my-agent-api-key", cfg.APIKey)
+	assert.Equal(t, "my-agent-name", cfg.Name)
+	assert.Equal(t, true, cfg.DevMode)
+	assert.Equal(t, "https://custom.server.com", cfg.ServerURL)
+}
