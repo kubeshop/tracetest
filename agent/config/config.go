d4f77c65a7665a9bc6cc776cdaa8a7e38de3d79f
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-08-23 18:18:22 -0300

    feat(agent): inject env variables in agent configuration

--- agent/config/config.go
+++ agent/config/config.go
@@ -2,26 +2,46 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"path"
 	"strings"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	ServerURL string `json:"serverURL"`
-	DevMode   bool   `json:"devMode"`
-	APIKey    string `json:"apiKey"`
-	AgentName string `json:"name"`
+	DevMode   bool   `mapstructure:"dev_mode"`
+	APIKey    string `mapstructure:"api_key"`
+	Name      string `mapstructure:"name"`
+	ServerURL string `mapstructure:"server_url"`
 }
 
 func LoadConfig() (Config, error) {
-	viper.SetEnvPrefix("tracetest")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+	vp := viper.NewWithOptions(
+		viper.EnvKeyReplacer(strings.NewReplacer(".", "_")),
+	)
+
+	homeFolder, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("could not get user home folder")
+	}
+	tracetestFolder := path.Join(homeFolder, ".tracetest")
+
+	vp.SetEnvPrefix("tracetest")
+	vp.AddConfigPath(tracetestFolder)
+	vp.AddConfigPath("tracetest-agent.yaml")
+	vp.SetConfigName("agent")
+	vp.SetConfigType("env")
+	vp.AutomaticEnv()
+
+	vp.SetDefault("DEV_MODE", false)
+	vp.SetDefault("SERVER_URL", "https://cloud.tracetest.io")
 
 	config := Config{}
 
-	err := viper.Unmarshal(&config)
+	vp.ReadInConfig()
+
+	err = vp.Unmarshal(&config)
 	if err != nil {
 		return Config{}, fmt.Errorf("could not load config: %w", err)
 	}
