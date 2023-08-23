1c4af241852dd40d28c97c07fb162b3437af1875
Sebastian Choren <sebastian.choren@gmail.com>
2023-08-23 16:40:39 -0300

    add k6 test

---
+++ testing/load/load-test.js
@@ -0,0 +1,68 @@
+
+import { check } from "k6";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.2/index.js";
+import { Http, Tracetest } from "k6/x/tracetest";
+import { sleep } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "5m", target: 5 },
+    { duration: "5m", target: 10 },
+    { duration: "5m", target: 5 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+  },
+};
+
+const tracetest = Tracetest({
+  serverUrl: "http://localhost:11633",
+});
+const testId = "kc_MgKoVR";
+let pokemonId = 6; // charizad
+const http = new Http();
+const url = "http://localhost:8081/pokemon/import";
+
+export default function () {
+  const payload = JSON.stringify({
+    id: pokemonId,
+  });
+  const params = {
+    tracetest: {
+      testId,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = http.post(url, payload, params);
+
+  check(response, {
+    "is status 200": (r) => r.status === 200,
+    "body matches de id": (r) => JSON.parse(r.body).id === pokemonId,
+  });
+
+  pokemonId = pokemonId + 1;
+  sleep(1);
+}
+
+// enable this to return a non-zero status code if a tracetest test fails
+export function teardown() {
+  tracetest.validateResult();
+}
+
+export function handleSummary(data) {
+  // combine the default summary with the tracetest summary
+  const tracetestSummary = tracetest.summary();
+  const defaultSummary = textSummary(data);
+  const summary = `
+    ${defaultSummary}
+    ${tracetestSummary}
+  `;
+
+  return {
+    stdout: summary,
+    "tracetest.json": tracetest.json(),
+  };
+}
---
+++ testing/load/run.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+TRACETEST_CLI=${TRACETEST_CLI:-"tracetest"}
+cmdExitCode=$("$TRACETEST_CLI" &> /dev/null; echo $?)
+if [ $cmdExitCode -ne 0 ]; then
+  echo "\$TRACETEST_CLI not set to executable. set to $TRACETEST_CLI";
+  exit 2
+fi
+
+TARGET_URL=${TARGET_URL:-"http://localhost:11633"}
+if [  "$TARGET_URL" = "" ]; then
+  echo "\$TARGET_URL not set";
+  exit 2
+fi
+
+DOCKER_COMPOSE="docker compose -f infra/docker-compose.yaml -f ../../examples/docker-compose.demo.yaml"
+TRACETEST="tracetest -s $TARGET_URL"
+
+DOCKER_LOG=/tmp/load-test.log
+# printDockerLog() {
+#   $DOCKER_COMPOSE kill
+#   echo
+#   echo "Error occured. Printing docker log ($DOCKER_LOG):"
+#   cat $DOCKER_LOG
+# }
+
+# print docker log on any error
+# trap 'printDockerLog' ERR
+
+$DOCKER_COMPOSE up > $DOCKER_LOG 2>&1 &
+../../scripts/wait-for-port.sh 11633
+../../scripts/wait-for-port.sh 8081
+sleep 5
+$TRACETEST apply test -f tracetest-test.yaml
+
+rm -f ./k6
+currentDir=$(pwd)
+dir=$(mktemp -d)
+cd $dir
+go install go.k6.io/xk6/cmd/xk6@latest
+xk6 build v0.42.0 --with github.com/kubeshop/xk6-tracetest \
+  --replace go.buf.build/grpc/go/prometheus/prometheus=buf.build/gen/go/prometheus/prometheus/protocolbuffers/go@latest \
+  --replace go.buf.build/grpc/go/gogo/protobuf=buf.build/gen/go/gogo/protobuf/protocolbuffers/go@latest
+mv ./k6 $currentDir
+cd $currentDir
+
+./k6 run load-test.js -o xk6-tracetest
+
+$DOCKER_COMPOSE down
