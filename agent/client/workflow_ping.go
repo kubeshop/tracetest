e603f4ec712069e3b284b5458adcfba35bf38cd9
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-09-13 14:32:59 -0600

    cleanup

--- agent/client/workflow_ping.go
+++ agent/client/workflow_ping.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/kubeshop/tracetest/agent/proto"
@@ -10,11 +9,10 @@ import (
 
 func (c *Client) startHearthBeat(ctx context.Context) error {
 	client := proto.NewOrchestratorClient(c.conn)
-	ticker := time.NewTicker(2 * time.Second)
+	ticker := time.NewTicker(2 * time.Minute)
 
 	go func() {
 		for range ticker.C {
-			fmt.Println("@@PING")
 			client.Ping(ctx, c.sessionConfig.AgentIdentification)
 		}
 	}()
