34cbee71796c71309af83981ac05463fad285763
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-09-27 14:50:03 -0300

    fix: allow version to be specified in parameter

--- k8s/agent/deploy-agent.sh
+++ k8s/agent/deploy-agent.sh
@@ -1,5 +1,29 @@
 NAMESPACE=$1
 API_KEY=$2
+AGENT_VERSION="${3:-latest}"
+FILE_PATH="https://raw.githubusercontent.com/kubeshop/tracetest/k8s/agent/deploy-agent.yaml"
+
+showUsageAndExit() {
+    echo "Usage: ./script <namespace> <api-key> (<version>)?"
+    echo "Examples:"
+    echo "./script tracetest my-api-key"
+    echo "./script my-namespace my-api-key v0.13.9"
+
+    exit 1
+}
+
+if [ -z "${NAMESPACE}" ]; then
+    echo "Error: Namespace is required"
+    showUsageAndExit
+fi
+
+if [ -z "${API_KEY}" ]; then
+    echo "Error: API key is required"
+    showUsageAndExit
+fi
 
 kubectl create -n $NAMESPACE secret generic tracetest-agent-secret --from-literal=api-key=$API_KEY
-kubectl apply -n $NAMESPACE -f https://raw.githubusercontent.com/kubeshop/tracetest/main/k8s/agent/deploy-agent.yml
+MANIFEST=$(curl $FILE_PATH)
+MANIFEST_WITH_VERSION=$(echo $MANIFEST | sed "s/:TAG/:$AGENT_VERSION/g")
+
+echo $MANIFEST_WITH_VERSION | kubectl apply -n $NAMESPACE
