6844e5c39f1ce55e284665519a943e849b4a7daa
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2023-09-27 15:17:10 -0300

    feat(agent): k8s deployment script (#3190)
    * feat(agent): k8s deployment script
    
    * tag latest image
    
    * update tag with placeholder
    
    * fix: allow version to be specified in parameter
    
    * fix: kubectl apply from pipe

---
+++ k8s/agent/deploy-agent.sh
@@ -0,0 +1,26 @@
+NAMESPACE=$1
+API_KEY=$2
+AGENT_VERSION="${3:-latest}"
+FILE_PATH="https://raw.githubusercontent.com/kubeshop/tracetest/k8s/agent/deploy-agent.yaml"
+
+showUsageAndExit() {
+    echo "Usage: ./script <namespace> <api-key> (<version>)?"
+    echo "Examples:"
+    echo "./script tracetest my-api-key"
+    echo "./script my-namespace my-api-key v0.13.9"
+
+    exit 1
+}
+
+if [ -z "${NAMESPACE}" ]; then
+    echo "Error: Namespace is required"
+    showUsageAndExit
+fi
+
+if [ -z "${API_KEY}" ]; then
+    echo "Error: API key is required"
+    showUsageAndExit
+fi
+
+kubectl create -n $NAMESPACE secret generic tracetest-agent-secret --from-literal=api-key=$API_KEY
+curl $FILE_PATH | sed "s/:TAG/:$AGENT_VERSION/g" | kubectl apply -n $NAMESPACE -f -
