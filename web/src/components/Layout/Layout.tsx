6ba4cbc4a041354327c0b4a3b4713c6d428cc209
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-10-06 16:26:03 -0500

    feat(frontend): add menu bottom section

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -14,6 +14,7 @@ import MissingVariablesModalProvider from 'providers/MissingVariablesModal/Missi
 import NotificationProvider from 'providers/Notification/Notification.provider';
 import {ConfigMode} from 'types/DataStore.types';
 import * as S from './Layout.styled';
+import MenuBottom from './MenuBottom';
 
 export type TCustomHeader = typeof Header;
 
@@ -85,6 +86,7 @@ const Layout = ({hasMenu = false}: IProps) => {
                       </S.MenuContainer>
 
                       <S.MenuContainer>
+                        <MenuBottom />
                         <Menu
                           defaultSelectedKeys={[
                             footerMenuItems.findIndex(value => value.path === pathname).toString() || '0',
