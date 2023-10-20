900f483a782b78405704df90b9ac67d5bb295067
Sebastian Choren <schoren@users.noreply.github.com>
2023-10-20 15:07:02 -0300

    Fix agent docker (#3288)

---
+++ agent/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Validate that TRACETEST_API_KEY is not empty
+if [ -z "$TRACETEST_API_KEY" ]; then
+  echo "Error: TRACETEST_API_KEY environment variable is empty." >&2
+  exit 1
+fi
+
+# Execute tracetest with the API key and any additional arguments
+exec tracetest start --api-key "$TRACETEST_API_KEY" "$@"
