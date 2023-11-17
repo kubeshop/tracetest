a2922de929f988317dc0f83e11f849e899a9f490
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-11-17 13:24:08 -0600

    fix: Deeplink Base Url

--- web/src/services/DeepLink.service.ts
+++ web/src/services/DeepLink.service.ts
@@ -1,17 +1,23 @@
 import {TVariableSetValue} from 'models/VariableSet.model';
 import Test from 'models/Test.model';
-import {getServerBaseUrl} from '../utils/Common';
+import {getServerBaseUrl} from 'utils/Common';
 
 export type TDeepLinkConfig = {
   variables: TVariableSetValue[];
   useVariableSetId: boolean;
   test: Test;
   variableSetId?: string;
+  baseUrl?: string;
 };
 
 const DeepLinkService = () => ({
-  getLink({variables, useVariableSetId, test: {id: testId}, variableSetId}: TDeepLinkConfig) {
-    const baseUrl = getServerBaseUrl();
+  getLink({
+    baseUrl = getServerBaseUrl(),
+    variables,
+    useVariableSetId,
+    test: {id: testId},
+    variableSetId,
+  }: TDeepLinkConfig) {
     const filteredVariables = variables.filter(variable => !!variable && variable.key);
     const stringVariables = encodeURI(JSON.stringify(filteredVariables));
 
