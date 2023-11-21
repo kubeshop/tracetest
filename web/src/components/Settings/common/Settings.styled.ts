bab89be9f76df64f2c951d5c11c1503da16ac76a
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-11-21 17:33:32 -0500

    fix(frontend): fix minor ui styles

--- web/src/components/Settings/common/Settings.styled.ts
+++ web/src/components/Settings/common/Settings.styled.ts
@@ -43,10 +43,10 @@ export const SwitchContainer = styled.div`
   align-items: center;
   display: flex;
   gap: 8px;
-  margin-bottom: 12px;
 `;
 
 export const SwitchLabel = styled.label<{$disabled?: boolean}>`
   color: ${({$disabled, theme}) => ($disabled ? theme.color.textLight : theme.color.text)};
   cursor: ${({$disabled}) => ($disabled ? 'not-allowed' : 'pointer')};
+  margin-bottom: 24px;
 `;
