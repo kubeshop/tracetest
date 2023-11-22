7a63fc7189a3ed2daf0e87362fb878cb65153ba9
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-11-22 12:33:29 -0500

    fix(frontend): fix default summary time

--- web/src/models/Summary.model.ts
+++ web/src/models/Summary.model.ts
@@ -1,4 +1,5 @@
 import {TTestSchemas} from 'types/Common.types';
+import Date from 'utils/Date';
 
 export type TRawTestSummary = TTestSchemas['TestSummary'];
 type Summary = {
@@ -12,15 +13,19 @@ type Summary = {
   };
 };
 
-const Summary = (summary: TRawTestSummary = {}): Summary => ({
-  runs: summary.runs ?? 0,
-  hasRuns: !!summary.runs && summary.runs > 0,
-  lastRun: {
-    time: summary.lastRun?.time ?? '',
-    passes: summary.lastRun?.passes ?? 0,
-    fails: summary.lastRun?.fails ?? 0,
-    analyzerScore: summary.lastRun?.analyzerScore ?? 0,
-  },
-});
+const Summary = (summary: TRawTestSummary = {}): Summary => {
+  const time = summary.lastRun?.time ?? '';
+
+  return {
+    runs: summary.runs ?? 0,
+    hasRuns: !!summary.runs && summary.runs > 0,
+    lastRun: {
+      time: Date.isDefaultDate(time) ? '' : time,
+      passes: summary.lastRun?.passes ?? 0,
+      fails: summary.lastRun?.fails ?? 0,
+      analyzerScore: summary.lastRun?.analyzerScore ?? 0,
+    },
+  };
+};
 
 export default Summary;
