1d86ee15b38b47d6d9a2950226a635507d67ac48
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2023-12-05 15:31:27 -0500

    fix: default path in router

--- web/src/components/Router/Router.tsx
+++ web/src/components/Router/Router.tsx
@@ -1,5 +1,6 @@
 import {Navigate, Route, Routes} from 'react-router-dom';
 
+import Layout from 'components/Layout/Layout';
 import VariableSet from 'pages/VariableSet';
 import Home from 'pages/Home';
 import TestSuites from 'pages/TestSuites';
@@ -11,32 +12,36 @@ import TestSuiteRunOverview from 'pages/TestSuiteRunOverview';
 import TestSuiteRunAutomate from 'pages/TestSuiteRunAutomate';
 import AutomatedTestRun from 'pages/AutomatedTestRun';
 import CreateTest from 'pages/CreateTest';
-import Layout from 'components/Layout/Layout';
+import {useDashboard} from 'providers/Dashboard/Dashboard.provider';
 
-const Router = () => (
-  <Routes>
-    <Route element={<Layout hasMenu />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/testsuites" element={<TestSuites />} />
-      <Route path="/variablesets" element={<VariableSet />} />
-      <Route path="/settings" element={<Settings />} />
-    </Route>
+const Router = () => {
+  const {baseUrl} = useDashboard();
 
-    <Route element={<Layout />}>
-      <Route path="/test/create/:triggerType" element={<CreateTest />} />
-      <Route path="/test/:testId" element={<Test />} />
-      <Route path="/test/:testId/run/:runId" element={<RunDetail />} />
-      <Route path="/test/:testId/run/:runId/:mode" element={<RunDetail />} />
-      <Route path="/test/:testId/run" element={<AutomatedTestRun />} />
+  return (
+    <Routes>
+      <Route element={<Layout hasMenu />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/testsuites" element={<TestSuites />} />
+        <Route path="/variablesets" element={<VariableSet />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
 
-      <Route path="/testsuite/:testSuiteId" element={<TestSuite />} />
-      <Route path="/testsuite/:testSuiteId/run/:runId" element={<TestSuiteRunOverview />} />
-      <Route path="/testsuite/:testSuiteId/run/:runId/overview" element={<TestSuiteRunOverview />} />
-      <Route path="/testsuite/:testSuiteId/run/:runId/automate" element={<TestSuiteRunAutomate />} />
-    </Route>
+      <Route element={<Layout />}>
+        <Route path="/test/create/:triggerType" element={<CreateTest />} />
+        <Route path="/test/:testId" element={<Test />} />
+        <Route path="/test/:testId/run/:runId" element={<RunDetail />} />
+        <Route path="/test/:testId/run/:runId/:mode" element={<RunDetail />} />
+        <Route path="/test/:testId/run" element={<AutomatedTestRun />} />
 
-    <Route path="*" element={<Navigate to="/" />} />
-  </Routes>
-);
+        <Route path="/testsuite/:testSuiteId" element={<TestSuite />} />
+        <Route path="/testsuite/:testSuiteId/run/:runId" element={<TestSuiteRunOverview />} />
+        <Route path="/testsuite/:testSuiteId/run/:runId/overview" element={<TestSuiteRunOverview />} />
+        <Route path="/testsuite/:testSuiteId/run/:runId/automate" element={<TestSuiteRunAutomate />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to={baseUrl} />} />
+    </Routes>
+  );
+};
 
 export default Router;
