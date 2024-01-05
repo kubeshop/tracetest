bc29afcfacc1a186c5acbaee5d2861da248b2ca6
xoscar <oscar-rreyes1@hotmail.com>
2024-01-05 10:36:10 -0600

    fixing messaging

--- web/src/components/RunDetailAutomateMethods/methods/GithubActions/GithubActions.tsx
+++ web/src/components/RunDetailAutomateMethods/methods/GithubActions/GithubActions.tsx
@@ -1,6 +1,6 @@
 import {Typography} from 'antd';
 import {getServerBaseUrl} from 'utils/Common';
-import {GITHUB_ACTION_URL} from 'constants/Common.constants';
+import {GITHUB_ACTION_URL, CLI_DOCS_URL} from 'constants/Common.constants';
 import {withCustomization} from 'providers/Customization';
 import {FramedCodeBlock} from 'components/CodeBlock';
 import * as S from './GithubActions.styled';
@@ -18,8 +18,11 @@ const GithubActions = () => {
         <S.Title>GitHub Action Configuration</S.Title>
       </S.TitleContainer>
       <Typography.Paragraph>
-        Integrate Tracetest into your GitHub pipeline by adding this snippet to your workflow steps to utilize Tracetest
-        CLI and Agent for test runs:
+        Integrate Tracetest into your GitHub pipeline by adding this snippet to your workflow steps to utilize{' '}
+        <a href={CLI_DOCS_URL} target="__blank">
+          Tracetest CLI
+        </a>{' '}
+        for test runs:
       </Typography.Paragraph>
       <FramedCodeBlock title="Snippet" language="yaml" value={actionConfig} minHeight="120px" maxHeight="120px" />
       <S.Subtitle type="secondary">
