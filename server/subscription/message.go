339874c044c7efe8765d119b8f20c0623d9b16e7
Sebastian Choren <schoren@users.noreply.github.com>
2024-01-15 17:18:27 -0300

    Update server/subscription/message.go
    Co-authored-by: Matheus Nogueira <matheus.nogueira2008@gmail.com>

--- server/subscription/message.go
+++ server/subscription/message.go
@@ -50,38 +50,3 @@ func DecodeMessage(data []byte) (Message, error) {
 	return m, nil
 }
 
-// func decodeTimeFunc(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
-// 	// From https://github.com/mitchellh/mapstructure/issues/159#issuecomment-482201507
-// 	if t != reflect.TypeOf(time.Time{}) {
-// 		return data, nil
-// 	}
-
-// 	switch f.Kind() {
-// 	case reflect.String:
-// 		return time.Parse(time.RFC3339, data.(string))
-// 	case reflect.Float64:
-// 		return time.Unix(0, int64(data.(float64))*int64(time.Millisecond)), nil
-// 	case reflect.Int64:
-// 		return time.Unix(0, data.(int64)*int64(time.Millisecond)), nil
-// 	default:
-// 		return data, nil
-// 	}
-// }
-
-// func (m Message) DecodeContent(output interface{}) error {
-// 	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-// 		Metadata: nil,
-// 		DecodeHook: mapstructure.ComposeDecodeHookFunc(
-// 			decodeTimeFunc,
-// 		),
-// 		Result: output,
-// 	})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if err := decoder.Decode(m.Content); err != nil {
-// 		return err
-// 	}
-// 	return err
-// }
