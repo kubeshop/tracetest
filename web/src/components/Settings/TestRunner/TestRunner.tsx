01c297418ed7b13405964f81d76800fd9b9061c9
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-01-24 17:28:39 -0500

    feat(frontend): add help info in settings pages (#3558)

--- web/src/components/Settings/TestRunner/TestRunner.tsx
+++ web/src/components/Settings/TestRunner/TestRunner.tsx
@@ -6,12 +6,13 @@ import TestRunnerForm from './TestRunnerForm';
 const TestRunner = () => {
   return (
     <S.Container>
+      <S.Title level={2}>Test Runner</S.Title>
       <S.Description>
-        <p>The Test Runner allows you to configure the behavior used to execute your tests and generate the results</p>
+        <p>The Test Runner allows you to configure the behavior used to execute your tests and generate the results.</p>
         <DocsBanner>
-          Need more information about Test Runner?{' '}
+          Need more information about the Test Runner?{' '}
           <a href={TEST_RUNNER_DOCUMENTATION_URL} target="_blank">
-            Go to our docs
+            Learn more in our docs
           </a>
         </DocsBanner>
       </S.Description>
