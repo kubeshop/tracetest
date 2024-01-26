6c32413b74e303f7389bd13738d52ae6c14c337b
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-01-26 12:38:15 -0500

    feat(frontend): add total spans in awaiting trace status (#3569)

--- web/src/components/TestState/TestStateProgress.tsx
+++ web/src/components/TestState/TestStateProgress.tsx
@@ -4,12 +4,16 @@ import * as S from './TestState.styled';
 interface IProps {
   label: string;
   percent: number;
+  showInfo?: boolean;
+  info?: string;
 }
 
-const TestStateProgress = ({label, percent}: IProps) => (
+const TestStateProgress = ({label, percent, showInfo, info}: IProps) => (
   <S.Container hasMinWidth>
     <Progress percent={percent} showInfo={false} status="active" strokeLinecap="square" strokeWidth={6} />
-    <S.Text>{label}</S.Text>
+    <S.Text>
+      {label} {showInfo && info}
+    </S.Text>
   </S.Container>
 );
 
