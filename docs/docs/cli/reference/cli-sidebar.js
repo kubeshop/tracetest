0e123e02f4d64532e0144865aa8e6c48bf6a5393
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-01-30 14:37:10 -0600

    chore(docs): Migrating to vercel (#3583)
    updating docgen

---
+++ docs/docs/cli/reference/cli-sidebar.js
@@ -0,0 +1,97 @@
+
+/** @type {import('@docusaurus/plugin-content-docs/lib/sidebars/types').SidebarItem[]} */
+const pages = [
+    
+    {
+        type: "doc",
+		label: "tracetest",
+		id: "cli/reference/tracetest"
+	},
+    {
+        type: "doc",
+		label: "tracetest apply",
+		id: "cli/reference/tracetest_apply"
+	},
+    {
+        type: "doc",
+		label: "tracetest completion",
+		id: "cli/reference/tracetest_completion"
+	},
+    {
+        type: "doc",
+		label: "tracetest completion bash",
+		id: "cli/reference/tracetest_completion_bash"
+	},
+    {
+        type: "doc",
+		label: "tracetest completion fish",
+		id: "cli/reference/tracetest_completion_fish"
+	},
+    {
+        type: "doc",
+		label: "tracetest completion powershell",
+		id: "cli/reference/tracetest_completion_powershell"
+	},
+    {
+        type: "doc",
+		label: "tracetest completion zsh",
+		id: "cli/reference/tracetest_completion_zsh"
+	},
+    {
+        type: "doc",
+		label: "tracetest configure",
+		id: "cli/reference/tracetest_configure"
+	},
+    {
+        type: "doc",
+		label: "tracetest dashboard",
+		id: "cli/reference/tracetest_dashboard"
+	},
+    {
+        type: "doc",
+		label: "tracetest delete",
+		id: "cli/reference/tracetest_delete"
+	},
+    {
+        type: "doc",
+		label: "tracetest export",
+		id: "cli/reference/tracetest_export"
+	},
+    {
+        type: "doc",
+		label: "tracetest get",
+		id: "cli/reference/tracetest_get"
+	},
+    {
+        type: "doc",
+		label: "tracetest list",
+		id: "cli/reference/tracetest_list"
+	},
+    {
+        type: "doc",
+		label: "tracetest run",
+		id: "cli/reference/tracetest_run"
+	},
+    {
+        type: "doc",
+		label: "tracetest server",
+		id: "cli/reference/tracetest_server"
+	},
+    {
+        type: "doc",
+		label: "tracetest server install",
+		id: "cli/reference/tracetest_server_install"
+	},
+    {
+        type: "doc",
+		label: "tracetest start",
+		id: "cli/reference/tracetest_start"
+	},
+    {
+        type: "doc",
+		label: "tracetest version",
+		id: "cli/reference/tracetest_version"
+	},
+]
+
+module.exports = pages;
