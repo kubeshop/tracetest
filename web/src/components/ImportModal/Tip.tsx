caa7bb62e0db5840ea279b57f3dd40c6c7af41ef
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-03-05 11:08:45 -0500

    feat(frontend): add variable set help info (#3707)

--- web/src/components/ImportModal/Tip.tsx
+++ web/src/components/ImportModal/Tip.tsx
@@ -1,12 +1,11 @@
-import {BulbOutlined} from '@ant-design/icons';
 import {Typography} from 'antd';
 
 const Tip = () => (
   <>
-    <Typography.Title level={3} type="secondary">
-      <BulbOutlined /> What are the supported formats?
-    </Typography.Title>
-    <Typography.Text type="secondary">We support Tracetest Definition, cURL & Postman. OpenAPI is coming soon!</Typography.Text>
+    <Typography.Title level={3}>💡 What are the supported formats?</Typography.Title>
+    <Typography.Text type="secondary">
+      We support Tracetest Definition, cURL & Postman. OpenAPI is coming soon!
+    </Typography.Text>
   </>
 );
 
