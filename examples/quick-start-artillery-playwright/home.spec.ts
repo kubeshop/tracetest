1545f2be2e6a8b3b367d92725b85c1ba04ce857b
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-03-08 11:07:20 -0600

    chore(examples): Adding Artillery + Playwright + Tracetest example (#3709)
    * chore(examples): Adding Artillery + Playwright + Tracetest example
    
    * docs(recipes): add pw + artillery + tt
    
    ---------
    
    Co-authored-by: Adnan Rahic <ado.raha198@gmail.com>

---
+++ examples/quick-start-artillery-playwright/home.spec.ts
@@ -0,0 +1,65 @@
+import { Page } from 'playwright';
+import { expect } from '@playwright/test';
+import Tracetest from '@tracetest/playwright';
+import { config } from 'dotenv';
+
+config();
+
+const { TRACETEST_API_TOKEN = '' } = process.env;
+
+const definition = `
+type: Test
+spec:
+  id: artillery-playwight-import-pokemon
+  name: "Artillery Playwright - Import Pokemon"
+  trigger:
+    type: playwright
+  specs:
+    - selector: span[tracetest.span.type="general" name = "validate request"] span[tracetest.span.type="http"]
+      name: "All HTTP Spans: Status  code is 200"
+      assertions:
+        - attr:http.status_code = 200
+    - selector: span[tracetest.span.type="http" name="GET" http.method="GET"]
+      assertions:
+        - attr:http.route = "/api/v2/pokemon/\${var:POKEMON_ID}"
+    - selector: span[tracetest.span.type="database"]
+      name: "All Database Spans: Processing time is less than 1s"
+      assertions:
+        - attr:tracetest.span.duration < 1s
+  outputs:
+    - name: DATABASE_POKEMON_ID
+      selector: span[tracetest.span.type="database" name="create postgres.pokemon" db.system="postgres" db.name="postgres" db.user="postgres" db.operation="create" db.sql.table="pokemon"]
+      value: attr:db.result | json_path '$.id'
+`;
+
+export async function importPokemon(page: Page) {
+  const tracetest = await Tracetest({ apiToken: TRACETEST_API_TOKEN });
+  const title = 'Artillery Playwright - Import Pokemon';
+  const pokemonId = Math.floor(Math.random() * 101).toString();
+  await page.goto('/');
+
+  await tracetest?.setOptions({
+    [title]: {
+      definition,
+      runInfo: {
+        variables: [
+          {
+            key: 'POKEMON_ID',
+            value: pokemonId,
+          },
+        ],
+      },
+    },
+  });
+
+  await tracetest?.capture(title, page);
+
+  expect(await page.getByText('Pokeshop')).toBeTruthy();
+
+  await page.click('text=Import');
+
+  await page.getByLabel('ID').fill(pokemonId);
+  await page.getByRole('button', { name: 'OK', exact: true }).click();
+
+  await tracetest?.summary();
+}
--- examples/quick-start-artillery/README.md
+++ examples/quick-start-artillery/README.md
@@ -2,4 +2,4 @@
 
 > [Read the detailed recipe for setting up Tracetest + Artillery in our documentation.](https://docs.tracetest.io/tools-and-integrations/artillery)
 
-This is a quick start showing how you can create programs to run trace-based tests from your Artillery setup utilizing the tracetest engine and plugin. The example shows how to define the Tracetest tests, execute them, and wait for results to be ready. [Read the Typescript Quick Start Docs](https://docs.tracetest.io/tools-and-integrations/artillery) to see how to run this example.
+This is a quick start showing how you can create programs to run trace-based tests from your Artillery setup utilizing the tracetest engine and plugin. The example shows how to define the Tracetest tests, execute them, and wait for results to be ready. [Read the Tracetest + Artillery Quick Start Docs](https://docs.tracetest.io/tools-and-integrations/artillery) to see how to run this example.
