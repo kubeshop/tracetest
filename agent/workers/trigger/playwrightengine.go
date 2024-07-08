cd5d7eb04c924a2502b67d2aa904b14ecd9217f2
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-07-08 16:18:15 -0600

    fix(agent): fixing agent image for playwrightt

--- agent/workers/trigger/playwrightengine.go
+++ agent/workers/trigger/playwrightengine.go
@@ -81,6 +81,18 @@ func start(traceId, spanId, url, method string) (string, error) {
 		return "", err
 	}
 
+	if os.Getenv("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD") != "1" {
+		res, err := execCommand(
+			app,
+			"playwright",
+			"install",
+		)
+
+		if err != nil {
+			return "", fmt.Errorf("error installing playwright: %s, %w", res, err)
+		}
+	}
+
 	path, err := filepath.Abs(fmt.Sprintf("%s/%s", wd, scriptPath))
 	if err != nil {
 		return "", err
