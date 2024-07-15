f9e40d94db314c2adc04b5f76e11eee8131e2ae7
Sebastian Choren <sebastian.choren@gmail.com>
2024-07-15 16:31:44 -0300

    feat(agent): allow insecure controlplane on demand

--- cli/config/api.go
+++ cli/config/api.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/http"
+	"os"
 	"strings"
 
 	"github.com/kubeshop/tracetest/cli/analytics"
@@ -23,6 +26,22 @@ func GetAPIClient(cliConfig Config) *openapi.APIClient {
 	config.AddDefaultHeader("x-organization-id", cliConfig.OrganizationID)
 	config.AddDefaultHeader("x-environment-id", cliConfig.EnvironmentID)
 	config.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", cliConfig.Jwt))
+	if os.Getenv("TRACETEST_DEV_FORCE_URL") == "true" {
+		if config.HTTPClient == nil {
+			config.HTTPClient = http.DefaultClient
+		}
+		if config.HTTPClient.Transport == nil {
+			config.HTTPClient.Transport = http.DefaultTransport
+		}
+
+		if t, ok := config.HTTPClient.Transport.(*http.Transport); ok {
+			if t.TLSClientConfig == nil {
+				t.TLSClientConfig = &tls.Config{}
+			}
+
+			t.TLSClientConfig.InsecureSkipVerify = true
+		}
+	}
 
 	config.Scheme = cliConfig.Scheme
 	config.Host = strings.TrimSuffix(cliConfig.Endpoint, "/")
