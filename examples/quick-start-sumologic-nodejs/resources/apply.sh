94eea7dbee5b6cb3d7db6acf3209a4025f22c870
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-08-13 15:08:05 -0600

    updating sumo logic example

--- examples/quick-start-sumologic-nodejs/resources/apply.sh
+++ examples/quick-start-sumologic-nodejs/resources/apply.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+
+set -e
+
+TOKEN=$TRACETEST_TOKEN
+ENVIRONMENT_ID=$TRACETEST_ENVIRONMENT_ID
+ACCESS_ID=$SUMO_LOGIC_ACCESS_ID
+ACCESS_KEY=$SUMO_LOGIC_ACCESS_KEY
+
+apply() {
+  echo "Configuring TraceTest"
+  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID
+
+  echo "
 ---
 type: DataStore
 spec:
@@ -12,5 +26,13 @@ spec:
     # Create your ID and Key under Administration > Security > Access Keys
     # in your Sumo Logic account:
     # https://help.sumologic.com/docs/manage/security/access-keys/#create-your-access-key
-    accessID: "your-access-id"
-    accessKey: "your-access-key"
+    accessID: ${ACCESS_ID}
+    accessKey: ${ACCESS_KEY}
+" > /resources/datastore.yaml
+
+  echo "Applying Resources"
+  tracetest apply datastore -f /resources/datastore.yaml
+  tracetest apply test -f /resources/test.yaml
+}
+
+apply
