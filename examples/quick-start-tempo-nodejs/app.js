298aef1c7918e2fc7f0c391ece3b075008b263f9
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-08-14 09:15:43 -0600

    chore(docs/examples): Updating Tempo X Node.js (#3960)
    * chore(docs/examples): Updating Tempo X Node.js
    
    * updating pokeshop grafana tempo example/recipe
    
    * updating pokeshop grafana tempo example/recipe
    
    * updating grafana tempo nodejs examples
    
    * updating grafana cloud tempo pokeshop
    
    * PR suggestions updates

--- examples/quick-start-tempo-nodejs/app.js
+++ examples/quick-start-tempo-nodejs/app.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
 app.get("/", (req, res) => {
-  res.send("Hello World")
-})
+  res.send("Hello World");
+});
+
 app.listen(8080, () => {
-  console.log(`Listening for requests on http://localhost:8080`)
-})
+  console.log(`Listening for requests on http://localhost:8080`);
+});
