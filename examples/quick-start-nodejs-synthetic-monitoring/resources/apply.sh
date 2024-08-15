668fca6b46d762010b931aad3a260fd81889fc19
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-08-15 13:06:36 -0600

    chore(examples/docs): Updating Synth Monitoring Examples

---
+++ examples/quick-start-nodejs-synthetic-monitoring/resources/apply.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+TOKEN=$TRACETEST_TOKEN
+ENVIRONMENT_ID=$TRACETEST_ENVIRONMENT_ID
+
+apply() {
+  echo "Configuring TraceTest"
+  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID
+
+  echo "Applying Resources"
+  tracetest apply datastore -f /resources/datastore.yaml
+  tracetest apply test -f /resources/test.yaml
+  tracetest apply monitor -f /resources/monitor.yaml
+}
+
+apply
