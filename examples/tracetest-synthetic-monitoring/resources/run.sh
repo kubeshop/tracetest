a0b2475c6ebf19b4819212cf21853c054104d340
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-08-16 12:13:38 -0600

    chore(examples/docs): Updating Synth Monitoring Examples (#3971)
    * chore(examples/docs): Updating Synth Monitoring Examples
    
    * updating the synth playwright
    
    * updates based on feedback

--- examples/tracetest-synthetic-monitoring/resources/run.sh
+++ examples/tracetest-synthetic-monitoring/resources/run.sh
@@ -5,15 +5,13 @@ set -e
 TOKEN=$TRACETEST_TOKEN
 ENVIRONMENT_ID=$TRACETEST_ENVIRONMENT_ID
 
-apply() {
+run() {
   echo "Configuring Tracetest"
   tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID
 
-  echo "Applying Resources"
-  tracetest apply datastore -f /resources/datastore.yaml
-  tracetest apply test -f /resources/test.yaml
-  tracetest apply monitor -f /resources/monitor.yaml
+  echo "Running Trace-Based Tests..."
+  tracetest run test -f /resources/test.yaml
   tracetest list monitor
 }
 
-apply
+run
