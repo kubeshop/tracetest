2ca16058222a2616db9a4e81622cf13a5b1c270c
Daniel Dias <danielbpdias@gmail.com>
2024-08-22 19:31:34 -0300

    fix(example): update observability to the rescue example

--- examples/observability-to-the-rescue/gateway-api/config.js
+++ examples/observability-to-the-rescue/gateway-api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.YOUR_API_PORT,
+  port: process.env.API_PORT,
   otelServiceName: process.env.OTEL_SERVICE_NAME,
   otelExporterGrpcUrl: process.env.OTEL_EXPORTER_GRPC_URL,
   walletAPIEndpoint: process.env.WALLET_API_ENDPOINT,
