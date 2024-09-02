b93ead9726128317caf44627f4353032c946f092
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-09-02 10:46:22 -0600

    removing unecessary code

--- examples/tracetest-jaeger-graphql-pokeshop/resources/apply.sh
+++ examples/tracetest-jaeger-graphql-pokeshop/resources/apply.sh
@@ -4,11 +4,10 @@ set -e
 
 TOKEN=$TRACETEST_TOKEN
 ENVIRONMENT_ID=$TRACETEST_ENVIRONMENT_ID
-SERVER_URL=$SERVER_URL
 
 apply() {
   echo "Configuring TraceTest"
-  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID --server-url $SERVER_URL
+  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID
 
   echo "Applying Resources"
   tracetest apply datastore -f /resources/datastore.yaml
--- examples/tracetest-jaeger-graphql-pokeshop/resources/run.sh
+++ examples/tracetest-jaeger-graphql-pokeshop/resources/run.sh
@@ -4,11 +4,10 @@ set -e
 
 TOKEN=$TRACETEST_TOKEN
 ENVIRONMENT_ID=$TRACETEST_ENVIRONMENT_ID
-SERVER_URL=$SERVER_URL
 
 run() {
   echo "Configuring Tracetest"
-  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID --server-url $SERVER_URL
+  tracetest configure --token $TOKEN --environment $ENVIRONMENT_ID
 
   echo "Running Trace-Based Tests..."
   tracetest run test -f /resources/test.yaml
