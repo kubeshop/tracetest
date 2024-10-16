83c9d0c0e56d304430118e3fcedacfda71f9b8b2
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-10-16 15:05:30 -0600

    updating the browser example to use the tracetest sdk

--- examples/getting-started/browser/src/instrumentation.js
+++ examples/getting-started/browser/src/instrumentation.js
@@ -1,45 +1,8 @@
-import { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } from "@opentelemetry/core";
-import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
-import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { ZoneContextManager } from "@opentelemetry/context-zone";
-import { UserInteractionInstrumentation } from "@tracetest/instrumentation-user-interaction";
-import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
-import { Resource } from "@opentelemetry/resources";
+import TracetestWebSDK from "@tracetest/opentelemetry-web";
 
-const provider = new WebTracerProvider({
-  resource: new Resource({
-    "service.name": "browser-app",
-  }),
+const sdk = new TracetestWebSDK({
+  serviceName: "browser-app",
+  endpoint: "http://localhost:4318/v1/traces",
 });
 
-provider.addSpanProcessor(
-  new BatchSpanProcessor(new OTLPTraceExporter({ url: "http://localhost:4318/v1/traces" }), {
-    maxQueueSize: 10000,
-    scheduledDelayMillis: 200,
-  })
-);
-
-provider.register({
-  contextManager: new ZoneContextManager(),
-  propagator: new CompositePropagator({
-    propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
-  }),
-});
-
-registerInstrumentations({
-  tracerProvider: provider,
-  instrumentations: [
-    new UserInteractionInstrumentation(),
-    getWebAutoInstrumentations({
-      "@opentelemetry/instrumentation-fetch": {
-        propagateTraceHeaderCorsUrls: /.*/,
-        clearTimingResources: true,
-      },
-      "@opentelemetry/instrumentation-user-interaction": {
-        enabled: false,
-      },
-    }),
-  ],
-});
+sdk.start();
